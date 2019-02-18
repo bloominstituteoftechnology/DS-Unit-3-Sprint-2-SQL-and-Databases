e1e6952ebf43a39313d12c4bc03202b44ecdeb6f
Mark Oliver <mark.c.oliver@gmail.com>
2019-02-18 17:58:17 -0500

    done

---
+++ module1-introduction-to-sql/rpg_queries.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+import sqlite3 as sql
+import pandas as pd
+print(pd.__version__)
+conc = sql.connect('rpg_db.sqlite3')
+cursor = conc.cursor()
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+print(cursor.fetchall())
+
+q = 'SELECT COUNT(*) from charactercreator_character'
+c = conc.execute(q).fetchone()
+print(f"How many total Characters are there? {c[0]}")
+
+q = 'SELECT COUNT(*) FROM django_content_type AS d WHERE d.app_label=="charactercreator" and d.model != "character"';
+c = conc.execute(q).fetchone()
+print(f"How many of each specific subclass? {c[0]})")
+
+q = "SELECT COUNT(*) FROM armory_item"
+c = conc.execute(q).fetchone()
+print(f"How many total Items? {c[0]})")
+
+q = "SELECT COUNT(*) FROM armory_weapon"
+c = conc.execute(q).fetchone()
+q = "SELECT COUNT(*) FROM armory_item"
+cn = conc.execute(q).fetchone()
+print(f"How many of the Items are weapons?  {c[0]} How many are not? {cn[0] - c[0]})")
+
+q = """
+SELECT i.character_id, c.name, count(*) 
+FROM charactercreator_character_inventory AS i JOIN 
+charactercreator_character as c ON i.character_id = c.character_id
+GROUP BY i.character_id
+ORDER BY count(*) DESC
+LIMIT 20
+"""
+c = conc.execute(q).fetchall()
+d = {}
+for o in c:
+  d[o[0]] = o[1:]
+df = pd.DataFrame.from_dict(d, orient='index', columns=['character name','# of items'])
+df.index.name = 'character id'
+print("How many Items does each character have? (Return first 20 rows)")
+print(df.head(20),'\n\n')
+
+
+q = """
+SELECT i.character_id, c.name, count(*) 
+FROM armory_weapon AS w JOIN charactercreator_character_inventory AS i 
+ON w.item_ptr_id = i.character_id JOIN
+charactercreator_character as c ON i.character_id = c.character_id
+GROUP BY i.character_id
+ORDER BY count(*) DESC
+LIMIT 20
+"""
+c = conc.execute(q).fetchall()
+d = {}
+for o in c:
+  d[o[0]] = o[1:]
+df = pd.DataFrame.from_dict(d, orient='index', columns=['character name','# of weapons'])
+df.index.name = 'character id'
+print(f"How many Weapons does each character have? (Return first 20 rows")
+print(df.head(20),'\n\n')
+
+q = "SELECT COUNT(*) from charactercreator_character"
+t = conc.execute(q).fetchone()[0]
+q = f"SELECT COUNT(*)/{t}. FROM charactercreator_character_inventory"
+print(f" On average, how many Items does each Character have?  {conc.execute(q).fetchone()[0]}")
+
+q = """
+SELECT AVG(items)
+FROM (
+     SELECT character_id, count(item_id) as items
+     FROM charactercreator_character_inventory
+     GROUP BY character_id);"""
+print(f" On average, how many Items does each Character have?  {conc.execute(q).fetchone()[0]}")
+
+q = """
+SELECT AVG(items)
+FROM (
+     SELECT character_id, count(item_id) as items
+     FROM armory_weapon AS w JOIN charactercreator_character_inventory AS i 
+     ON w.item_ptr_id = i.character_id
+     GROUP BY character_id)"""
+
+print(f" On average, how many Weapons does each Character have?  {conc.execute(q).fetchone()[0]}")
