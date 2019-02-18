361cbef0ac928a0cfad869fc4480a164a0d9473a
phantasm320 <xsportrudy@yahoo.com>
2019-02-17 20:18:08 -0600

    Made a SQLite3 converter to pd DataFrame

--- module1-introduction-to-sql/read_db.py
+++ module1-introduction-to-sql/read_db.py
@@ -1,14 +1,23 @@
 import sqlite3
-
+import pandas as pd
 
 class Read_DB():
 
     def __init__(self, db_file):
         self.db_file = db_file
 
-    def read_db(self, columns, table):
+    def create_dataframe(self, columns, table):
+        data = []
+        col_names = columns.split(",")
         conn = sqlite3.connect(self.db_file)
         c = conn.cursor()
-        c.execute('SELECT {} FROM {}'.format(columns, table))
-        for row in c.fetchall():
-            print(row)
+        for row in c.execute('SELECT {} FROM {}'.format(columns, table)):
+            pre_data = []
+            for i in row:
+                pre_data.append(i)
+            data.append(pre_data)
+
+        if columns == '*':
+            return pd.DataFrame(data=data)
+        else:
+            return pd.DataFrame(data=data, columns=col_names)
--- module1-introduction-to-sql/rpg_queries.py
+++ module1-introduction-to-sql/rpg_queries.py
@@ -1,6 +1,7 @@
 import sqlite3
+import pandas as pd
 from read_db import*
 
 df = Read_DB('rpg_db.sqlite3')
 
-df.read_db("*", "armory_item")
+print(df.create("*", "armory_item"))
