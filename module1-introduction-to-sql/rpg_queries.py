52dce0651afd8dbfd436306cf7e22d4b063e5e99
Kailey Smith <krsmith310@gmail.com>
2019-02-18 15:49:23 -0700

    updated assignment 1 using pandas for output of queries

--- module1-introduction-to-sql/rpg_queries.py
+++ module1-introduction-to-sql/rpg_queries.py
@@ -1,4 +1,5 @@
 import sqlite3
+import pandas as pd
 
 conn = sqlite3.connect('rpg_db.sqlite3')
 curs = conn.cursor()
@@ -6,98 +7,89 @@ curs = conn.cursor()
 
 # How many total Characters are there?
 def total_characters():
-    curs.execute("""SELECT COUNT(distinct character_id)
-                    FROM charactercreator_character;""")
-    return curs.fetchall()
+    print(pd.read_sql_query("""SELECT COUNT(distinct character_id) as character_count
+    FROM charactercreator_character;""", conn))
 
 
 # How many of each specific subclass?
 def subclass_count():
-    curs.execute("""SELECT 'mages', COUNT(*) 
-                    FROM charactercreator_mage
+    print(pd.read_sql_query("""SELECT 'mages' as subclass, COUNT(*) as character_count
+    FROM charactercreator_mage
 
-                    UNION
+    UNION
 
-                    SELECT 'clerics', COUNT(*) 
-                    FROM charactercreator_cleric
+    SELECT 'clerics', COUNT(*)
+    FROM charactercreator_cleric
 
-                    UNION
+    UNION
 
-                    SELECT 'fighter', COUNT(*) 
-                    FROM charactercreator_fighter
+    SELECT 'fighter', COUNT(*)
+    FROM charactercreator_fighter
 
-                    UNION
+    UNION
 
-                    SELECT 'thieves', COUNT(*) 
-                    FROM charactercreator_thief;""")
-    return curs.fetchall()
+    SELECT 'thieves', COUNT(*)
+    FROM charactercreator_thief;""", conn))
 
 
 # How many total Items?
 def total_items():
-    curs.execute("""SELECT COUNT(distinct item_id)
-                    FROM armory_item;""")
-    return curs.fetchall()
+    print(pd.read_sql_query("""SELECT COUNT(distinct item_id) as item_count
+        FROM armory_item;""", conn))
 
 
 # How many of the Items are weapons? How many are not?
 def weapon_items():
-    curs.execute("""SELECT COUNT(*)
+    print(pd.read_sql_query("""SELECT COUNT(*) as weapon_count
                     FROM armory_item
                     WHERE item_id IN
                         (SELECT distinct item_ptr_id
-                        FROM armory_weapon);""")
-    return curs.fetchall()
+                        FROM armory_weapon);""", conn))
 
 
 def nonweapon_items():
-    curs.execute("""SELECT COUNT(*)
+    print(pd.read_sql_query("""SELECT COUNT(*) as nonweapon_count
                     FROM armory_item
                     WHERE item_id NOT IN
                         (SELECT distinct item_ptr_id
-                        FROM armory_weapon);""")
-    return curs.fetchall()
+                        FROM armory_weapon);""", conn))
 
 
 # How many Items does each character have? (Return first 20 rows)
 def character_items():
-    curs.execute("""SELECT character_id, count(item_id)
+    print(pd.read_sql_query("""SELECT character_id, count(item_id) as item_count
                     FROM charactercreator_character_inventory
                     GROUP BY character_id
-                    LIMIT 20;""")
-    return curs.fetchall()
+                    LIMIT 20;""", conn))
 
 
 # How many Weapons does each character have? (Return first 20 rows)
 def character_weapons():
-    curs.execute("""SELECT character_id, count(item_id)
+    print(pd.read_sql_query("""SELECT character_id, count(item_id) as weapon_count
                     FROM charactercreator_character_inventory
                     WHERE item_id IN
                         (SELECT distinct item_ptr_id
                         FROM armory_weapon)
                     GROUP BY character_id
-                    LIMIT 20;""")
-    return curs.fetchall()
+                    LIMIT 20;""", conn))
 
 
 # On average, how many Items does each Character have?
 def avg_items():
-    curs.execute("""SELECT AVG(items)
+    print(pd.read_sql_query("""SELECT AVG(items) as average_items
                     FROM (
                         SELECT character_id, count(item_id) as items
                         FROM charactercreator_character_inventory
-                        GROUP BY character_id);""")
-    return curs.fetchall()
+                        GROUP BY character_id);""", conn))
 
 
 # On average, how many Weapons does each character have?
 def avg_weapons():
-    curs.execute("""SELECT AVG(items)
+    print(pd.read_sql_query("""SELECT AVG(items) as average_weapons
                     FROM (
                         SELECT character_id, count(item_id) as items
                         FROM charactercreator_character_inventory
                         WHERE item_id IN (
                                     SELECT distinct item_ptr_id
                                     FROM armory_weapon)
-                        GROUP BY character_id);""")
-    return curs.fetchall()
+                        GROUP BY character_id);""", conn))
