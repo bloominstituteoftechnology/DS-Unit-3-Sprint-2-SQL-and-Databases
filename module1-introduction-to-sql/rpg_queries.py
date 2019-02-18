af76a79fd0d38e516c4805706f7dbe0c53881481
Quinn Dougherty <quinndougherty92@gmail.com>
2019-02-18 16:08:55 -0500

    done assignment

--- module1-introduction-to-sql/rpg_queries.py
+++ module1-introduction-to-sql/rpg_queries.py
@@ -9,6 +9,9 @@ ASSIGNMENT: construct sql commands for the following questions.
     6: On average, how many Weapons does each character have?
 '''
 import sqlite3 as sl
+from numpy import divide as div
+from numpy import mean
+from functools import reduce
 
 conn = sl.connect('rpg_db.sqlite3')
 
@@ -26,11 +29,15 @@ def selcount(tab):
     return q(tab)
 
 queries = {1: selcount('character'), 
-        2: '', 
-        3: '', 
-        4: '', 
-        5: '',
-        6: ''}
+           2: '<~ this is too complicated because its four different queries rn~>', 
+           3: 'SELECT COUNT(*) FROM armory_item;', 
+           4: 'SELECT COUNT(*) FROM armory_weapon', 
+           5: 'SELECT COUNT(item_id) FROM charactercreator_character_inventory GROUP BY character_id;',
+           6: '''SELECT COUNT(item_id) 
+                FROM charactercreator_character_inventory as invs
+                JOIN armory_weapon as weap
+                ON weap.item_ptr_id = invs.item_id
+                GROUP BY character_id;'''}
 
 total_characters = quer_fetch(queries[1])[0][0]
 
@@ -44,12 +51,27 @@ def cartesianproducttest(l):
     it was returning four copies of a really large number rather than 4 separate smaller numbers. 
     
     this tests my hypothesis--- that the product of all 4 individuals is equal to the number i got. '''
-    from functools import reduce
+    
     assert 28090800 == reduce(lambda x,y: x*y, l)
+    pass 
 
+total_items = quer_fetch(queries[3])[0][0]
+weapons_count = quer_fetch(queries[4])[0][0]
 
+items_list = [t[0] for t in quer_fetch(queries[5])]
 
-print(total_characters)
-print(count_each_subclass)
+weaps_list = [t[0] for t in quer_fetch(queries[6])]
+
+print(f'there are {total_characters} characters. ')
+print(reduce(lambda s,t: s+t, [f'there are {num} {cla}s. ' for num,cla in zip(count_each_subclass, classes)]))
 
 cartesianproducttest(count_each_subclass)
+
+print(f'there are {total_items} total items')
+
+print(f'{total_items-weapons_count} is the number of non-weapon items. ')
+
+print(f'The mean number of items each character carries is {mean(items_list):.3}')
+
+print(f'The mean number of weapons carried by a character is {mean(weaps_list):.3}')
+
