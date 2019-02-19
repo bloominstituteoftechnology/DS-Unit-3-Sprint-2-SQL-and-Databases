bf187e2b111bf0bb9e0185a97d9ceb0b87071be0
Dpgofast <43558343+Dpgofast@users.noreply.github.com>
2019-02-19 17:52:27 -0600

    ..

--- module2-sql-for-analysis/insert_titanic.py
+++ module2-sql-for-analysis/insert_titanic.py
@@ -7,18 +7,18 @@ conn = pig.connect(dbname=dbname,user=user,
                     password=password,host=host)
 curse = conn.cursor()
 
-# make_titanic_table = curse.execute('''
-#     CREATE TABLE titanic_table(
-#         survived boolean PRIMARY KEY,
-#         pclass int,
-#         name varchar(40),
-#         sex text,
-#         age int,
-#         spouse_sibling text,
-#         parents_children text,
-#         fare numeric
-#     )
-# '''),conn.commit()
+make_titanic_table = curse.execute('''
+    CREATE TABLE titanic_table(
+        survived boolean PRIMARY KEY,
+        pclass int,
+        name varchar(40),
+        sex text,
+        age int,
+        spouse_sibling text,
+        parents_children text,
+        fare numeric
+    )
+'''),conn.commit()
 
 
 with open('titanic.csv', 'r') as f:
