73e8d72eba904b63f8ed0521339beb25c842ce56
Crawford <crawford.collins@gmail.com>
2019-02-19 17:37:51 -0600

    updated add.sh maybe

--- add.sh
+++ add.sh
@@ -1,5 +1,5 @@
 git add module2-sql-for-analysis/work.ipynb module2-sql-for-analysis/example.py \
-.gitignore add.sh
+.gitignore add.sh;
 read -p "enter your commit message: " m;
-git commit -mm
-git push
+git commit -m $m;
+git push ;
