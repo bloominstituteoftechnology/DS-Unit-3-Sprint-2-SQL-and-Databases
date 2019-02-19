6861904fa65b63068677f829d270906738848c8b
Mark Oliver <mark.c.oliver@gmail.com>
2019-02-19 10:24:09 -0500

    sql, pep8

--- module1-introduction-to-sql/rpg_queries.py
+++ module1-introduction-to-sql/rpg_queries.py
@@ -12,26 +12,25 @@ q = 'SELECT COUNT(*) from charactercreator_character'
 c = conc.execute(q).fetchone()
 print(f"How many total Characters are there? {c[0]}")
 
-q = 'SELECT COUNT(*) FROM django_content_type AS d WHERE d.app_label=="charactercreator" and d.model != "character"';
+q = """SELECT COUNT(*) FROM django_content_type AS d
+WHERE d.app_label=='charactercreator' and d.model != 'character'"""
 c = conc.execute(q).fetchone()
 print(f"How many subclasses are there? {c[0]}")
 q = """
-   SELECT d.model AS model FROM django_content_type AS d WHERE d.app_label=="charactercreator" and d.model != "character"
+   SELECT d.model AS model FROM django_content_type AS d
+   WHERE d.app_label=="charactercreator" and d.model != "character"
 """
 print(f"How many of each specific subclass?")
 c = conc.execute(q).fetchall()
 for m in c:
-   if m[0] == 'necromancer':
-       print('\t', 'mage-necromancer', end='')
-   else:
-       print('\t', m[0], end='')
-   q = f"select count(*) from charactercreator_{m[0]}"
-   c = conc.execute(q).fetchone()[0]
-   if m[0] == 'mage':
-       nq = f"select count(*) from charactercreator_necromancer"
-       nc = conc.execute(nq).fetchone()[0]
-       c -= nc
-   print(f': {c}') 
+    print('\t', 'mage:necromancer' if m[0] == 'necromancer' else m[0], end='')
+    q = f"select count(*) from charactercreator_{m[0]}"
+    c = conc.execute(q).fetchone()[0]
+    if m[0] == 'mage':
+        nq = f"select count(*) from charactercreator_necromancer"
+        nc = conc.execute(nq).fetchone()[0]
+        c -= nc
+    print(f': {c}')
 
 q = "SELECT COUNT(*) FROM armory_item"
 c = conc.execute(q).fetchone()
@@ -39,13 +38,17 @@ print(f"How many total Items? {c[0]})")
 
 q = "SELECT COUNT(*) FROM armory_weapon"
 c = conc.execute(q).fetchone()
-q = "SELECT COUNT(*) FROM armory_item"
+
+q = """SELECT COUNT(*)
+FROM armory_item
+WHERE item_id NOT IN (SELECT distinct item_ptr_id FROM armory_weapon)"""
 cn = conc.execute(q).fetchone()
-print(f"How many of the Items are weapons?  {c[0]} How many are not? {cn[0] - c[0]})")
+
+print(f"How many of the Items are weapons?  {c[0]} How many are not? {cn[0]})")
 
 q = """
-SELECT i.character_id, c.name, count(*) 
-FROM charactercreator_character_inventory AS i JOIN 
+SELECT i.character_id, c.name, count(*)
+FROM charactercreator_character_inventory AS i JOIN
 charactercreator_character as c ON i.character_id = c.character_id
 GROUP BY i.character_id
 ORDER BY count(*) DESC
@@ -54,16 +57,17 @@ LIMIT 20
 c = conc.execute(q).fetchall()
 d = {}
 for o in c:
-  d[o[0]] = o[1:]
-df = pd.DataFrame.from_dict(d, orient='index', columns=['character name','# of items'])
+    d[o[0]] = o[1:]
+df = pd.DataFrame.from_dict(d, orient='index', columns=[
+                            'character name', '# of items'])
 df.index.name = 'character id'
 print("How many Items does each character have? (Return first 20 rows)")
-print(df.head(20),'\n\n')
+print(df.head(20), '\n\n')
 
 
 q = """
-SELECT i.character_id, c.name, count(*) 
-FROM armory_weapon AS w JOIN charactercreator_character_inventory AS i 
+SELECT i.character_id, c.name, count(*)
+FROM armory_weapon AS w JOIN charactercreator_character_inventory AS i
 ON w.item_ptr_id = i.character_id JOIN
 charactercreator_character as c ON i.character_id = c.character_id
 GROUP BY i.character_id
@@ -73,16 +77,19 @@ LIMIT 20
 c = conc.execute(q).fetchall()
 d = {}
 for o in c:
-  d[o[0]] = o[1:]
-df = pd.DataFrame.from_dict(d, orient='index', columns=['character name','# of weapons'])
+    d[o[0]] = o[1:]
+df = pd.DataFrame.from_dict(d, orient='index', columns=[
+                            'character name', '# of weapons'])
 df.index.name = 'character id'
 print(f"How many Weapons does each character have? (Return first 20 rows")
-print(df.head(20),'\n\n')
+print(df.head(20), '\n\n')
 
 q = "SELECT COUNT(*) from charactercreator_character"
 t = conc.execute(q).fetchone()[0]
 q = f"SELECT COUNT(*)/{t}. FROM charactercreator_character_inventory"
-print(f" On average, how many Items does each Character have?  {conc.execute(q).fetchone()[0]}")
+print(
+    f" On average, how many Items does each Character have?  \
+    {conc.execute(q).fetchone()[0]}")
 
 q = """
 SELECT AVG(items)
@@ -90,7 +97,9 @@ FROM (
      SELECT character_id, count(item_id) as items
      FROM charactercreator_character_inventory
      GROUP BY character_id);"""
-print(f" On average, how many Items does each Character have?  {conc.execute(q).fetchone()[0]}")
+print(
+  f" On average, how many Items does each Character have?  \
+  {conc.execute(q).fetchone()[0]}")
 
 
 q = """
@@ -98,11 +107,11 @@ SELECT (total * 1.0)/n  FROM (
 (SELECT SUM(items) as total
 FROM (
      SELECT i.character_id as id, count(*) as items
-     FROM armory_weapon AS w JOIN charactercreator_character_inventory AS i 
+     FROM armory_weapon AS w JOIN charactercreator_character_inventory AS i
      ON w.item_ptr_id = i.character_id
      GROUP BY i.character_id))
 ,
-(SELECT n FROM (select count(*) as n FROM charactercreator_character))) 
+(SELECT n FROM (select count(*) as n FROM charactercreator_character)))
  """
 fo = conc.execute(q).fetchone()
 print(f" On average, how many Weapons does each Character have?  {fo[0]}")
