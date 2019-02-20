b14333e7b8756762b8595bb85ea52afdad451454
ed.chin.git <edgardochin@gmail.com>
2019-02-20 13:52:50 -0600

    save local workflow

--- module2-sql-for-analysis/titanic.py
+++ module2-sql-for-analysis/titanic.py
@@ -1,49 +1,80 @@
 import psycopg2 as pgres
 import pandas as pd
 
-# Connect to an ElephantSQL
-conx_str = """
-dbname='zoafqkfp' user='zoafqkfp'
-host='stampy.db.elephantsql.com'
-password='MCJu21HwCynkStIs3FOmV-xHXSAXOSDD'
-"""
-conx = pgres.connect(conx_str)
-cur = conx.cursor()
+ele_conn = ''
+ele_cur=None
 
+
+# ___ Connect to ElephantSQL db _________
+def conx_elephant(conx_str):
+    # instantiate and return connection obj
+    cnx = pgres.connect(conx_str)
+    return cnx
+
+
+# _____ DROP table ____________
+def drop_table(tbl_name):
+    qry = "DROP TABLE " + tbl_name
+    ele_cur.execute(qry)
+    ele_conn.commit()
+    return
 
 
 # ---- CREATE a new table------------------
-qry = "CREATE TABLE test_table (id serial PRIMARY KEY, name varchar(40) NOT NULL, data JSONB);"
-cur.execute(qry)
-conx.commit()
+def create_table(tbl_name, fields_str):
+    qry = "CREATE TABLE " + tbl_name + 
+    ele_cur.execute(qry)
+    ele_conn.commit()
+    return
 
-# ---- INSERT data
-# fill aquery placeholders and let Psycopg perform
-# the correct conversion (no more SQL injections!)
-cur = conx.cursor()
-new_data = [("Gretsch", {'year': '1975', 'color': 'silver'}),
-            ("Fender", {'year': '1972', 'color': 'blonde'}),
-            ("Rickenbacker", {'year': '1965', 'color': 'red'}),
-            ("Gibson", {'year': '1959', 'color': 'sunburst'}),
-            ("Taylor", {'year': '1992', 'color': 'spruce'})]
-in_qry = "INSERT INTO test_table (name, data) VALUES (%s, (%s+'::JSONB');"
-print(in_qry)
-# for name, data in new_data:
-#     cur.execute(in_qry, (name, data))
-# conx.commit()
 
-# ---print contents of table
+# ___________ INSERT data ___________________________
+def insert_data():
+    cur = ele_conn.cursor()
+    new_data = [("Gretsch", {'year': '1975', 'color': 'silver'}),
+                ("Fender", {'year': '1972', 'color': 'blonde'}),
+                ("Rickenbacker", {'year': '1965', 'color': 'red'}),
+                ("Gibson", {'year': '1959', 'color': 'sunburst'}),
+                ("Taylor", {'year': '1992', 'color': 'spruce'})]
+    in_qry = "INSERT INTO test_table (name, data) VALUES (%s, (%s+'::JSONB');"
+    print(in_qry)
+    # for name, data in new_data:
+    #     cur.execute(in_qry, (name, data))
+    # conx.commit()
+    return
+
+# _______  SELECT data _______________________________
 # rows = cur.execute("SELECT * FROM test_table;")
 # for row in rows:
 #    print(row)
 # cur.fetchone()
 # cur.fetchall()
 
-# ------DROP  table -----------
-qry = "DROP TABLE test_table"
-cur.execute(qry)
-conx.commit()
 
-# --- we're done here ------
-cur.close()
-conx.close()
+def run_conversion(ele_cur):
+    return
+
+
+def main():
+    # ____ Connect to an ElephantSQL __________
+    conx_str = """
+    dbname='zoafqkfp' user='zoafqkfp'
+    host='stampy.db.elephantsql.com'
+    password='MCJu21HwCynkStIs3FOmV-xHXSAXOSDD'
+    """
+    ele_conn = conx_elephant(conx_str)
+    
+    # ____ create cursor ___
+    ele_cur = ele_conn.cursor()  
+    
+    # ____ Port titanic.csv to Postgres ___
+    # run_conversion(ele_cur)
+
+    # ___ end main ___________
+    ele_cur.close()   # close cursor
+    ele_conn.close()  # close connection   
+    return
+
+#  Launched from the command line
+if __name__ == '__main__':
+    main()
---
+++ module2-sql-for-analysis/titanic_queries.py
@@ -0,0 +1,139 @@
+"""
+----------------------------------------------------------------
+             RPG DataBase Queries
+----------------------------------------------------------------
+"""
+import psycopg2
+
+
+# ___ Connect to ElephantSQL db _________
+def conx_elephant(conx_str):
+    # instantiate and return connection obj
+    cnx = psycopg2.connect(conx_str)
+    return cnx
+
+
+# ___ QUERIES _________________________________
+def run_queries(c):
+    print('----- R P G  D a t a b a s e   I N F O ------')
+    # _____ How Many Total Characters_____________________
+    for row in c.execute('SELECT COUNT(character_id) FROM charactercreator_character'):
+        print('There are a total of', row[0], 'Characters')
+
+    # _____ How many of each Character sublass  _____________
+    for row in c.execute('SELECT COUNT(character_ptr_id) FROM charactercreator_cleric'):
+        print(row[0], 'Clerics')
+    for row in c.execute('SELECT COUNT(character_ptr_id) FROM charactercreator_fighter'):
+        print(row[0], 'Fighters')
+    for row in c.execute('SELECT COUNT(character_ptr_id) FROM charactercreator_thief'):
+        print(row[0], 'Thieves')
+    for mage in c.execute('SELECT COUNT(character_ptr_id) FROM charactercreator_mage'):
+        for necro in c.execute('SELECT COUNT(mage_ptr_id) FROM charactercreator_necromancer'):
+            print(mage[0], 'Mages, of which', necro[0], 'are Necromancers')
+    # for necro in c.execute('SELECT COUNT(mage_ptr_id) FROM charactercreator_necromancer'):
+    #    print('   ', necro[0], 'Mages are Necromancers')
+
+    print()
+
+    # _______________ How many Total Items _________________________________
+    for i in c.execute('SELECT COUNT(item_id) FROM armory_item'):
+        print('There are a total of', i[0], 'items')
+    # _______________ How many of Weapons and Non-Weapons ________________
+    for w in c.execute('SELECT COUNT(item_ptr_id) FROM armory_weapon'):
+        print(w[0], 'Weapons')
+    print((i[0] - w[0]), 'Non-Weapons')
+    print()
+
+    # _______________ how many Items does each Character have? ____________
+    query1 = '''
+    SELECT character.name, item.name
+        FROM charactercreator_character AS character,
+             charactercreator_character_inventory AS inventory,
+             armory_item AS item
+        WHERE character.character_id = inventory.character_id
+          AND inventory.item_id = item.item_id
+        ORDER BY character.name
+        LIMIT 20;
+    '''
+    print('Character  -->  Item')
+    print('--------------------')
+    rows = c.execute(query1)
+    for row in rows:
+        name = row[0]
+        print(name, '-->', row[1])
+    print('--------------------')
+
+    # _____ On average, how many Items does each Character have? _________
+    query2 = '''
+    SELECT AVG(item_count)
+    FROM (
+        SELECT character.name, count(item.item_id) as item_count
+        FROM charactercreator_character AS character,
+             charactercreator_character_inventory AS inventory,
+             armory_item AS item
+        WHERE character.character_id = inventory.character_id
+          AND inventory.item_id = item.item_id
+        GROUP BY character.name
+        ORDER BY character.name
+        LIMIT 20 );
+    '''
+    for i in c.execute(query2):
+        print('Each character has average of', i[0], 'items\n')
+
+    # _____ how many WEAPONS does each Character have? ___________________
+    query1 = '''
+    SELECT character.name, item.name, weapon.power
+        FROM charactercreator_character AS character,
+             charactercreator_character_inventory AS inventory,
+             armory_item AS item,
+             armory_weapon as weapon
+        WHERE character.character_id = inventory.character_id
+          AND inventory.item_id = weapon.item_ptr_id
+          AND weapon.item_ptr_id = item.item_id
+        ORDER BY character.name
+        LIMIT 20;
+    '''
+    print('Character  -->  WEAPON')
+    print('--------------------')
+    rows = c.execute(query1)
+    for row in rows:
+        name = row[0]
+        print(name, '-->', row[1], '  ', row[2])
+    print('--------------------')
+
+    # ____ On average, how many Weapons does each Character have? ___________
+    query2 = '''
+    SELECT AVG(weapon_count)
+    FROM (
+        SELECT character.name, count(weapon.item_ptr_id) as weapon_count
+            FROM charactercreator_character AS character,
+                 charactercreator_character_inventory AS inventory,
+                 armory_weapon AS weapon
+            WHERE character.character_id = inventory.character_id
+            AND inventory.item_id = weapon.item_ptr_id
+            GROUP BY character.name
+            ORDER BY character.name
+            LIMIT 20);
+    '''
+    for i in c.execute(query2):
+        print('Each character has average of', i[0], 'weapons\n')
+
+    return
+
+
+def main():
+    conx_str = """
+    dbname='zoafqkfp' user='zoafqkfp'
+    host='stampy.db.elephantsql.com'
+    password='MCJu21HwCynkStIs3FOmV-xHXSAXOSDD'
+    """
+    conn = conx_elephant(conx_str)
+    cur = conn.cursor()  # create cursor
+    # run_queries(cur)
+    cur.close()
+    conn.close()   # Close the connection
+    return
+
+#  Launched from the command line
+if __name__ == '__main__':
+    main()
