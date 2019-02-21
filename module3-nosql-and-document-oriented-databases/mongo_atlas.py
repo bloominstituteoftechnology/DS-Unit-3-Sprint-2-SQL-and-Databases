e9cd804f20e6ee97d88f4829f18670260d528a80
Mark Oliver <mark.c.oliver@gmail.com>
2019-02-21 00:12:30 -0500

    removed blank lines, pep8 120 col formatted

--- module3-nosql-and-document-oriented-databases/mongo_atlas.py
+++ module3-nosql-and-document-oriented-databases/mongo_atlas.py
@@ -2,7 +2,7 @@
 
 
 # "How was working with MongoDB different from working with PostgreSQL? What was easier, and what was harder?"
-#  Working with MongoDB today was taking data several collections to make 'character' documents, that was harder
+#  Working with MongoDB today was taking data from several collections to make 'character' documents, that was harder
 #  than moving tables from one relational database to another
 # coding: utf-8
 
@@ -19,61 +19,23 @@ import re
 import os
 from dotenv import load_dotenv
 
-
-# In[191]:
-
-
 load_dotenv()
-
-
-# In[192]:
-
 
 connection_string = os.getenv('ATLAS_CONNECTION_STRING')
 
-
-# In[194]:
-
-
 client = pymongo.MongoClient(connection_string)
 
-
-# In[195]:
-
-
 db = client.get_database()
-
-
-# In[196]:
-
-
-c = db.get_collection('cml')
-
-
-# In[197]:
-
-
-f = c.find_one()
-
-
-# In[215]:
-
 
 collection_name = 'characters'
 db.drop_collection(collection_name)
 db.create_collection(collection_name)
 
 
-# In[218]:
-
-
 rpg_db = sql.connect('../module1-introduction-to-sql/rpg_db.sqlite3')
 cursor = rpg_db.cursor()
 cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
 print(cursor.fetchall())
-
-
-# In[219]:
 
 
 if rpg_db.execute('SELECT w.power FROM armory_weapon w WHERE w.item_ptr_id = 139 ').fetchone()[0] == 0:
@@ -88,10 +50,6 @@ if rpg_db.execute('SELECT w.power FROM armory_weapon w WHERE w.item_ptr_id = 139
         )
     rpg_db.commit()
 
-
-# In[220]:
-
-
 if rpg_db.execute('SELECT i.value FROM armory_item i WHERE i.item_id = 1 ').fetchone()[0] == 0:
     L = rpg_db.execute('SELECT count(*) from armory_item').fetchone()[0]
     for i in range(L):
@@ -105,46 +63,21 @@ if rpg_db.execute('SELECT i.value FROM armory_item i WHERE i.item_id = 1 ').fetc
         )
     rpg_db.commit()
 
-
-# In[221]:
-
-
-# print(rpg_db.execute('SELECT w.power from armory_weapon w').fetchall())
-# print(rpg_db.execute('SELECT i.value, i.weight from armory_item i').fetchall())
-
-
-# In[222]:
-
-
 q = """SELECT sql FROM sqlite_master
 WHERE tbl_name = 'charactercreator_character' AND type = 'table'"""
 columns = rpg_db.execute(q).fetchall()
 
-
-# In[223]:
-
-
 q = columns[0][0]
 q = q[q.index('AUTOINCREMENT'):]
 r = regex.findall("(?<=(?:NOT NULL|AUTOINCREMENT),\s\")(\w+)\"\s(\w+)", q)
-print(r)
 
-
-# In[225]:
-
-
-r.insert(0, ('_id','integer'))
-r
-
-
-# In[226]:
-
+r.insert(0, ('_id', 'integer'))
 
 requests = []
 for ch in rpg_db.execute('SELECT * FROM charactercreator_character').fetchall():
-#     print(ch, type(ch))
+    #     print(ch, type(ch))
     d = {}
-    for n,i in zip(r,range(len(r))):
+    for n, i in zip(r, range(len(r))):
         d[n[0]] = ch[i]
     w = list(rpg_db.execute(
         f"""
@@ -156,7 +89,8 @@ for ch in rpg_db.execute('SELECT * FROM charactercreator_character').fetchall():
 		WHERE i.character_id = {ch[0]}
         """
     ).fetchall())
-    wl = [{'power': r[0], 'id': r[1], 'name': r[2], 'value': r[3], 'weight': r[4]} for r in w]
+    wl = [{'power': r[0], 'id': r[1], 'name': r[2],
+           'value': r[3], 'weight': r[4]} for r in w]
     d['weapons'] = wl
     it = list(rpg_db.execute(
         f"""
@@ -166,13 +100,10 @@ for ch in rpg_db.execute('SELECT * FROM charactercreator_character').fetchall():
         AND item.item_id NOT IN (SELECT DISTINCT item_ptr_id FROM armory_weapon) 
         """
     ).fetchall())
-    il = [{'id': r[0], 'name': r[1], 'value': r[2], 'weight': r[3]} for r in it]        
-    d['items'] = il 
-    requests.append(InsertOne(d));
-
-
-# In[227]:
+    il = [{'id': r[0], 'name': r[1], 'value': r[2], 'weight': r[3]}
+          for r in it]
+    d['items'] = il
+    requests.append(InsertOne(d))
 
 
 result = db.characters.bulk_write(requests)
-
