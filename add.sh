51e1f4df7bc1592d1f294fffb3bd69290d057ca0
Crawford <crawford.collins@gmail.com>
2019-02-21 15:34:33 -0600

    starting thursday assignment

--- add.sh
+++ add.sh
@@ -1,5 +1,5 @@
 git add module2-sql-for-analysis/work.ipynb module2-sql-for-analysis/example.py \
-.gitignore add.sh;
+.gitignore add.sh  module4-acid-and-database-scalability-tradeoffs/setup.py module2-sql-for-analysis/.gitignore; 
 read -p "enter your commit message: " m;
 git commit -m "$m";
 git push ;
---
+++ module4-acid-and-database-scalability-tradeoffs/setup.py
@@ -0,0 +1,5 @@
+dbname = 'uriewcad'
+user = dbname
+password = 'sxNWaGII98LdNEur_VSX5Y6q8IS3FztF'
+host = 'stampy.db.elephantsql.com'
+url = 'postgres://uriewcad:sxNWaGII98LdNEur_VSX5Y6q8IS3FztF@stampy.db.elephantsql.com:5432/uriewcad'
