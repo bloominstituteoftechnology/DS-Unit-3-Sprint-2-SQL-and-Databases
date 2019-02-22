7dfe737a2aba9baa4d547e1d67b885d3e47db7a8
johnpharmd <jhumphre@uw.edu>
2019-02-22 10:02:38 -0800

    Refactor 1st northwind query to correct functioning

--- northwind.py
+++ northwind.py
@@ -5,10 +5,10 @@ import sqlite3
 
 connection = sqlite3.connect('northwind_small.sqlite3')
 
-q_one = pd.read_sql_query("SELECT * from Products.UnitPrice LIMIT 10 ORDER BY DESC;", connection)
-print('Ten most expensive items from Products are', q_one)
+q_one = pd.read_sql_query("SELECT ProductName, UnitPrice from Product ORDER BY UnitPrice DESC LIMIT 10;", connection)
+print('Ten most expensive items from Product are', q_one)
 
-q_two= pd.read_sql_query("SELECT * from Employees;", connection)
+q_two= pd.read_sql_query("SELECT * from Employee;", connection)
 print('The average age of an employee at the time of their hiring is', q_two)
 
 
