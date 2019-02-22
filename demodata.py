46321823a4144f0bb8ceb9cc5c90522b4ef996e5
johnpharmd <jhumphre@uw.edu>
2019-02-22 08:20:17 -0800

    Refactor demodata.py

--- demodata.py
+++ demodata.py
@@ -4,7 +4,11 @@ import pandas as pd
 import sqlite3
 
 # Make connection object
+connection = sqlite3.connect('demo_data.sqlite3')
+c = connection.cursor()
 
-
+# Create table
+c.execute('''CREATE TABLE
+          ''')
 
 # Output some data
