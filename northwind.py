d3ce4afaca51ef2994e13d47b7e1a61a622b8896
johnpharmd <jhumphre@uw.edu>
2019-02-22 10:22:13 -0800

    Refactor to apparent correct functioning first query of Part 3

--- northwind.py
+++ northwind.py
@@ -12,10 +12,9 @@ q_two= pd.read_sql_query("SELECT * from Employee;", connection)
 # print('The average age of an employee at the time of their hiring is', q_two)
 
 # Part 3
-q_three = ("SELECT ProductName, UnitPrice, CompanyName from Product JOIN Suppliers ORDER BY DESC LIMIT 10;", connection)
-print('The ten most expensive items (per unit price) with their suppliers,
-      in the database, are', q_three)
+q_three = pd.read_sql_query("SELECT ProductName, UnitPrice, CompanyName from Product JOIN Supplier ORDER BY UnitPrice DESC LIMIT 10;", connection)
+print('\nThe ten most expensive items (per unit price) with their suppliers, in the database, are', q_three)
 
 q_four = ()
-print('The largest category (by number of products) is', q_four)
+print('\nThe largest category (by number of products) is', q_four)
 
