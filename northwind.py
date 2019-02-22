ba931ebf636ffdb96821aace2202d712d07f76be
johnpharmd <jhumphre@uw.edu>
2019-02-22 09:45:33 -0800

    Add connection logic, first query

--- northwind.py
+++ northwind.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python 3
 
+import pandas as pd
+import sqlite3
+
+connection = sqlite3.connect('northwind_small.sqlite3')
+
+q_one = pd.read_sql_query("SELECT * from Products.UnitPrice LIMIT 10 ORDER BY DESC;", connection)
+print('Ten most expensive items from Products are', q_one)
+
+q_two= pd.read_sql_query("SELECT * from Employees;", connection)
+print('The average age of an employee at the time of their hiring is', q_two)
+
+
+# Stretch
+# q_three_table = pd.read_sql_query("SELECT * from <table>;", connection)
 
