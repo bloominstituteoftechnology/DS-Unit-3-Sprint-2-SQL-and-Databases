20c3166bc08c945a7ab5794a1c98d6c62187396d
johnpharmd <jhumphre@uw.edu>
2019-02-22 08:23:58 -0800

    Create table, insert data

--- demodata.py
+++ demodata.py
@@ -8,7 +8,10 @@ connection = sqlite3.connect('demo_data.sqlite3')
 c = connection.cursor()
 
 # Create table
-c.execute('''CREATE TABLE
-          ''')
+c.execute('''CREATE TABLE demo
+          (s, x, y)''')
+
+# Insert data into table
+
 
 # Output some data
