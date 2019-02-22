e5379d7ea810336aa5476e32caf2a0793a5a6a5b
Manjula Mishra <manjulamishra@users.noreply.github.com>
2019-02-22 12:52:37 -0800

    Ave_Age, employee age by city  questions fixed

--- northwind.py
+++ northwind.py
@@ -6,37 +6,24 @@ conn = sqlite3.connect(
 # create a cursor
 curs = conn.cursor()
 
-curs.execute(
-    "SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;").fetchall()
-[('Category',), ('Customer',), ('CustomerCustomerDemo',),
- ('CustomerDemographic',), ('Employee',), ('EmployeeTerritory',), ('Order',),
- ('OrderDetail',), ('Product',), ('Region',), ('Shipper',), ('Supplier',),
- ('Territory',)]
-
-curs.execute('SELECT sql FROM sqlite_master WHERE name="Customer";').fetchall()
-[('CREATE TABLE "Customer" \n(\n  "Id" VARCHAR(8000) PRIMARY KEY, \n "CompanyName" VARCHAR(8000) NULL, \n  "ContactName" VARCHAR(8000) NULL, \n "ContactTitle" VARCHAR(8000) NULL, \n  "Address" VARCHAR(8000) NULL, \n  "City" VARCHAR(8000) NULL, \n  "Region" VARCHAR(8000) NULL, \n  "PostalCode" VARCHAR(8000) NULL, \n  "Country" VARCHAR(8000) NULL, \n  "Phone" VARCHAR(8000) NULL, \n  "Fax" VARCHAR(8000) NULL \n)')]
-
 
 # Question 1What are the ten most expensive
 # items (per unit price) in the database?
 most_expensive = """SELECT Id, ProductName, UnitPrice
-
-FROM Product 
-ORDER BY UnitPrice DESC LIMIT 10;"""
+FROM Product ORDER BY UnitPrice DESC LIMIT 10;"""
 curs.execute(most_expensive)
 print(curs.fetchall())
 
 
 # Q2 What is the average age of an employee at the time of their hiring?
 # (Hint: a lot of arithmetic works with dates.)
-avg_age = """SELECT AVG(strftime('%Y', date('now'))-strftime('%Y', Employee.BirthDate) )
-FROM Employee;"""
+avg_age = """SELECT AVG(HireDate - BirthDate)
+From Employee;"""
 curs.execute(avg_age)
 print(curs.fetchall())
 
 # (Stretch) How does the average age of employee at hire vary by city?
-avg_age = """SELECT City,
-AVG(strftime('%Y', date('now'))-strftime('%Y', Employee.BirthDate) )
+avg_age = """SELECT City, AVG(HireDate-BirthDate)
 FROM Employee
 GROUP BY Employee.City;
 """
@@ -53,8 +40,7 @@ print(curs.fetchall())
 
 #  What is the largest category (by number of products in it)?
 
-largest_category = """SELECT Category.Id, 
-COUNT(Product.CategoryId)
+largest_category = """SELECT Category.Id, COUNT(Product.CategoryId)
 FROM  Category, Product
 WHERE Category.Id = Product.CategoryId
 GROUP BY Product.CategoryId;"""
