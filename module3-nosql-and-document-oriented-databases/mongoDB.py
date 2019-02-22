6e8aff662f6bcb6b8b489bd98f73d99f8e2f7523
Tony Tia <webmaster@tonytia.com>
2019-02-22 08:05:32 -0800

    adding sqllite.readme

---
+++ module3-nosql-and-document-oriented-databases/mongoDB.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import pymongo
+
+conn_string="mongodb://dbUser19:LSVyKnHW@cluster0-shard-00-00-nadgn.mongodb.net:27017,cluster0-shard-00-01-nadgn.mongodb.net:27017,cluster0-shard-00-02-nadgn.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
+
+client=pymongo.MongoClient(conn_string)
+
+db=client.test
+
