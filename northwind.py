823302744f49fd7739e60a44934f261d134329e8
johnpharmd <jhumphre@uw.edu>
2019-02-22 10:15:08 -0800

    Work further on Part 3, first query

--- northwind.py
+++ northwind.py
@@ -9,12 +9,12 @@ q_one = pd.read_sql_query("SELECT ProductName, UnitPrice from Product ORDER BY U
 print('Ten most expensive items from Product are', q_one)
 
 q_two= pd.read_sql_query("SELECT * from Employee;", connection)
-print('The average age of an employee at the time of their hiring is', q_two)
+# print('The average age of an employee at the time of their hiring is', q_two)
 
 # Part 3
-q_three = ()
-print('The ten most expensive items (per unit price), with their suppliers,
-      in the database are', q_three)
+q_three = ("SELECT ProductName, UnitPrice, CompanyName from Product JOIN Suppliers ORDER BY DESC LIMIT 10;", connection)
+print('The ten most expensive items (per unit price) with their suppliers,
+      in the database, are', q_three)
 
 q_four = ()
 print('The largest category (by number of products) is', q_four)
