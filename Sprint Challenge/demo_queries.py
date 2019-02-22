8c2a184b7f4399df67aff8531d1ea366a41f2825
ed.chin.git <edgardochin@gmail.com>
2019-02-22 10:53:05 -0600

    finished part 1

--- Sprint Challenge/demo_queries.py
+++ Sprint Challenge/demo_queries.py
@@ -12,5 +12,24 @@ def run_queries(cur):
     ORDER BY demo.x;
     '''
     for row in cur.execute(qry):
-        print('There are ', row[0], ' rows where noth x and y are at least 5')
+        print('There are ', row[0], ' rows where both x and y are at least 5')
+
+    # _________ HOW MANY DISTINCT VALUES ______________
+    qry = 'SELECT COUNT(DISTINCT demo.y) FROM demo GROUP BY demo.y;'
+    ctr = 0
+    for row in cur.execute(qry):
+        ctr = ctr+1
+    print('demo table has', ctr, 'distint values of y')
+    qry = 'SELECT COUNT(DISTINCT demo.x) FROM demo GROUP BY demo.x;'
+    ctr = 0
+    for row in cur.execute(qry):
+        ctr = ctr+1
+    print('demo table has', ctr, 'distint values of x')
+    qry = 'SELECT COUNT(DISTINCT demo.s) FROM demo GROUP BY demo.s;'
+    ctr = 0
+    for row in cur.execute(qry):
+        ctr = ctr+1
+    print('demo table has', ctr, 'distint values of s')
+
     return
+
