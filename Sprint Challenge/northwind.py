abbd77393cd866b5781bad8e55471cd014e43f2e
hughjafro <cmtolbert1@gmail.com>
2019-02-22 12:07:48 -0800

    Added query outputs to northwind.py file

--- Sprint Challenge/northwind.py
+++ Sprint Challenge/northwind.py
@@ -19,10 +19,30 @@ FROM Product AS prod
 ORDER BY prod.UnitPrice DESC
 LIMIT 10;"""
 
+'''
+
+ProductName	UnitPrice
+0	Côte de Blaye	263.50
+1	Thüringer Rostbratwurst	123.79
+2	Mishi Kobe Niku	97.00
+3	Sir Rodney's Marmalade	81.00
+4	Carnarvon Tigers	62.50
+5	Raclette Courdavault	55.00
+6	Manjimup Dried Apples	53.00
+7	Tarte au sucre	49.30
+8	Ipoh Coffee	46.00
+9	Rössle Sauerkraut	45.60
+'''
+
 #What is the average age of an employee at the time of their hiring?
 avg_age_query = """
 SELECT AVG(HireDate - BirthDate) as avg_age
 FROM Employee;"""
+
+'''
+     avg_age
+0  37.222222
+'''
 
 # (*Stretch*) How does the average age of employee at hire vary by city?
 
@@ -37,6 +57,21 @@ JOIN Supplier AS sup ON sup.ID = prod.SupplierID
 ORDER BY prod.UnitPrice DESC
 LIMIT 10;"""
 
+'''
+                ProductName  UnitPrice                        CompanyName
+0            Côte de Blaye     263.50         Aux joyeux ecclésiastiques
+1  Thüringer Rostbratwurst     123.79  Plutzer Lebensmittelgroßmärkte AG
+2          Mishi Kobe Niku      97.00                      Tokyo Traders
+3   Sir Rodney's Marmalade      81.00           Specialty Biscuits, Ltd.
+4         Carnarvon Tigers      62.50                      Pavlova, Ltd.
+5     Raclette Courdavault      55.00                       Gai pâturage
+6    Manjimup Dried Apples      53.00                        G'day, Mate
+7           Tarte au sucre      49.30                   Forêts d'érables
+8              Ipoh Coffee      46.00                       Leka Trading
+9        Rössle Sauerkraut      45.60  Plutzer Lebensmittelgroßmärkte AG
+'''
+
+
 # What is the largest category (by number of products in it)?
 largest_category_query = """
 SELECCT cat.CategoryName, COUNT(prod.CategoryID)
@@ -46,6 +81,11 @@ ON prod.CategoryID = cat.ID
 GROUP BY prod.CategoryID
 ORDER BY COUNT(prod.CategoryID) DESC
 LIMIT 1;"""
+
+'''
+CategoryName  COUNT(prod.CategoryID)
+0  Confections                      13 
+'''
 
 # (*Stretch*) What is the top territory (by number of employees),
 # andhow many employees does it have?
