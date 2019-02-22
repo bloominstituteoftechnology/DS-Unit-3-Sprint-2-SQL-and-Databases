3ea53a99acfe328356dedb8480956d28993c1d85
veritaem <dcharris776@gmail.com>
2019-02-22 13:38:21 -0500

    sprint stuff

--- Sprint stuff/northwind.py
+++ Sprint stuff/northwind.py
@@ -5,30 +5,42 @@ conn = sqlite3.connect("northwind_small.sqlite3")
 curs = conn.cursor()
 
 # Queries
-Q_1 = """SELECT Product.UnitPrice
+Q_1 = curs.execute("""SELECT Product.UnitPrice
 FROM Product
 ORDER BY Product.UnitPrice DESC
 LIMIT 10
-;"""
-Q_2 =  """select AVG(HireDate - BirthDate)
+;""")
+Q_2 =  curs.execute("""select AVG(HireDate - BirthDate)
 from Employee;
-"""
-Stretchg_1 = """"""
+""")
+Stretchg_1 = curs.execute("""select AVG(HireDate - BirthDate), City
+from Employee
+Group By City;
+""")
 
 # Other Part
 
-Q_3 = """SELECT Product.Id, Product.UnitPrice, Supplier.CompanyName
+Q_3 = curs.execute("""SELECT Product.Id, Product.UnitPrice, Supplier.CompanyName
 FROM Product
 JOIN Supplier
 ON Product.id
 ORDER BY Product.UnitPrice DESC
 LIMIT 10
-;"""
-Q_4 = """SELECT COUNT(DISTINCT Category.CategoryName), Category.CategoryName
+;""")
+Q_4 = curs.execute("""SELECT COUNT(DISTINCT Category.CategoryName), Category.CategoryName
 FROM Category
 JOIN Product
 ON Category.id
 ORDER BY COUNT(Category.CategoryName) DESC
 LIMIT 1
-;"""
-Stretchg_2 = """"""
+;""")
+Stretchg_2 = curs.execute("""SELECT COUNT(DISTINCT Employee.FirstName), Employee.LastName, Employee.FirstName
+FROM Employee
+JOIN EmployeeTerritory
+ON Employee.Id = EmployeeTerritory.EmployeeId
+JOIN Territory
+ON EmployeeTerritory.TerritoryId = Territory.Id
+ORDER BY COUNT(DISTINCT Employee.FirstName) desc
+LIMIT 1;""")
+
+conn.commit()
