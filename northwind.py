b9d02151e5c89515fca4e3ea7318d0f4b03a750f
Tony Tia <webmaster@tonytia.com>
2019-02-22 11:00:39 -0800

    Update correction on expensive item.

--- northwind.py
+++ northwind.py
@@ -8,8 +8,8 @@ curs.execute('select name from sqlite_master where type="table" order by name;')
 
 curs.execute('select sql from sqlite_master where type="table" order by name;').fetchall()
 
-curs.execute('select ProductName from Product order by UnitPrice limit 10').fetchall()
-[('Geitost',), ('Guaraná Fantástica',), ('Konbu',), ('Filo Mix',), ('Tourtière',), ('Rhönbräu Klosterbier',), ('Tunnbröd',), ('Teatime Chocolate Biscuits',), ('Rogede sild',), ('Zaanse koeken',)]
+curs.execute('select ProductName,UnitPrice from Product order by UnitPrice desc limit 10').fetchall()
+[('Côte de Blaye', 263.5), ('Thüringer Rostbratwurst', 123.79), ('Mishi Kobe Niku', 97), ("Sir Rodney's Marmalade", 81), ('Carnarvon Tigers', 62.5), ('Raclette Courdavault', 55), ('Manjimup Dried Apples', 53), ('Tarte au sucre', 49.3), ('Ipoh Coffee', 46), ('Rössle Sauerkraut', 45.6)]
 
 curs.execute('SELECT firstname, lastname, AVG(hiredate - birthdate) FROM employee').fetchall()
 [('Nancy', 'Davolio', 37.22222222222222)]
@@ -19,4 +19,5 @@ curs.execute('SELECT firstname, lastname, AVG(hiredate - birthdate) FROM employe
 
 
 
-
+ curs.execute('select territoryDescription, count(employeeid) from territory, employeeterritory where territory.id = employeeterritory.territoryid  group by employeeid order by count(employeeid) limit 1').fetchall()
+[('Wilton', 2)]
