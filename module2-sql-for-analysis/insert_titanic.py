b5f26f43fe99b93b2884da0ce813f4467fbb506d
joshdsolis <39255273+joshdsolis@users.noreply.github.com>
2019-02-25 10:50:20 -0600

    Add files via upload

--- module2-sql-for-analysis/insert_titanic.py
+++ module2-sql-for-analysis/insert_titanic.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env/ python
 
-
-
 import sqlite3
 import psycopg2 as pg
 import csv
+import pandas as pd
 
+
+# Loading up the CSV in order to edit some of the troublesome names
+# Namely eliminating apostrophes
+
+def f(x):
+    if isinstance(x, str):
+        return (#x.replace(' ', '_')
+               # .replace('/', '_')
+                #.replace(')','')
+               # .replace('(','')
+                x.replace("'", ''))
+                #.replace(".", ""))
+    else:
+        return x
+df = pd.read_csv('titanic.csv').rename(columns=f).applymap(f)
+df.to_csv("titanic_edit.csv", index=False)
 sl_conn = sqlite3.connect('../joshdsolis/titanic.db')
+
+
 curs = sl_conn.cursor()
 curs.execute("""CREATE TABLE t (
     Survived int, 
@@ -20,7 +37,7 @@ curs.execute("""CREATE TABLE t (
 );""")
 
 
-with open('titanic.csv', 'rt') as fin:
+with open('titanic_edit.csv', 'rt') as fin:
     dr = csv.DictReader(fin)
     to_db = [(i['Survived'], i['Pclass'], i['Name'], i['Sex'],
     i['Age'], i['Siblings/Spouses Aboard'], i['Parents/Children Aboard'],
@@ -40,10 +57,10 @@ sl_conn.commit()
 sl_conn.close()
 
 
-dbname = 'TODO'
-user ='TODO'
-password='TODO'
-host = 'TODO'
+dbname = 'hplkjeta'
+user ='hplkjeta'
+password='oZIHUw7MLlfv7STG_kigAzstwcYwUE2z'
+host = 'stampy.db.elephantsql.com'
 
  
 pg_conn = pg.connect(dbname=dbname, user=user,
@@ -56,7 +73,7 @@ results = sl_conn.execute('SELECT * FROM t;').fetchall()
 def make_and_populate_character_table():
     pg_curs = pg_conn.cursor()
 
-    pg_curs.execute("""CREATE TABLE titanic (
+    pg_curs.execute("""CREATE TABLE titanic2 (
     Survived int, 
     Pclass int, 
     Name varchar(500), 
@@ -68,9 +85,9 @@ def make_and_populate_character_table():
     );""")
 
     for result in results:
-        insert_result = """INSERT INTO titanic
+        insert_result = """INSERT INTO titanic2
         (Survived, Pclass, Name, Sex, Age, Siblings_Spouses_Aboard, Parents_Children_Aboard, Fare)
         VALUES""" + str(result)
-    
         pg_curs.execute(insert_result)
+    
     pg_conn.commit()
