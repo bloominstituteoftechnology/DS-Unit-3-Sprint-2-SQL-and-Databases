49fdc93e6e773c704641bfbbe715f00f4d60d8fc
trista-paul <44633004+trista-paul@users.noreply.github.com>
2019-03-25 16:15:14 -0700

    progress by 4:15 pm

---
+++ module1-introduction-to-sql/rpg_queries.py
@@ -0,0 +1,63 @@
+import sqlite3
+
+'''import rpg_db and make cursor object'''
+rpgdb = sqlite3.connect('rpg_db.sqlite3')
+cursor = rpgdb.cursor()
+
+'''how many characters are in the game?'''
+problem1 = 'SELECT COUNT(*) FROM charactercreator_character;'
+cursor.execute(problem1).fetchall()
+
+
+'''how many characters of each class are in the game?'''
+problem2_cleric = 'SELECT COUNT(*) FROM charactercreator_cleric;'
+cursor.execute(problem2_cleric).fetchall()
+
+problem2_fighter = 'SELECT COUNT(*) FROM charactercreator_fighter;'
+cursor.execute(problem2_fighter).fetchall()
+
+problem2_mage = 'SELECT COUNT(*) FROM charactercreator_mage;'
+cursor.execute(problem2_mage).fetchall()
+
+problem2_thief = 'SELECT COUNT(*) FROM charactercreator_thief;'
+cursor.execute(problem2_thief).fetchall()
+
+problem2_necromancer = 'SELECT COUNT(*) FROM charactercreator_necromancer;'
+cursor.execute(problem2_necromancer).fetchall()
+
+
+'''how many items are in the game?'''
+problem3 = 'SELECT COUNT(*) FROM armory_item;'
+cursor.execute(problem3).fetchall()
+
+
+'''how many weapons are in the game?
+    how many non-weapon items?'''
+problem4_weapons = 'SELECT COUNT(*) FROM armory_weapon;'
+cursor.execute(problem4_weapons).fetchall()
+
+problem4_items = 'SELECT COUNT(*)
+                      FROM armory_item
+                      WHERE armory_item.item_id not in (
+                      SELECT item_ptr_id FROM armory_weapon);'
+cursor.execute(problem4_items).fetchall()
+
+'''how many items do the first twenty characters with items have?'''
+problem5 = 'SELECT character_id, COUNT(character_id) AS FREQUENCY
+                FROM charactercreator_character_inventory
+                GROUP BY character_id
+                LIMIT 20;'
+cursor.execute(problem5).fetchall()
+
+
+'''how many weapons do the first twenty characters with weapons have?'''
+problem6 = 'SELECT charactercreator_character_inventory.character_id,
+                COUNT(charactercreator_character_inventory.character_id) AS FREQUENCY
+                FROM charactercreator_character_inventory, armory_weapon
+                WHERE charactercreator_character_inventory.item_id = armory_weapon.item_ptr_id
+                GROUP BY character_id
+                LIMIT 20;'
+ cursor.execute(problem6).fetchall()
+
+
+
