dcfca024204c8220229c2055aa89ca3d7f2d260d
Cole Hudson <cole@colejhudson.com>
2019-03-29 08:53:19 -0700

    fixed incorrect usage of northwest db

--- sprint-challenge/demo_data.py
+++ sprint-challenge/demo_data.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+DB = "tiny"
 TABLE = "part_one"
 DATA = [
     ('g', 3, 9),
@@ -8,26 +9,26 @@ DATA = [
 ]
 
 # Connect to the database
-northwind = sqlite3.connect('../db/northwind_small.sqlite3')
+tiny = sqlite3.connect('../db/{}.sqlite3'.format(DB))
 
 # Remove the table 
-northwind.execute("drop table if exists {}".format(TABLE))
+tiny.execute("drop table if exists {}".format(TABLE))
 
 # Create the table 
-northwind.execute("create table {}(s varchar(1), x int, y int)".format(TABLE))
+tiny.execute("create table {}(s varchar(1), x int, y int)".format(TABLE))
 
 # Insert the data
-northwind.executemany('insert into {} values (?, ?, ?)'.format(TABLE), DATA)
+tiny.executemany('insert into {} values (?, ?, ?)'.format(TABLE), DATA)
 
 # Save changes
-northwind.commit()
+tiny.commit()
 
 print("Q:", "Count how many rows you have - it should be 3!")
-print("A:", northwind.execute("select count(*) from {}".format(TABLE)).fetchall()[0])
+print("A:", tiny.execute("select count(*) from {}".format(TABLE)).fetchall()[0])
 
 print("Q:", "How many rows are there where both x and y are at least 5?")
-print("A:", northwind.execute("select count(*) from {} where x >= 5 and y >= 5".format(TABLE)).fetchall()[0])
+print("A:", tiny.execute("select count(*) from {} where x >= 5 and y >= 5".format(TABLE)).fetchall()[0])
 
 print("Q:", "How many unique values of y are there?")
-print("A:", northwind.execute("select count(distinct  y) from {}".format(TABLE)).fetchall()[0])
+print("A:", tiny.execute("select count(distinct  y) from {}".format(TABLE)).fetchall()[0])
 
