6eb2b83280e95353f490314cc417258cdea36cbf
xyz <dkav@live.com>
2019-03-29 16:27:48 -0400

    PEP8d, commented, and functioned code

--- demo_data.py
+++ demo_data.py
@@ -1,31 +1,32 @@
 import sqlite3
 
-conn = sqlite3.connect('demo_data.sqlite3')
-curs = conn.cursor()
 
-curs.execute("""
-    CREATE TABLE demo(
-    s varhar(2),
-    x smallint,
-    y smallint)
-""")
-conn.commit()
+# Function that connects to a db and executes an sql statement in the sql param
+def execute_sql(sql, db_name):
+    conn = sqlite3.connect(db_name)
+    curs = conn.cursor()
+    return [curs.execute(sql).fetchall(), conn.commit()][0]
 
-curs.execute("""
-    INSERT INTO demo
-        (s, x, y)
-    VALUES
-        ('g', 3, 9),
-        ('v', 5, 7),
-        ('f', 8, 7);
-""")
-conn.commit()
+db = "demo_data.sqlite3"
+
+sql = "CREATE TABLE demo(s varhar(2), x smallint, y smallint);"
+
+execute_sql(sql, db)
+
+sql = ("""INSERT INTO demo (s, x, y)
+          VALUES ('g', 3, 9), ('v', 5, 7), ('f', 8, 7);""")
+
+execute_sql(sql, db)
 
 # Number of rows
-print(curs.execute('SELECT COUNT(*) FROM demo;').fetchall())
+sql = "SELECT COUNT(*) FROM demo;"
+print('Total number of rows:', execute_sql(sql, db))
 
 # Number of rows where both x and y are at least 5
-print(curs.execute('SELECT COUNT(*) FROM demo WHERE x >= 5 and y >= 5;').fetchall())
+sql = "SELECT COUNT(*) FROM demo WHERE x >= 5 and y >= 5;"
+print('Number of rows where bith x and y are at least 5:',
+      execute_sql(sql, db))
 
 # Unique values of y
-print(curs.execute('SELECT COUNT(DISTINCT y) from demo').fetchall())
+sql = "SELECT COUNT(DISTINCT y) from demo"
+print('Unique values of y:', execute_sql(sql, db))
--- northwind.py
+++ northwind.py
@@ -1,45 +1,52 @@
 import sqlite3
 
-conn = sqlite3.connect('northwind_small.sqlite3')
-curs = conn.cursor()
 
 # Test the connection, get names for all tables
-print('Connection is working, here are the tables names:',
-      curs.execute("""SELECT name FROM sqlite_master WHERE type='table' ORDER BY
-      name;""").fetchall())
+q1 = "SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;"
 
 # The ten most expensive items per unit price
-q = """SELECT ProductName, UnitPrice FROM Product
-       ORDER BY UnitPrice DESC LIMIT 10"""
-
-print('Ten most expensive items:', curs.execute(q).fetchall())
+q2 = """SELECT ProductName, UnitPrice FROM Product
+        ORDER BY UnitPrice DESC LIMIT 10"""
 
 # Average age of an employee at the time of their hiring
-q = """SELECT AVG(substr(HireDate, 1, 4) - substr(BirthDate, 1, 4))
+q3 = """SELECT AVG(substr(HireDate, 1, 4) - substr(BirthDate, 1, 4))
         FROM Employee"""
 
-print('Average age of employee at time of hiring:', curs.execute(q).fetchall())
-
 # Stretch: how does the average age of an employee at hire vary by city?
+q4 = """SELECT City, AVG(substr(HireDate, 1, 4) - substr(BirthDate, 1, 4))
+        FROM Employee GROUP BY City"""
 
 # What are the ten most expensive items in the db and their suppliers
-q = """SELECT ProductName, UnitPrice, CompanyName FROM Product
-    JOIN supplier
-    ON Product.supplierId = Supplier.id
-    ORDER BY UnitPrice DESC LIMIT 10;"""
-
-print('Ten most expensive items and the comapies that supply them:',
-      curs.execute(q).fetchall())
+q5 = """SELECT ProductName, UnitPrice, CompanyName FROM Product
+        JOIN supplier
+        ON Product.supplierId = Supplier.id
+        ORDER BY UnitPrice DESC LIMIT 10;"""
 
 # What is the largest category by number of unique products?
-q = """SELECT CategoryName, COUNT(*)
-       FROM Product JOIN Category
-       ON Product.CategoryId = Category.Id
-       GROUP BY CategoryName
-       ORDER BY COUNT(*) DESC"""
-
-print('Number of unique products per category:', curs.execute(q).fetchall())
+q6 = """SELECT CategoryName, COUNT(*)
+        FROM Product JOIN Category
+        ON Product.CategoryId = Category.Id
+        GROUP BY CategoryName
+        ORDER BY COUNT(*) DESC LIMIT 1"""
 
 # Stretch: What is the name of the employee with the most territories?
 # Use `TerritoryId`(not name, region, or other fields) as the unique identifier
 # for territories.
+q7 = """SELECT FirstName, LastName, COUNT(*)
+        FROM Employee JOIN EmployeeTerritory
+        ON Employee.Id = EmployeeTerritory.EmployeeId
+        GROUP BY FirstName, LastName
+        ORDER BY COUNT(*) DESC LIMIT 1"""
+
+queries = [q1, q2, q3, q4, q5, q6, q7]
+
+
+def execute_sql(queries, db_name):
+    """Instantiate connection and cursor objects
+       and execute SQL queries from queries list"""
+    conn = sqlite3.connect(db_name)
+    curs = conn.cursor()
+    for q in queries:
+        print('\n', curs.execute(q).fetchall())
+
+execute_sql(queries, 'northwind_small.sqlite3')
