8f269088bacee3ff1afbeec0df2bec8b54d97017
trista-paul <44633004+trista-paul@users.noreply.github.com>
2019-03-29 10:51:56 -0700

    Reformatted

--- northwind.py
+++ northwind.py
@@ -10,31 +10,30 @@ cursor = northwind.cursor()
 
 '''what are the ten most expensive items per unit price?'''
 problem1 = """SELECT ProductName FROM PRODUCT
-              ORDER BY UnitPrice DESC
-              LIMIT 10;"""
+                  ORDER BY UnitPrice DESC
+                  LIMIT 10;"""
 print(cursor.execute(problem1).fetchall())
 
 
 '''What is the average hiring age?'''
 problem2 = """SELECT AVG(strftime('%Y', HireDate) - strftime('%Y', BirthDate))
-              FROM Employee;"""
+                  FROM Employee;"""
 print(cursor.execute(problem2).fetchall()
 
 
-'''what are the ten most expensive items per unit price
-   BY supplier? '''
+'''what are the ten most expensive items per unit price BY supplier? '''
 problem3 = """SELECT ProductName, CompanyName
-              FROM Product, Supplier
-              WHERE Product.SupplierId = Supplier.Id
-              ORDER BY UnitPrice DESC
-              LIMIT 10;"""
+                  FROM Product, Supplier
+                  WHERE Product.SupplierId = Supplier.Id
+                  ORDER BY UnitPrice DESC
+                  LIMIT 10;"""
 print(cursor.execute(problem3).fetchall())
 
 
 '''what is the largest category by num of unique products?'''
 problem4 = """SELECT COUNT(*)
-              FROM Category, Product
-              WHERE Category.Id = Product.CategoryId
-              GROUP BY CategoryName
-              LIMIT 1;"""
+                  FROM Category, Product
+                  WHERE Category.Id = Product.CategoryId
+                  GROUP BY CategoryName
+                  LIMIT 1;"""
 print(cursor.execute(problem4).fetchall())
