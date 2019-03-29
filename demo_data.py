b0041890f2ca33857fcb153dfecbfde212c5cf10
ssingh1187 <sshilpa1187@users.noreply.github.com>
2019-03-29 08:52:40 -0700

    Sprint Challenge

---
+++ demo_data.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+conn = sqlite3.connect('demo_data.sqlite3')
+curs = conn.cursor()
+
+
+query2 = """
+INSERT INTO demo
+(s, x, y)
+VALUES ('g', 3, 9), ('v', 5, 7), ('f', 8, 7);
+"""
+print(curs.execute(query2).fetchall())
+
+print('Total number of rows:')
+query3 = """
+SELECT COUNT(*)
+FROM demo;
+"""
+print(curs.execute(query3).fetchall())
+
+print('How many rows are there where both x and y are at least 5?')
+query4 = """
+SELECT COUNT(*)
+FROM demo
+WHERE x >= 5 and y >= 5;
+"""
+print(curs.execute(query4).fetchall())
+
+print('How many unique values of y are there?')
+query5 = """
+SELECT COUNT (DISTINCT y)
+FROM demo
+"""
+print(curs.execute(query5).fetchall())
