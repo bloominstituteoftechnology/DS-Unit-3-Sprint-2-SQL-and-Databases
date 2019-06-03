3509616a542728e5f186f99fa1407e27a64208dd
Matt Kirby <31753757+mkirby1995@users.noreply.github.com>
2019-06-03 18:05:24 -0500

    Add files via upload

---
+++ module1-introduction-to-sql/rpg_queries.py
@@ -0,0 +1,59 @@
+import sqlite3
+conn = sqlite3.connect('rpg_db.sqlite3')
+curs = conn.cursor()
+print('Assignment_1')
+
+
+# 1. How many total Characters are there?
+char_query = 'SELECT COUNT(*) FROM charactercreator_character;'
+char_result = curs.execute(char_query)
+# Answer
+print('1. There are', char_result.fetchall()[0][0], 'total characters')
+
+
+# 2. How many of each specific subclass?
+# Mages
+mage_query = 'SELECT COUNT(*) FROM charactercreator_mage;'
+mage_result = curs.execute(mage_query)
+mages = mage_result.fetchall()[0][0]
+# Thiefs
+thief_query = 'SELECT COUNT(*) FROM charactercreator_thief;'
+thief_result = curs.execute(thief_query)
+thiefs= thief_result.fetchall()[0][0]
+# Clerics
+cleric_query = 'SELECT COUNT(*) FROM charactercreator_cleric;'
+cleric_result = curs.execute(cleric_query)
+clerics = cleric_result.fetchall()[0][0]
+# Fighters
+fighter_query = 'SELECT COUNT(*) FROM charactercreator_fighter;'
+fighter_result = curs.execute(fighter_query)
+fighters = fighter_result.fetchall()[0][0]
+# Answer
+print('2. There are',mages,'mages',thiefs,'thiefs',clerics,'clerics','and',
+      fighters,'fighters')
+
+
+# 3. How many total Items?
+item_query = 'SELECT COUNT(*) FROM armory_item;'
+item_result = curs.execute(item_query)
+items = item_result.fetchall()[0][0]
+# Answer
+print('3. There are', items, 'total items')
+
+
+# 4. How many of the Items are weapons? How many are not?
+weapon_query = 'SELECT COUNT(*) FROM armory_weapon;'
+weapon_result = curs.execute(weapon_query)
+weapons = weapon_result.fetchall()[0][0]
+nonweapons = items - weapons
+# Answer
+print('4. There are',weapons,'weapons','and',nonweapons,'nonweapns')
+
+
+# 5. How many Items does each character have? (Return first 20 rows)
+char_item_query = "SELECT character_id, COUNT(*) AS `num` FROM charactercreator_character_inventory GROUP BY character_id"
+char_item_result = curs.execute(char_item_query)
+print('5. Character item counts\n',char_item_result.fetchall()[:20])
+
+
+# 6. How many Weapons does each character have? (Return first 20 rows)
