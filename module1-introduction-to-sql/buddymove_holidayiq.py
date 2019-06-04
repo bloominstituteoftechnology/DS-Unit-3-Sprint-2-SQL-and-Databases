4ac0fe8ca83ed5e1a6ed2340edb3cdbf371baad5
Liv Johnson <livjab@gmail.com>
2019-06-04 10:34:59 -0500

    answered questions using sql

--- module1-introduction-to-sql/buddymove_holidayiq.py
+++ module1-introduction-to-sql/buddymove_holidayiq.py
@@ -4,8 +4,28 @@ import sqlite3
 df = pd.read_csv("buddymove_holidayiq.csv")
 
 conn = sqlite3.connect("buddymove_holidayiq.sqlite3")
-df.to_sql(name='df', con=conn)
 
-query = 'SELECT COUNT(*) FROM df'
-results = conn.execute(query)
-print(results.fetchall())
+df.to_sql('df', conn, if_exists="replace")
+
+"""
+Count how many rows you have - it should be 249!
+"""
+print(pd.read_sql_query('SELECT COUNT(*) FROM df;', conn))
+
+
+"""
+How many users who reviewed at least 100 Nature in the category also reviewed
+at least 100 in the Shopping category?
+"""
+print(pd.read_sql_query('SELECT COUNT(*) FROM df WHERE Nature>=100 AND Shopping >=100;', conn))
+
+
+"""
+What are the average number of reviews for each category?
+"""
+print(pd.read_sql_query('SELECT AVG(Sports) FROM df;', conn))
+print(pd.read_sql_query('SELECT AVG(Religious) FROM df;', conn))
+print(pd.read_sql_query('SELECT AVG(Nature) FROM df;', conn))
+print(pd.read_sql_query('SELECT AVG(Theatre) FROM df;', conn))
+print(pd.read_sql_query('SELECT AVG(Shopping) FROM df;', conn))
+print(pd.read_sql_query('SELECT AVG(Picnic) FROM df;', conn))
