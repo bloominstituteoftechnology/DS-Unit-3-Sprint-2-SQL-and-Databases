d4cbb48c34870a949c5eab41ec45d9f583756b3a
Rice_from_data <nedderlander@gmail.com>
2019-06-03 17:02:27 -0700

    done basic assignment no stretch

---
+++ module1-introduction-to-sql/buddymove_holidayiq.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import sqlite3
+
+# load df
+df = pd.read_csv('buddymove_holidayiq.csv')
+
+#connect to blank database
+with sqlite3.connect('buddymove_holidayiq.sqlite3') as conn:
+    df.to_sql('buddymove_holidayiq', con=conn, index=False, if_exists='replace')
+
+
+#create our connection object
+curs = conn.cursor()
+
+#query db for # of rows
+query = """select count('User ID')
+from buddymove_holidayiq;
+"""
+result = curs.execute(query)
+output = result.fetchall().pop()[0]
+
+print('SQL row length is', output)
+
+#query db for descriminating user reviews
+query = """select count('User Id')
+from buddymove_holidayiq
+where Nature >=100 and Shopping >= 100;
+"""
+result = curs.execute(query)
+output = result.fetchall().pop()[0]
+
+print('%d users reviewed at least 100 in Nature and also reviewed at least 100 in Shopping' % output)
+
