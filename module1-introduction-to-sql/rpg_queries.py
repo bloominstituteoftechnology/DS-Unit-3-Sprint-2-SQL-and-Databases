9703b423e2afa88e956ee5ed4285051b05752d4e
standroidbeta <43893890+standroidbeta@users.noreply.github.com>
2019-06-06 19:09:22 -0400

    Updated the rpg_queries.py with more queries.

--- module1-introduction-to-sql/rpg_queries.py
+++ module1-introduction-to-sql/rpg_queries.py
@@ -17,13 +17,37 @@ def character_count():
 
 
 def sub_characters_count():
-    """Queries and prints total number of characters."""
+    """Queries and prints total number of sub-characters."""
     conn = sqlite3.connect('rpg_db.sqlite3')
     curs = conn.cursor()
-    query = 'SELECT COUNT (name) FROM charactercreator_character;'
+    query = 'SELECT COUNT (*) AS Category_Count, "Total Clerics" ' \
+            'AS Description FROM charactercreator_cleric ' \
+            'UNION ALL ' \
+            'SELECT COUNT (*) AS Category_Count, "Total Fighters" ' \
+            'AS Description FROM charactercreator_fighter ' \
+            'UNION ALL ' \
+            'SELECT COUNT (*) AS Category_Count, "Total_Mages" ' \
+            'AS Description FROM charactercreator_mage ' \
+            'UNION ALL ' \
+            'SELECT COUNT (*) AS Category_Count, "Total Necromancers" ' \
+            'AS Description FROM charactercreator_necromancer ' \
+            'UNION ALL ' \
+            'SELECT COUNT (*) AS Category_Count, "Total Thieves" ' \
+            'AS Description FROM charactercreator_thief;'
     result = curs.execute(query)
     total_subs = result.fetchall()
 
     return print('Sub-Character Totals:', total_subs)
 
 
+def total_items_count():
+    """Queries and prints the total items."""
+    conn = sqlite3.connect('rpg_db.sqlite3')
+    curs = conn.cursor()
+    query = 'SELECT COUNT (*) FROM charactercreator_character_inventory;'
+    result = curs.execute(query)
+    total_items = result.fetchall()
+
+    return print('Total Items:', total_items[0][0])
+
+def
---
+++ module2-sql-for-analysis/insert_titanic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import sqlite3
+import psycopg2
+
+
+pg_conn = psycopg2.connect()
