44f27dc716d6f755cc5a599e484c324170bb484f
JLDaniel77 <JLDaniel77@users.noreply.github.com>
2019-06-06 10:17:09 -0400

    Completed insert_titanic.py assignment

--- module2-sql-for-analysis/insert_titanic.py
+++ module2-sql-for-analysis/insert_titanic.py
@@ -1,30 +1,31 @@
 import psycopg2
-import sqlite3
 import pandas as pd
 
 # Import titanic data into DataFrame
 df = pd.read_csv('titanic.csv')
 
-# Cleaning up column names
+# Data cleaning
 df.columns = df.columns.str.replace('/', '_')
 df.columns = df.columns.str.replace(' ', '_')
 df.columns = df.columns.str.lower()
-df = df.rename(columns={'siblings_spouses_aboard': 'siblings_spouses', 'parents_children_aboard': 'parents_children'})
+df = df.rename(columns={'siblings_spouses_aboard': 'siblings_spouses',
+                        'parents_children_aboard': 'parents_children'})
+df['name'] = df['name'].replace("'", '', regex=True)
+df['name'] = df['name'].str.replace('\'', '')
 
-# Convert DataFrame to sqlite3 database
-sl_conn = sqlite3.connect('Titanic.sqlite3')
-sl_curs = sl_conn.cursor()
-df.to_sql('Titanic', con=sl_conn)
+# Convert DataFrame to list of lists
+titanic = df.values.tolist()
+
 
 # Postgresql login credentials
 dbname = 'xgbbacwp'
 user = 'xgbbacwp'
-password = 'mJroxQrHR5UAZ0hTGw0DlCg0X8FQTKe_'
+password = ''
 host = 'raja.db.elephantsql.com'
 
 # Create a connection
 pg_conn = psycopg2.connect(dbname=dbname, user=user,
-                        password=password, host=host)
+                           password=password, host=host)
 pg_curs = pg_conn.cursor()
 
 # Add Titanic table to postgresql database
@@ -38,21 +39,20 @@ CREATE TABLE Titanic (
     age INT,
     siblings_spouses INT,
     parents_children INT,
-    fare FLOAT(2)
+    fare MONEY
 )
 """
 pg_curs.execute(create_titanic_table)
 
-# Select passenger data
-passengers = sl_curs.execute('SELECT * FROM Titanic;').fetchall()
 
 # Add passenger data to Titanic table
-for passenger in passengers:
+for passenger in titanic:
     insert = '''INSERT INTO Titanic(
-        passenger_id, survived, pclass, name, sex, age, siblings_spouses, 
+        survived, pclass, name, sex, age, siblings_spouses, 
         parents_children, fare) VALUES''' + str(tuple(passenger))
     pg_curs.execute(insert)
 
+# Commit changes
 pg_conn.commit()
 
 # Create new cursor and inspect data
