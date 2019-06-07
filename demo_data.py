202d3225ae3b954f0bbd20370833c0df33137605
Tom Higgins <higgins2718@outlook.com>
2019-06-07 11:36:49 -0700

    Fixed pep8

--- demo_data.py
+++ demo_data.py
@@ -2,16 +2,27 @@ import sqlite3
 conn = sqlite3.connect('demo_data.sqlite3')
 curs = conn.cursor()
 print(curs)
+
+"""
+Create table mytable
+"""
 command = "CREATE TABLE IF NOT EXISTS mytable (" \
         "s text PRIMARY KEY NOT NULL, x INTEGER NOT NULL, y INTEGER NOT NULL);"
 result = curs.execute(command)
+
+"""
+Populate table mytable
+"""
 command2 = "INSERT INTO mytable (s, x, y)" \
           "VALUES ('g', 3, 9)," \
           "('v', 5, 7)," \
           "('f', 8, 7);"
 result2 = curs.execute(command2)
 conn.commit()
+
+
 def select_all_tasks(conn):
+
     """
     Query all rows in the tasks table
     :param conn: the Connection object
