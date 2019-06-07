b310b750b4887d6790bc63c9cec4c5483996a162
danhorsley <34445739+danhorsley@users.noreply.github.com>
2019-06-07 17:20:29 +0100

    Update northwind.py
    updating last bit

--- northwind.py
+++ northwind.py
@@ -20,3 +20,31 @@ for row in curs.execute("""SELECT City,AVG(HireDate-BirthDate) as age FROM Emplo
                 
         """).fetchall():
                         print('City : ',row[0], '-------average age ',row[1],' years old')
+print('The top ten most expensive products and their suppliers are:')
+for row in curs.execute("""SELECT ProductName, UnitPrice, CompanyName 
+                FROM Product JOIN Supplier
+                ON Product.supplierID = Supplier.ID
+                ORDER BY UnitPrice DESC
+                LIMIT 10
+""").fetchall():
+    print('Product :',row[0],' -- Price $:',row[1],' -- Supplier :',row[2])
+print('the largest category by number of unique products in it is:')
+print(curs.execute("""SELECT Description, Count(DISTINCT(Product.ID)) as ct
+                FROM Product JOIN Category
+                ON Product.CategoryId = Category.ID
+                GROUP BY Description
+                ORDER BY ct DESC
+                LIMIT 1
+                
+""").fetchall()[0])
+print('the employee with the most territories is :')
+for row in (curs.execute("""SELECT Employee.FirstName, Employee.LastName,
+                Count(DISTINCT(TerritoryId)) as counter 
+                FROM Employee JOIN EmployeeTerritory
+                ON Employee.ID = EmployeeTerritory.EmployeeID
+                GROUP BY Employee.ID, Employee.FirstName, Employee.LastName
+                ORDER BY counter DESC
+                LIMIT 1
+                
+""").fetchall()):
+    print('is ',row[0],row[1],'with',row[2],'territories')
