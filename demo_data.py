9ece6d3616642314b8919293341b70717f7b2bd9
Patricia Bradshaw <46237708+tbradshaw91@users.noreply.github.com>
2019-06-07 08:31:45 -0700

    Added Returns

--- demo_data.py
+++ demo_data.py
@@ -30,12 +30,14 @@ conn.commit()
 # Counting how many rows; I should have 3
 query1 = 'SELECT count(*) FROM demo;'
 print ('Number of Rows:',conn.cursor().execute(query1).fetchone()[0], '\n')
+## Number of Rows: 3
 
 # How many rows where both x and y are at least 5?
 query2 = 'SELECT count(s) FROM demo WHERE x >= 5 AND y >= 5;'
 print ('Number of Rows Where X and Y Are At Least 5:',conn.cursor().execute(query2).fetchone()[0], '\n')
+## Number of Rows Where X and Y Are At Least 5: 2 
 
 # Counting unique values of y are there?
 query3 = 'SELECT COUNT(DISTINCT y) FROM demo;'
 print ('Number of Unique Y Values:',conn.cursor().execute(query3).fetchone()[0], '\n')
-
+## Number of Unique Y Values: 2 
