e95eff2a5602a90218e4078b477e2fe479771ec5
Tom Higgins <higgins2718@outlook.com>
2019-06-07 11:49:49 -0700

    Fixed bug

--- northwind.py
+++ northwind.py
@@ -3,7 +3,7 @@ conn = sqlite3.connect('northwind_small.sqlite3')
 curs = conn.cursor()
 
 
-print(curs.execute("SELECT * FROM Product, OrderDetail WHERE "
+print(curs.execute("SELECT DISTINCT(Product.ProductName) FROM Product, OrderDetail WHERE "
                    "Product.ID = OrderDetail.ProductID ORDER BY "
                    "OrderDetail.UnitPrice DESC LIMIT 10;").fetchall())
 
@@ -44,15 +44,23 @@ print(curs.execute("SELECT AVG(Employee.HireDate - Employee.BirthDate)"
 [(37.22222222222222,)]
 
 """
+print("Most expensive products and their suppliers")
 print(curs.execute("SELECT * FROM Product, Supplier, OrderDetail "
                    "WHERE Product.ID = OrderDetail.ProductID AND "
                    "Product.SupplierID = Supplier.ID ORDER BY "
                    "OrderDetail.UnitPrice DESC LIMIT 10;").fetchall())
 
-
+"""
+[(38, 'Côte de Blaye', 18, 1, '12 - 75 cl bottles', 263.5, 17, 0, 15, 0, 18,
+ 'Aux joyeux ecclésiastiques',
+ 'Guylène Nodier', 'Sales Manager', '203, Rue des Francs-Bourgeois', 'Paris', 'Western Europe', '75004',
+  'France', '(1) 03.83.00.68', '(1) 03.83.00.62', None, '10518/38', 10518, 38, 263.5, 15, 0.0),
+   ............
+"""
 """
 Most popular category
 """
+print("Most popular category")
 print(curs.execute("SELECT Category.CategoryName FROM Product, Category WHERE "
                    "Product.CategoryID = Category.ID GROUP BY "
                    "Category.CategoryName ORDER BY count(*) "
