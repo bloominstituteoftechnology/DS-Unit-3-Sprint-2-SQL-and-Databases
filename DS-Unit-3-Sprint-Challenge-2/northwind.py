c97e1670f48a09e0e867846149eda6e96d407f84
standroidbeta <43893890+standroidbeta@users.noreply.github.com>
2019-06-07 17:36:05 -0400

    Part 3 done.

--- DS-Unit-3-Sprint-Challenge-2/northwind.py
+++ DS-Unit-3-Sprint-Challenge-2/northwind.py
@@ -31,3 +31,29 @@ def avg_hire_age():
     return print('Employee average age at hire date:', aha[0][0])
 
 
+def most_expensive_with_supplier():
+    """Finds the most expensive units per price."""
+    conn = sqlite3.connect('northwind_small.sqlite3')
+    curs = conn.cursor()
+    query = 'SELECT ProductName, CompanyName, UnitPrice FROM Product ' \
+            'INNER JOIN Supplier ' \
+            'ON Product.SupplierID = Supplier.Id ' \
+            'ORDER BY UnitPrice DESC ' \
+            'LIMIT 10;'
+    result = curs.execute(query)
+    top10_ws = result.fetchall()
+
+    return print('10 Most Expensive Product with Suppliers:', top10_ws)
+
+
+def top_unique_category():
+    """Queries for the largest unique
+     category"""
+    conn = sqlite3.connect('northwind_small.sqlite3')
+    curs = conn.cursor()
+    query = 'SELECT CategoryName, MAX(DISTINCT Description) ' \
+            'FROM Category;'
+    result = curs.execute(query)
+    tulc = result.fetchall()
+
+    return print('Largest unique category:', tulc[0])
