ce5da87770a4a54d607dd256fcd6241f11cfc6d2
dustiny5 <dustiny5@users.noreply.github.com>
2019-06-07 10:15:13 -0700

    Update add print(#)

--- sprint2/demo_data.py
+++ sprint2/demo_data.py
@@ -27,11 +27,14 @@ curs.execute(insert)
 # Query - Count how many rows
 result_count = curs.execute('SELECT COUNT(*) FROM demo;').fetchone()
 print('There are:', result_count[0], 'rows')
+print('###################################')
 
 # Query - How many rows where both x and y are >= 5
 result_count_5 = curs.execute('SELECT COUNT(*) FROM demo WHERE x >=5 AND y >= 5;').fetchone()
 print('There are:', result_count_5[0], 'rows of x and y >= 5')
+print('###################################')
 
 # Query - How many unique values of y are there?
 result_unique_y = curs.execute('SELECT COUNT(DISTINCT y) FROM demo;').fetchone()
 print('There are:', result_unique_y[0], 'unique values of y')
+print('###################################')
