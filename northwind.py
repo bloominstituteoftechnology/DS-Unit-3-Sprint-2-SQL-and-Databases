3037706ceca70811c025fbca66d8b2fa820d1150
DylanGraves <33925282+DylanGraves@users.noreply.github.com>
2019-06-07 11:01:43 -0700

    Submit

---
+++ northwind.py
@@ -0,0 +1,42 @@
+ import sqlite3
+
+conn = sqlite3.connect('northwind_small.sqlite3')
+curs = conn.cursor()
+
+query1 = """
+SELECT ProductName, UnitPrice
+FROM Product
+ORDER BY UnitPrice DESC
+LIMIT 10
+"""
+
+print(curs.execute(query1).fetchall())
+conn.commit()
+
+query2 = """
+SELECT AVG(HireDate - BirthDate)
+FROM Employee
+"""
+
+# Part 3
+
+query3 = """
+SELECT Product.ProductName, Product.UnitPrice, Supplier.CompanyName
+FROM Product, Supplier
+WHERE Product.SupplierID = Supplier.ID
+ORDER BY UnitPrice DESC
+LIMIT 10
+"""
+
+print(curs.execute(query3).fetchall())
+conn.commit()
+
+query4 = """
+SELECT COUNT(CategoryId)
+FROM Product
+ORDER BY COUNT(CategoryId) DESC
+LIMIT 1
+"""
+
+print(curs.execute(query4).fetchall())
+conn.commit()
