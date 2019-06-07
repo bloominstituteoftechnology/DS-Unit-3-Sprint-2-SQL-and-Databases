ecc90dc6273aa2ea7efc59c0556598e47c52fc6e
Michael Brady <mikebrady44@gmail.com>
2019-06-07 13:52:34 -0400

    Final Sprint Challenge Submission

--- Sprint Challenge/demo_data.py
+++ Sprint Challenge/demo_data.py
@@ -1,52 +1,76 @@
 import sqlite3
 
-conn = sqlite3.connect('demo_date.sqlite3')
+# create and establish connection to database
+conn = sqlite3.connect('demo_data.sqlite3')
 curs = conn.cursor()
 
-query = """CREATE TABLE demo (
+
+# function to streamline query execution
+def execute_query(query):
+    curs.execute(query)
+
+# query to create table
+query = """CREATE TABLE IF NOT EXISTS demo (
     s charvar(1),
-    x int, 
+    x int,
     y int
 )"""
 
-curs.execute(query)
+execute_query(query)
 
+# Insert rows into table
 query = """INSERT INTO demo (s, x, y)
            VALUES ('g', 3, 9)"""
-curs.execute(query)
+execute_query(query)
 
 query = """INSERT INTO demo (s, x, y)
            VALUES ('v', 5, 7)"""
-curs.execute(query)
+execute_query(query)
 
 query = """INSERT INTO demo (s, x, y)
            VALUES ('f', 8, 7)"""
-curs.execute(query)
+execute_query(query)
 
+# Commit changes to finalize changes to database
 conn.commit()
+
+# Re-establish cursor
 curs = conn.cursor()
 
-query = """SELECT COUNT(*) FROM demo;"""
 
-result = curs.execute(query)
-result = result.fetchall()[0][0]
+# Function to execute exploratory query
+def explore_query(query):
+    result = curs.execute(query)
+    result = result.fetchall()[0][0]
+    return result
+
+# Determine the number of rows in the table
+query = """SELECT COUNT(*) FROM demo;"""
+result = explore_query(query)
 
 # Output: 'Query 1: The demo table has 3 rows'
-print(f'Query 1: The demo table has {result} rows')
+print('Query 1: The demo table has ' + str(result) + ' rows')
 
-query = """SELECT COUNT(*) 
+# Determine how many rows have x and y values greater than 5
+query = """SELECT COUNT(*)
            FROM demo
            WHERE x >= 5 AND y >= 5"""
-result = curs.execute(query)
-result = result.fetchall()[0][0]
+result = explore_query(query)
 
 # Output: 'Query 2: 2 rows have x and y values greater than or equal to 5'
-print(f'Query 2: {result} rows have x and y values greater than or equal to 5')
+print('Query 2: ' + str(result) + """ rows have x and y values greater than or
+        equal to 5""")
 
+# Determine how many unique values are in y
 query = """SELECT COUNT(DISTINCT(y))
            FROM demo"""
-result = curs.execute(query)
-result = result.fetchall()[0][0]
+result = explore_query(query)
 
 # Output: 'Query 3: Column y has 2 unique values'
-print(f'Query 3: Column y has {result} unique values')
+print('Query 3: Column y has ' + str(result) + ' unique values')
+
+# Condensed Output
+# Query 1: The demo table has 3 rows
+# Query 2: 2 rows have x and y values greater than or
+#         equal to 5
+# Query 3: Column y has 2 unique values
--- Sprint Challenge/northwind.py
+++ Sprint Challenge/northwind.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+# Establish connection to database
 conn = sqlite3.connect('northwind_small.sqlite3')
 curs = conn.cursor()
 
@@ -9,34 +10,48 @@ query = """SELECT ProductName
            ORDER BY UnitPrice DESC
            LIMIT 10;"""
 result = curs.execute(query)
-print(result.fetchall())
 
-# What is the average age of an employee at the time of their hiring? (Hint: a lot of arithmetic works with dates.)
-query = """SELECT AVG(HireDate - BirthDate) as 'Average Worker Age at Hire'
+print('The Ten Most Expensive Items (per unit price) are:')
+for item in result.fetchall():
+    print(item[0])
+print('\n')
+
+# What is the average age of an employee at the time of their hiring? (Hint: a
+# lot of arithmetic works with dates.)
+query = """SELECT ROUND(AVG(HireDate - BirthDate), 2) as 'Average Worker Age at Hire'
            FROM Employee"""
 result = curs.execute(query)
-print(result.fetchall())
+
+print('The average worker age at hire is:')
+print(result.fetchall()[0][0])
+print('\n')
 
 # How does the average age of employee at hire vary by city?
 query = """SELECT City, AVG(HireDate - BirthDate) as 'Average Worker Age at Hire'
            FROM Employee
            GROUP BY City"""
 result = curs.execute(query)
-print(result.fetchall())
+
+print('Variation in Worker Age at Hire by City:')
+for pair in result.fetchall():
+    print(pair[0] + " " + str(pair[1]) + " years")
+
+print('\n')
 
 # What are the ten most expensive items (per unit price) in the database and
 # their suppliers?
-query = """SELECT Product.ProductName, Supplier.CompanyName as 'Supplier Name'
-            FROM Product, Supplier
-            WHERE Product.ProductName IN (
-                    SELECT ProductName
-                    FROM Product
-                    ORDER BY UnitPrice DESC
-                    LIMIT 10
-                    )
-            ORDER BY Product.UnitPrice DESC"""
+query = """SELECT ProductName as 'Product Name', Supplier.CompanyName as 'Supplier Name'
+            FROM Product
+            JOIN Supplier ON Product.SupplierId = Supplier.Id
+            ORDER BY UnitPrice DESC
+            LIMIT 10;"""
 result = curs.execute(query)
-print(result.fetchall())
+
+print('The Ten Most Expensive Items (per unit price) and their suppliers are:')
+print('Product, Supplier')
+for item in result.fetchall():
+    print(item[0] + ', ' + item[1])
+print('\n')
 
 # What is the largest category (by number of unique products in it)?
 query = """SELECT category.CategoryName as 'Category w/ Most Unique Products'
@@ -46,7 +61,10 @@ query = """SELECT category.CategoryName as 'Category w/ Most Unique Products'
             ORDER BY COUNT(CategoryId) DESC
             LIMIT 1;"""
 result = curs.execute(query)
-print(result.fetchall())
+
+print('The largest category (by number of unique products in it) is:')
+print(result.fetchall()[0][0])
+print('\n')
 
 # Who's the employee with the most territories? Use TerritoryId (not name,
 # region, or other fields) as the unique identifier for territories.
@@ -57,4 +75,54 @@ query = """SELECT Employee.FirstName, Employee.LastName
             ORDER BY COUNT(TerritoryId) DESC
             LIMIT 1;"""
 result = curs.execute(query)
-print(result.fetchall())
+
+print('The employee with the most territories is:')
+result = result.fetchall()
+print(result[0][0] + ' ' + result[0][1])
+
+# Output
+# The Ten Most Expensive Items (per unit price) are:
+# Côte de Blaye
+# Thüringer Rostbratwurst
+# Mishi Kobe Niku
+# Sir Rodney's Marmalade
+# Carnarvon Tigers
+# Raclette Courdavault
+# Manjimup Dried Apples
+# Tarte au sucre
+# Ipoh Coffee
+# Rössle Sauerkraut
+
+
+# The average worker age at hire is:
+# 37.22
+
+
+# Variation in Worker Age at Hire by City:
+# Kirkland 29.0 years
+# London 32.5 years
+# Redmond 56.0 years
+# Seattle 40.0 years
+# Tacoma 40.0 years
+
+
+# The Ten Most Expensive Items (per unit price) and their suppliers are:
+# Product, Supplier
+# Côte de Blaye, Aux joyeux ecclésiastiques
+# Thüringer Rostbratwurst, Plutzer Lebensmittelgroßmärkte AG
+# Mishi Kobe Niku, Tokyo Traders
+# Sir Rodney's Marmalade, Specialty Biscuits, Ltd.
+# Carnarvon Tigers, Pavlova, Ltd.
+# Raclette Courdavault, Gai pâturage
+# Manjimup Dried Apples, G'day, Mate
+# Tarte au sucre, Forêts d'érables
+# Ipoh Coffee, Leka Trading
+# Rössle Sauerkraut, Plutzer Lebensmittelgroßmärkte AG
+
+
+# The largest category (by number of unique products in it) is:
+# Confections
+
+
+# The employee with the most territories is:
+# Robert King
