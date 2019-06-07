257aba6d0587fb1f9169a689a3d63e0f26031fab
Jason Murphy <jmurphy@jasonmsiws.local>
2019-06-07 13:13:40 -0500

    split the northwind and demo_data files.

--- Sprint Challange/demo_data.py
+++ Sprint Challange/demo_data.py
@@ -24,10 +24,10 @@ insertvals = """
              """
 
 #SQL Script is executed
-#demo_conn.executescript(insertvals)
+demo_conn.executescript(insertvals)
 
 #Connection writes data to file.
-#demo_conn.commit()
+demo_conn.commit()
 
 
 ### Querying section
@@ -40,39 +40,3 @@ print('There are ',demo_curs.execute(query).fetchall()[0][0], ' rows where x and
 
 query = """SELECT count(DISTINCT y) FROM demo"""
 print('There are ',demo_curs.execute(query).fetchall()[0][0], ' unique values in y')
-
-nw_conn = sqlite3.connect('northwind_small.sqlite3')
-
-nw_curs = nw_conn.cursor()
-
-### Part 2 Querying with Join
-
-query = """SELECT Product.ProductName from Product ORDER BY Product.UnitPrice DESC LIMIT 10"""
-print('The 10 most expensive items are ', nw_curs.execute(query).fetchall())
-
-
-query = """SELECT AVG(date(Employee.HireDate) - date(Employee.BirthDate)) FROM Employee"""
-
-print('The Average age of employees at hireing date is ',nw_curs.execute(query).fetchall())
-
-
-### Part 3 Querying with Join
-
-query = """SELECT Product.ProductName, Supplier.CompanyName
-from Supplier LEFT JOIN Product
-ON Supplier.Id = Product.SupplierId
-ORDER BY Product.UnitPrice DESC
-LIMIT 10
-"""
-
-print('The 10 most expensive items and their supplier are ', nw_curs.execute(query).fetchall())
-
-query = """SELECT Category.CategoryName
-FROM Product JOIN Category
-ON Product.CategoryId = Category.Id
-GROUP BY Product.CategoryID
-ORDER BY COUNT(*) DESC
-LIMIT 1
-"""
-
-print('The largest category of unique items is ', nw_curs.execute(query).fetchall())
