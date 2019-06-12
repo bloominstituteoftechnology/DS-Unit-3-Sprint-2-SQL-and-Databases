53907efa3484d2ba20ba78b3da5bc03a1bad32f2
tortas <lseeback@yahoo.com>
2019-06-12 00:24:31 -0700

    Complete part 1 assignment 1

--- module1-introduction-to-sql/rpg_queries.py
+++ module1-introduction-to-sql/rpg_queries.py
@@ -29,3 +29,29 @@ curs.execute('SELECT COUNT(item_ptr_id) FROM armory_weapon')
 num_weapon = curs.fetchall()
 print('4. Weapons: ', num_weapon, ' Items: ', num_item[0][0]-num_weapon[0][0])
 
+curs.execute('''SELECT COUNT(item_id)
+FROM charactercreator_character_inventory
+GROUP BY character_id
+LIMIT 20''')
+print('5. ', curs.fetchall())
+
+curs.execute('''SELECT COUNT(item_id)
+FROM charactercreator_character_inventory
+WHERE item_id > 137
+GROUP BY character_id
+LIMIT 20''')
+print('6. ', curs.fetchall())
+
+
+curs.execute('''SELECT AVG(a.rcount) FROM
+(select count(*) as rcount
+FROM charactercreator_character_inventory cci
+GROUP BY character_id) a''')
+print('7. ', curs.fetchall())
+
+curs.execute('''SELECT AVG(a.rcount) FROM
+(select count(*) as rcount
+FROM charactercreator_character_inventory cci
+WHERE item_id > 137
+GROUP BY character_id) a''')
+print('8. ', curs.fetchall())
