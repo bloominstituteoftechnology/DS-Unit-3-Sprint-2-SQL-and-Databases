da9696761ad65c907f4e062d38825d5456ee4167
Paul <pkutrich@gmail.com>
2019-07-15 18:24:04 -0700

    Improve output readability

--- module1-introduction-to-sql/rpg_queries.py
+++ module1-introduction-to-sql/rpg_queries.py
@@ -9,7 +9,7 @@ query = """SELECT name FROM charactercreator_character;"""
 
 # How many total characters are there?
 hmm = curr.execute(query)
-print(len(hmm.fetchall()))
+print(f'Total characters: {len(hmm.fetchall())}\n')
 
 # How many of each specific subclass?
 queries = ['''SELECT character_ptr_id FROM charactercreator_cleric;''',
@@ -20,31 +20,35 @@ queries = ['''SELECT character_ptr_id FROM charactercreator_cleric;''',
 
 for query in queries:
     replies = curr.execute(query)
-    print()
+    print(query.split('_')[-1])
     print(len(replies.fetchall()))
 
 # How many total items:
 query = '''SELECT item_id FROM armory_item'''
 total = len(curr.execute(query).fetchall())
-print(total)
+print(f'total items: {total}')
 
 # How many are weapons? How many are not
-query = '''SELECT item_id FROM charactercreator_character_inventory'''
+query = '''SELECT item_ptr_id FROM armory_weapon'''
 weapons = len(curr.execute(query).fetchall())
 are_not = total - weapons
+print(f'non-weapon items: {are_not}')
 
 # How many items does each character have?
-thing = curr.execute('''SELECT name, COUNT(item_id) FROM charactercreator_character_inventory INNER JOIN 
+things = curr.execute('''SELECT name, COUNT(item_id) FROM charactercreator_character_inventory INNER JOIN 
                         charactercreator_character USING (character_id)
                         GROUP BY name 
                         ORDER BY name LIMIT 20;
                         ''')
-print(thing.fetchall())
+for thing in things.fetchall():
+    print(thing)
+print()
 
-another_thing = curr.execute('''SELECT cc.name, COUNT(item_ptr_id) FROM charactercreator_character_inventory cci
+another_things = curr.execute('''SELECT cc.name, COUNT(item_ptr_id) FROM charactercreator_character_inventory cci
                                 LEFT JOIN  charactercreator_character cc on cc.character_id = cci.character_id
                                 LEFT JOIN armory_item ai on cci.item_id = ai.item_id
                                 LEFT JOIN  armory_weapon aw on ai.item_id = aw.item_ptr_id
                                 GROUP BY cc.name
                                 ORDER BY cc.character_id LIMIT 20''')
-print(another_thing.fetchall())
+for thing in another_things.fetchall():
+    print(thing)
