db8943677d85f209d07fe921730967b9152d044a
Paul <pkutrich@gmail.com>
2019-07-15 18:32:12 -0700

    improve output readability

--- module1-introduction-to-sql/buddymovie_holidayiq.py
+++ module1-introduction-to-sql/buddymovie_holidayiq.py
@@ -1,21 +1,21 @@
 import pandas as pd
 import sqlite3
 
-# print(df.info)
 conn = sqlite3.connect('database')
-df = pd.read_csv('buddymove_holidayiq.csv')
-df.to_sql('database',
-          conn,
-          if_exists='replace')
+
+# df = pd.read_csv('buddymove_holidayiq.csv')
+# df.to_sql('database',
+#           conn,
+#           if_exists='replace')
 
 curr = conn.cursor()
 
 # How many rows?
 query = '''SELECT * FROM database'''
 do_it = curr.execute(query)
-print(len(do_it.fetchall()))
+print(f'Database contains {len(do_it.fetchall())} rows')
 
 # How many users reviewed...
 query = '''SELECT "User id" FROM database WHERE Nature > 100 AND Shopping > 100'''
 do_it = curr.execute(query)
-print(do_it.fetchall())
+print(f'{len(do_it.fetchall())} reviewers reviewed Nature and Shopping > 100 times.')
