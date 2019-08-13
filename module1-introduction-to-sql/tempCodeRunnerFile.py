d04b24642dd6822bf19d6fbf8093cda0092a6e35
Nayomi Chibana <nchibana@gmail.com>
2019-08-13 00:49:09 -0400

    deleted file

--- module1-introduction-to-sql/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-    rows_result = [x[0] for x in rows2]
-    labels = ['mages','clerics','necromancers','thiefs','fighters']
-    for label, row in zip(labels, rows_result):
-        print(f'Total number of {label}: {row}')
