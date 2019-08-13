8adb5ca9fbfdd1993c8b41c08ee7e6b1e32a6e16
Marvin A Davila <marvind@Marvins-MBP.fios-router.home>
2019-08-13 18:22:30 -0400

    Module2 Assignment

---
+++ module2-sql-for-analysis/insert_titanic.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import sqlite3
+import psycopg2
+
+
+dbname = 'bndoemhv'
+user = 'bndoemhv'
+password = 'S5fpKo7jwss3AWThqAICdpQFbYjdpRgc'  # Dont commit this
+host = 'raja.db.elephantsql.com'
+
+pg_conn = psycopg2.connect(dbname=dbname, user=user,
+                           password=password, host=host)
+
+pg_conn
+
+pg_curs = pg_conn.cursor()
+
+
+df = pd.read_csv("titanic.csv")
+
+conn = sqlite3.connect("titanic.sqlite3")
+df.to_sql(name='titanic_database', con=conn, if_exists='replace')
+
+df.columns
+
+curs = conn.cursor()
+
+print(curs.execute("SELECT COUNT(*) FROM titanic_database;").fetchall())
