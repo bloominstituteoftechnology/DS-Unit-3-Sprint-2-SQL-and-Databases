571a54b8f7285a7fbcf73b95c54d7f09508aac2f
Matthew Harshman <mjharshman66@gmail.com>
2019-08-14 02:28:19 -0700

    mod 2 assign full

---
+++ insert_titanic.py
@@ -0,0 +1,67 @@
+import psycopg2
+import pandas as pandas
+
+"""
+This script will take a local csv and insert the data into
+a postgreSQL table
+"""
+
+# Database info
+dbname = 'jehgnrff'
+user = 'jehgnrff'
+password = 'XXXX'
+host = 'otto.db.elephantsql.com'
+
+# Initialzie connection and cursor
+pg_conn = psycopg2.connect(dbname=dbname, user=user,
+                           password=password, host=host)
+
+pg_curs = pg_conn.cursor()
+
+# Retrieve data
+PATH = 'C:\Users\Cactuar\Projects\DS-Unit-3-Sprint-2-SQL-and-Databases\module2-sql-for-analysis'
+
+df = pd.read_csv(PATH)
+
+# Add index column SQL congruancy and rename
+df = df.reset_index()
+df = df.rename(columns={'index':'person_id'})
+
+# Replace apostrophe's in 'Name' to prevent SQL new column
+df['Name'] = df['Name'].map(lambda x: x.replace("'", ''))
+
+# Construct list of all the rows for later insertion
+people = []
+for i in range(0,877):
+    people.append(tuple(df.loc[i]))
+
+# Store table creation string as variable
+create_table = '''
+    CREATE TABLE titanic (
+        person_id SERIAL PRImARY KEY,
+        survived INT,
+        pclass INT,
+        name TEXT,
+        sex CHAR(7)
+        age NUMERIC(5,2)
+        siblings_spouses_aboard INT,
+        parents_children_aboard INT,
+        fare NUMERIC(8,5)
+    ):
+'''
+
+# Send instructions to create table
+pg_curs.execute(create_table)
+
+# Send looped INSERT intructions to table
+for person in people:
+    insert_data = '''
+        INSERT INTO titanic
+        (survived, pclass, name, sex, age, siblings_spouses_aboard,
+         parents_children_aboard, fare)
+         VALUES ''' + str(person[1:]) + ';'
+    pg.curs.execute(insert_data)
+
+# Close cursor and commit to database
+pg_curs.close()
+pg_conn.commit()
