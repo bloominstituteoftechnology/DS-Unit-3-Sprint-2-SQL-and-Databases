777e2db7160e5f422bb5c9af4053f3e87e62184f
Marvin A Davila <marvind@Marvins-MBP.fios-router.home>
2019-08-14 18:42:10 -0400

    Added Module3 Q/A

--- module3-nosql-and-document-oriented-databases/RPG_Mongo.py
+++ module3-nosql-and-document-oriented-databases/RPG_Mongo.py
@@ -1,25 +1,23 @@
 
 import pymongo
 import sqlite3
-# MongoDB Password: UkHGft1qF7I3eLWz
 
-
+# Connecting to MongoDB
 client = pymongo.MongoClient(
     "mongodb+srv://admin:UkHGft1qF7I3eLWz@cluster0-4qxo0.mongodb.net/test?retryWrites=true&w=majority")
 db = client.test
 
-# print(db)
-# print(client.nodes)
-#print(db.test.count_documents({'x': 1}))
-
+# Retrieving rpg data
 conn = sqlite3.connect("rpg_db.sqlite3")
 curs = conn.cursor()
 
+# Gathering all the characters info
 characters = curs.execute(
     '''SELECT * FROM charactercreator_character;''').fetchall()
 
-print(characters)
+# print(characters)
 
+# For loop to insert all characters with each of their traits
 '''for character in characters:
     db.test.insert_one({
         'sql_id': character[0],
@@ -33,4 +31,14 @@ print(characters)
         'wisdom': character[8]
     })'''
 
-print(list(db.test.find()))
+print(list(db.test.find()[0]))
+print('Checking if the length is correct with 302 characters:',
+      len(list(db.test.find())))
+
+'''
+Q: How was working with MongoDB different from working with PostgreSQL?
+   What was easier, and what was harder?
+
+A: MongoDb allows you to query data easier/faster in my opinion.
+   PostgreSQL has alot more steps to insert data
+'''
