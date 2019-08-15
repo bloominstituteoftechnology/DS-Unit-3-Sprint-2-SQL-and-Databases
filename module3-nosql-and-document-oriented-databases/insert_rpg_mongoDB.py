e0f441df4aad9c9ed4b9c68ba3fec56ab56c9b3d
Matthew Harshman <mjharshman66@gmail.com>
2019-08-14 18:46:40 -0700

    removed password/changed connection

--- module3-nosql-and-document-oriented-databases/insert_rpg_mongoDB.py
+++ module3-nosql-and-document-oriented-databases/insert_rpg_mongoDB.py
@@ -1,1 +1,16 @@
 import pymongo
+import sqlite3
+
+SOURCE = 'C:/Users/Cactuar/Projects/DS-Unit-3-Sprint-2-SQL-and-Databases'
+path = SOURCE + '/module1-introduction-to-sql/rpg_db.sqlite3'
+
+sl_conn = sqlite3.connect(path)
+sl_curs = sl_conn.cursor()
+
+characters = sl_curs.execute(
+    'SELECT * FROM charactercreator_character;'
+    ).fetchall()
+
+
+print(len(characters))
+sl_curs.close()
