0f741001c45f6199f1956e60cc35db2789fd607c
Nov05 <Nov05@users.noreply.github.com>
2019-08-16 12:36:12 -0500

    version 2, better print
    better print for sql results as data frames

--- sc/northwind.py
+++ sc/northwind.py
@@ -6,19 +6,34 @@ db_name = 'northwind_small.sqlite3'
 conn = sqlite3.connect(db_name)
 curs = conn.cursor()
 
+############################################################
+print("""
+  SELECT name FROM sqlite_master
+   WEHRE type='table'
+ORDER BY name; 
+""")
 print(curs.execute("SELECT name FROM sqlite_master \
 WHERE type='table' \
 ORDER BY name;").fetchall())
 
+print("""
+SELECT sql FROM sqlite_master
+ WHERE name="Customer";
+""")
 print(curs.execute('SELECT sql FROM sqlite_master \
 WHERE name="Customer";').fetchall()[0][0])
 print()
 
+############################################################
 # start from here I am not going to capitalize the SQL 
 # tokens cause it is really unnecessary for practice...
+############################################################
 
-# What are the ten most expensive items (per unit price) 
-# in the database?
+############################################################
+print("""
+What are the ten most expensive items (per unit price) \
+in the database?
+""")
 sql = """
   select ID, ProductName, UnitPrice
     from Product
@@ -29,9 +44,12 @@ with pd.option_context('display.max_rows', None,
     print(pd.read_sql(sql, conn))
 print()
 
-# What is the average age of an employee at the time of 
-# their hiring? (Hint: a lot of arithmetic works with 
-# dates.)
+############################################################
+print("""
+What is the average age of an employee at the time of \
+their hiring? (Hint: a lot of arithmetic works with \
+dates.)
+""")
 sql = """
 select ID, FirstName, LastName, 
        HireDate - BirthDate as HireAtAge
@@ -42,8 +60,11 @@ with pd.option_context('display.max_rows', None,
     print(pd.read_sql(sql, conn))
 print()
 
-# (Stretch) How does the average age of employee 
-# at hire vary by city?
+############################################################
+print("""
+(Stretch) How does the average age of employee \
+at hire vary by city?
+""")
 sql = """
 select avg(HireAtAge), City 
 from (
@@ -58,8 +79,11 @@ with pd.option_context('display.max_rows', None,
     print(pd.read_sql(sql, conn))
 print()
 
-# What are the ten most expensive items (per unit price) 
-# in the database and their suppliers?
+############################################################
+print("""
+What are the ten most expensive items (per unit price) \
+in the database and their suppliers?
+""")
 sql = """
    select p.ID, p.ProductName, p.UnitPrice,
           s.CompanyName as Supplier
@@ -73,11 +97,13 @@ with pd.option_context('display.max_rows', None,
     print(pd.read_sql(sql, conn))
 print()
 
-# Note: I could have used max(). But I think in real life
-#       people usually want to see a list (report).
-
-# What is the largest category 
-# (by number of unique products in it)?
+############################################################
+print("""
+What is the largest category \
+(by number of unique products in it)?
+Note: I could have used max(). But I think in real life \
+people usually want to see a list (report).
+""")
 sql = """
    select Count(c.ID) as ProductTypes,
           c.ID,
@@ -94,9 +120,12 @@ with pd.option_context('display.max_rows', None,
     print(pd.read_sql(sql, conn))
 print()
 
-# (Stretch) Who's the employee with the most territories? 
-# Use TerritoryId (not name, region, or other fields) 
-# as the unique identifier for territories.
+############################################################
+print("""
+(Stretch) Who's the employee with the most territories? \
+Use TerritoryId (not name, region, or other fields) \
+as the unique identifier for territories.
+""")
 sql = """
   select t.EmployeeID,
          t.TerritoryCount,
