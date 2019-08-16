955c421f1a88d6f119f8e966ad3841778150e802
gyhou <47550192+gyhou@users.noreply.github.com>
2019-08-16 12:35:15 -0500

    Add files via upload

--- SC/northwind.py
+++ SC/northwind.py
@@ -7,19 +7,6 @@ curs = conn.cursor()
 
 # Get names of table in database
 print(curs.execute("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;").fetchall())
-"""[('Category',),
- ('Customer',),
- ('CustomerCustomerDemo',),
- ('CustomerDemographic',),
- ('Employee',),
- ('EmployeeTerritory',),
- ('Order',),
- ('OrderDetail',),
- ('Product',),
- ('Region',),
- ('Shipper',),
- ('Supplier',),
- ('Territory',)]"""
 
 # What are the ten most expensive items (per unit price) in the database?
 """[('Côte de Blaye',),
@@ -33,21 +20,64 @@ print(curs.execute("SELECT name FROM sqlite_master WHERE type='table' ORDER BY n
  ('Ipoh Coffee',),
  ('Rössle Sauerkraut',)]"""
 print(curs.execute("""
-SELECT ProductName
+SELECT ProductName, SupplierId
 FROM Product
 ORDER BY UnitPrice DESC
 LIMIT 10;""").fetchall())
 
-# What is the largest category (by number of unique products in it)?
-# CategoryId: 3
-# 13 unique products
-"""[(3, 13)]"""
+# What is the average age of an employee at the time of their hiring? (Hint: a lot of arithmetic works with dates.)
+# 37.22 years old
 print(curs.execute("""
-SELECT CategoryId, MAX(cat_id_count)
+SELECT AVG(age)
 FROM (
+SELECT HireDate-BirthDate AS age
+FROM Employee
+GROUP BY Id);""").fetchall())
+
+# (Stretch) How does the average age of employee at hire vary by city?
+"""[('Kirkland', 29.0),
+ ('London', 32.5),
+ ('Redmond', 56.0),
+ ('Seattle', 40.0),
+ ('Tacoma', 40.0)]"""
+print(curs.execute("""
+SELECT City, AVG(age)
+FROM (
+SELECT City, HireDate-BirthDate AS age
+FROM Employee
+GROUP BY Id)
+GROUP BY City;""").fetchall())
+
+# What are the ten most expensive items (per unit price) in the database and their suppliers?
+"""[('Côte de Blaye', 'Aux joyeux ecclésiastiques'),
+ ('Thüringer Rostbratwurst', 'Plutzer Lebensmittelgroßmärkte AG'),
+ ('Mishi Kobe Niku', 'Tokyo Traders'),
+ ("Sir Rodney's Marmalade", 'Specialty Biscuits, Ltd.'),
+ ('Carnarvon Tigers', 'Pavlova, Ltd.'),
+ ('Raclette Courdavault', 'Gai pâturage'),
+ ('Manjimup Dried Apples', "G'day, Mate"),
+ ('Tarte au sucre', "Forêts d'érables"),
+ ('Ipoh Coffee', 'Leka Trading'),
+ ('Rössle Sauerkraut', 'Plutzer Lebensmittelgroßmärkte AG')]"""
+print(curs.execute("""
+SELECT ProductName, CompanyName
+FROM Supplier, (
+SELECT ProductName, SupplierId
+FROM Product
+ORDER BY UnitPrice DESC)
+WHERE Id=SupplierId
+LIMIT 10;""").fetchall())
+
+# What is the largest category (by number of unique products in it)?
+# Category: Confections (13 unique products)
+"""[('Confections', 13)]"""
+print(curs.execute("""
+SELECT CategoryName, MAX(cat_id_count)
+FROM Category, (
 SELECT CategoryId, COUNT(CategoryId) AS cat_id_count
 FROM Product
-GROUP BY CategoryId);""").fetchall())
+GROUP BY CategoryId)
+WHERE Id=CategoryId;""").fetchall())
 
 # (Stretch) Who's the employee with the most territories?
 # Robert King with 10 territories
