1e8cdf96e0964155c7fb20016db09d5be2ca948d
Matthew Mauney <mauney@users.noreply.github.com>
2019-09-10 10:36:34 -0400

    Style rpg_queries.py, update gitignore

--- module1-introduction-to-sql/rpg_queries.py
+++ module1-introduction-to-sql/rpg_queries.py
@@ -32,14 +32,15 @@ print(f'Of those {num_items} items, {num_weapons} are weapons '
       f'and {num_items - num_weapons} are not weapons.\n')
 
 # How many Items does each character have? (Return first 20 rows)
-items_per_char_query = (
-    'SELECT cc.character_id, cc.name, COUNT(cci.item_id) AS item_count '
-    'FROM charactercreator_character AS cc '
-    'INNER JOIN charactercreator_character_inventory AS cci '
-    'ON cc.character_id = cci.character_id '
-    'GROUP BY cc.character_id '
-    'LIMIT 20;'
-)
+items_per_char_query = ("""
+    SELECT cc.character_id, cc.name, COUNT(cci.item_id) AS item_count
+      FROM charactercreator_character AS cc
+           INNER JOIN charactercreator_character_inventory AS cci
+           ON cc.character_id = cci.character_id
+     GROUP BY cc.character_id
+     LIMIT 20
+     ;""")
+
 items_per_char = curs.execute(items_per_char_query).fetchall()
 print('**********Number of Items per Character***********')
 print(tabulate(items_per_char,
@@ -47,73 +48,78 @@ print(tabulate(items_per_char,
 
 
 # How many Weapons does each character have? (Return first 20 rows)
-weapons_per_char_query = (
-    'SELECT cc.character_id, cc.name, COUNT() AS weapon_count '
-    'FROM charactercreator_character AS cc '
-    'INNER JOIN charactercreator_character_inventory AS cci '
-    'ON cc.character_id = cci.character_id '
-    'INNER JOIN armory_item as ai '
-    'ON cci.item_id = ai.item_id '
-    'INNER JOIN armory_weapon as aw '
-    'ON ai.item_id = aw.item_ptr_id '
-    'GROUP BY cc.character_id '
-    'LIMIT 20;'
-)
+weapons_per_char_query = ("""
+    SELECT cc.character_id, cc.name, COUNT() AS weapon_count
+      FROM charactercreator_character AS cc
+           INNER JOIN charactercreator_character_inventory AS cci
+           ON cc.character_id = cci.character_id
+           INNER JOIN armory_item as ai
+           ON cci.item_id = ai.item_id
+           INNER JOIN armory_weapon as aw
+           ON ai.item_id = aw.item_ptr_id
+     GROUP BY cc.character_id
+     LIMIT 20
+     ;""")
+
 weapons_per_char = curs.execute(weapons_per_char_query).fetchall()
 print('\n**********Number of Weapons per Character***********')
 print(tabulate(weapons_per_char, headers=['ID', 'Name', 'Weapon Count']))
 
 # On average, how many Items does each Character have?
-avg_items_query = (
-    'SELECT AVG(item_count) FROM '
-    '('
-        'SELECT cc.character_id, COUNT(cci.item_id) AS item_count '
-        'FROM charactercreator_character AS cc '
-        'LEFT JOIN charactercreator_character_inventory AS cci '
-        'ON cc.character_id = cci.character_id '
-        'GROUP BY cc.character_id'
-    ')'
-)
+avg_items_query = ("""
+    SELECT AVG(item_count) FROM
+    (
+        SELECT cc.character_id, COUNT(cci.item_id) AS item_count
+          FROM charactercreator_character AS cc
+               LEFT JOIN charactercreator_character_inventory AS cci
+               ON cc.character_id = cci.character_id
+         GROUP BY cc.character_id
+    )
+    ;""")
+
 avg_items = curs.execute(avg_items_query).fetchone()[0]
 print(f'\nThere are {avg_items:.2f} items per player on average.')
 
 # On average, how many Weapons does each character have?
 
 # First, the average for those characters who have at least one weapon
-avg_weapons_min_one_query = (
-    'SELECT AVG(weapon_count) FROM '
-    '('
-        'SELECT cc.character_id, COUNT(aw.item_ptr_id) AS weapon_count '
-        'FROM charactercreator_character AS cc '
-        'INNER JOIN charactercreator_character_inventory as cci '
-        'ON cc.character_id = cci.character_id '
-        'INNER JOIN armory_item as ai '
-        'ON cci.item_id = ai.item_id '
-        'INNER JOIN armory_weapon as aw '
-        'ON ai.item_id = aw.item_ptr_id '
-        'GROUP BY cc.character_id'
-    ');'
-)
+avg_weapons_min_one_query = ("""
+    SELECT AVG(weapon_count) FROM
+    (
+        SELECT cc.character_id, COUNT(aw.item_ptr_id) AS weapon_count
+          FROM charactercreator_character AS cc
+               INNER JOIN charactercreator_character_inventory as cci
+               ON cc.character_id = cci.character_id
+               INNER JOIN armory_item as ai
+               ON cci.item_id = ai.item_id
+               INNER JOIN armory_weapon as aw
+               ON ai.item_id = aw.item_ptr_id
+         GROUP BY cc.character_id
+    )
+    ;""")
+
 avg_weapons_min_one = curs.execute(avg_weapons_min_one_query).fetchone()[0]
 print(f'\nThere are {avg_weapons_min_one:.2f} average weapons among players '
-       'who have at least one weapon.')
+      'who have at least one weapon.')
 
 # Second, the average for over all characters
-avg_weapons_query = (
-    'SELECT AVG(weapon_count) FROM '
-    '('
-        'SELECT cc.character_id, COUNT(aw.item_ptr_id) AS weapon_count '
-        'FROM charactercreator_character AS cc '
-        'INNER JOIN charactercreator_character_inventory as cci '
-        'ON cc.character_id = cci.character_id '
-        'INNER JOIN armory_item as ai '
-        'ON cci.item_id = ai.item_id '
-        'LEFT JOIN armory_weapon as aw '
-        'ON ai.item_id = aw.item_ptr_id '
-        'GROUP BY cc.character_id'
-    ');'
-)
+avg_weapons_query = ("""
+    SELECT AVG(weapon_count) FROM
+    (
+        SELECT cc.character_id, COUNT(aw.item_ptr_id) AS weapon_count
+          FROM charactercreator_character AS cc
+               INNER JOIN charactercreator_character_inventory as cci
+               ON cc.character_id = cci.character_id
+               INNER JOIN armory_item as ai
+               ON cci.item_id = ai.item_id
+               LEFT JOIN armory_weapon as aw
+               ON ai.item_id = aw.item_ptr_id
+         GROUP BY cc.character_id
+    )
+    ;""")
+
 avg_weapons = curs.execute(avg_weapons_query).fetchone()[0]
 print(f'\nThere are {avg_weapons:.2f} average weapons among all players.\n')
 
+curs.close()
 conn.close()
