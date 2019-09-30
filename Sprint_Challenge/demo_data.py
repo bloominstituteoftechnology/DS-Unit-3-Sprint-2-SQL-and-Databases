856ec0ba91a0449c1980482b773815ba583f2139
kmk028 <kmk028@users.noreply.github.com>
2019-09-29 19:53:19 -0700

    demo_data.py completed

--- Sprint_Challenge/demo_data.py
+++ Sprint_Challenge/demo_data.py
@@ -1,14 +1,14 @@
 import sqlite3 
 
-conn = sqlite3.connect('Sprint_Challenge/demo_data.sqlite3')
+conn = sqlite3.connect('demo_data.sqlite3')
 
 cur = conn.cursor()
 
 drop_query = 'DROP TABLE IF EXISTS demo;'
 
 table_query = '''CREATE TABLE demo( 
-                s CHAR(1) PRIMARY KEY
-                x INT NOT NULL
+                s CHAR(1) PRIMARY KEY,
+                x INT NOT NULL,
                 y INT NOT NULL);'''
 
 insert_query = """INSERT INTO demo VALUES(?, ?, ?);"""
@@ -24,12 +24,22 @@ conn.executemany(insert_query,values)
 
 conn.commit()
 
-dbfile = 'Sprint_Challenge/demo_data.sqlite3'
+dbfile = 'demo_data.sqlite3'
 
 def count(dbfile,query):
     c = sqlite3.connect(dbfile)
     curs = c.cursor()
     return curs.execute(query).fetchall()[0][0]
+
+x = count(dbfile,'SELECT COUNT(*) FROM demo;')
+print (f'Total number of rows in demo table is {x}\n')
+
+y = count(dbfile,'SELECT COUNT(*) FROM demo WHERE x>=5 AND y>=5;')
+print (f'number of rows in demo table where x and y are atleast 5 is {y}\n')
+
+z = count(dbfile,'SELECT COUNT(DISTINCT y) FROM demo ')
+print (f'Number of unique values in y is {z}\n')
+
 
 
 
