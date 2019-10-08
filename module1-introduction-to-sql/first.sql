17df58b226450291b4d1a693441642b244be4e1d
scottwmwork <scottwmwork@users.noreply.github.com>
2019-10-08 15:40:34 -0700

    added sql file

--- module1-introduction-to-sql/first.sql
+++ module1-introduction-to-sql/first.sql
@@ -1,1 +1,31 @@
 -- SQLite
+
+--How many total Characters are there?
+SELECT COUNT(character_id) 
+FROM charactercreator_character;
+
+-- How many of each specific subclass?
+SELECT COUNT(character_ptr_id)
+FROM charactercreator_mage;
+
+SELECT COUNT(mage_ptr_id)
+FROM charactercreator_necromancer;
+
+SELECT COUNT(character_ptr_id)
+FROM charactercreator_fighter;
+
+SELECT COUNT(character_ptr_id)
+FROM charactercreator_cleric;
+
+SELECT COUNT(character_ptr_id)
+FROM charactercreator_thief;
+
+--How many total Items?
+SELECT COUNT(item_id) 
+FROM armory_item;
+
+--How many of the Items are weapons?
+SELECT COUNT(item_ptr_id)
+FROM armory_weapon;
+
+
