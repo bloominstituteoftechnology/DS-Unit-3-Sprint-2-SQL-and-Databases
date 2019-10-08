34fd54acfa0bf8d9fd42dc38d11e9b7c6b1bac91
scottwmwork <scottwmwork@users.noreply.github.com>
2019-10-08 16:00:35 -0700

    updated sql file

--- module1-introduction-to-sql/first.sql
+++ module1-introduction-to-sql/first.sql
@@ -28,4 +28,18 @@ FROM armory_item;
 SELECT COUNT(item_ptr_id)
 FROM armory_weapon;
 
+--How many Items does each character have? (Return first 20 rows)
+SELECT character_id,item_id
+FROM charactercreator_character_inventory
+LIMIT 20;
+
+--How many Weapons does each character have?
+SELECT character_id,item_id
+FROM charactercreator_character_inventory
+WHERE item_id IN (SELECT item_ptr_id FROM armory_weapon)
+LIMIT 20;
+
+--On average, how many Items does each Character have?
+
+--On average, how many Weapons does each character have?
 
