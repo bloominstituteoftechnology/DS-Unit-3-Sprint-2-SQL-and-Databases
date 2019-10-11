528e12833227f0a4c61687f66d574d0366575b62
scottwmwork <scottwmwork@users.noreply.github.com>
2019-10-11 10:47:08 -0700

    sprint challenge finish

--- Sprint Challenge/demo_data.py
+++ Sprint Challenge/demo_data.py
@@ -1,14 +1,14 @@
 import sqlite3
 
-#Connect to sqlite file 
+# Connect to sqlite file
 conn = sqlite3.connect('demo_data.sqlite3')
 curs = conn.cursor()
 
-#Delete the table if one exists already
+# Delete the table if one exists already
 delete_table = 'DROP TABLE IF EXISTS demo'
 curs.execute(delete_table)
 
-#Create the table and insert values
+# Create the table and insert values
 createtbl = "CREATE TABLE demo (s varchar(255),x int, y int);"
 inserttbl = """
 INSERT INTO demo(
@@ -16,39 +16,37 @@ INSERT INTO demo(
     x,
     y)
 VALUES
-    ('g','v','f'), 
-    (3,5,8), 
+    ('g','v','f'),
+    (3,5,8),
     (9,7,7);
 """
 curs.execute(createtbl)
 curs.execute(inserttbl)
 
-#Counts the amount of rows
+# Counts the amount of rows
 count_row = """
 SELECT COUNT(s)
 FROM demo;
 """
-#Returns 3
+# Returns 3
 
-#counts how many instances that both x and y contain 5
+# counts how many instances that both x and y contain 5
 atleast_five = """
 SELECT COUNT(*)
 FROM demo
 WHERE x = 5 AND y = 5;
 """
-#Returns 0
 
-#counts the number of unique values in y
+# counts the number of unique values in y
 unique = """
 SELECT COUNT(DISTINCT y)
 FROM demo;
 """
-#Returns 2
 
-print(curs.execute(count_row).fetchall()) #Returns 3
-print(curs.execute(atleast_five).fetchall()) #Returns 0
-print(curs.execute(unique).fetchall()) #Returns 2
+print(curs.execute(count_row).fetchall())  # Returns 3
+print(curs.execute(atleast_five).fetchall())  # Returns 0
+print(curs.execute(unique).fetchall())  # Returns 2
 
-#Close and commit!
+# Close and commit!
 curs.close()
 conn.commit()
--- Sprint Challenge/northwind.py
+++ Sprint Challenge/northwind.py
@@ -1,24 +1,33 @@
 import sqlite3
 
-#connect to sqlite file
+# connect to sqlite file
 conn = sqlite3.connect('northwind_small.sqlite3')
 curs = conn.cursor()
 
-#ten most expensive items (per unit price) in the database
-print(
-    curs.execute('SELECT Id,UnitPrice FROM Product ORDER BY UnitPrice DESC LIMIT 10;').fetchall()
-)
-#Returns: [(38, 263.5), (29, 123.79), (9, 97), (20, 81), (18, 62.5), (59, 55), (51, 53), (62, 49.3), (43, 46), (28, 45.6)]
+expensive_items = """
+SELECT Id,UnitPrice
+FROM Product
+ORDER BY UnitPrice DESC
+LIMIT 10;
+"""
 
-#the average age of an employee at the time of their hiring
+# ten most expensive items (per unit price) in the database
+print(
+    curs.execute(expensive_items).fetchall()
+)
+
+# Returns: [(38, 263.5), (29, 123.79), (9, 97), (20, 81),
+# (18, 62.5), (59, 55), (51, 53), (62, 49.3), (43, 46), (28, 45.6)]
+
+# the average age of an employee at the time of their hiring
 employee_age = """
 SELECT AVG(date(HireDate) - date(BirthDate))
 FROM Employee;
 """
 print(curs.execute(employee_age).fetchall())
-#Returns: [(37.22222222222222,)]
+# Returns: [(37.22222222222222,)]
 
-#The ten most expensive items (per unit price) and suppliers
+# The ten most expensive items (per unit price) and suppliers
 unit_supply = """
 SELECT Product.Id, ProductName,SupplierID,CompanyName
 FROM Product
@@ -28,18 +37,18 @@ ORDER BY UnitPrice DESC
 LIMIT 10;
 """
 print(curs.execute(unit_supply).fetchall())
-#Returns:[(38, 'Côte de Blaye', 18, 'Aux joyeux ecclésiastiques'), 
-#(29, 'Thüringer Rostbratwurst', 12, 'Plutzer Lebensmittelgroßmärkte AG'), 
-# (9, 'Mishi Kobe Niku', 4, 'Tokyo Traders'), 
-# (20, "Sir Rodney's Marmalade", 8, 'Specialty Biscuits, Ltd.'), 
-# (18, 'Carnarvon Tigers', 7, 'Pavlova, Ltd.'), 
-# (59, 'Raclette Courdavault', 28, 'Gai pâturage'), 
-# (51, 'Manjimup Dried Apples', 24, "G'day, Mate"), 
-# (62, 'Tarte au sucre', 29, "Forêts d'érables"), 
-# (43, 'Ipoh Coffee', 20, 'Leka Trading'), 
+# Returns:[(38, 'Côte de Blaye', 18, 'Aux joyeux ecclésiastiques'),
+# (29, 'Thüringer Rostbratwurst', 12, 'Plutzer Lebensmittelgroßmärkte AG'),
+# (9, 'Mishi Kobe Niku', 4, 'Tokyo Traders'),
+# (20, "Sir Rodney's Marmalade", 8, 'Specialty Biscuits, Ltd.'),
+# (18, 'Carnarvon Tigers', 7, 'Pavlova, Ltd.'),
+# (59, 'Raclette Courdavault', 28, 'Gai pâturage'),
+# (51, 'Manjimup Dried Apples', 24, "G'day, Mate"),
+# (62, 'Tarte au sucre', 29, "Forêts d'érables"),
+# (43, 'Ipoh Coffee', 20, 'Leka Trading'),
 # (28, 'Rössle Sauerkraut', 12, 'Plutzer Lebensmittelgroßmärkte AG')]
 
-#The largest category by number of unique products in it
+# The largest category by number of unique products in it
 category = """
 SELECT CategoryName, COUNT(DISTINCT Product.Id) AS count
 FROM Category
@@ -50,5 +59,4 @@ ORDER By count DESC
 LIMIT 1;
 """
 print(curs.execute(category).fetchall())
-#Returns: [('Confections', 13)]
-
+# Returns: [('Confections', 13)]
