19296ff1338de91520db580e07a4aa6217c3048a
Vera Mendes <54785435+VeraMendes@users.noreply.github.com>
2019-11-05 19:05:36 -0800

    Add files via upload

--- module2-sql-for-analysis/RPG_elephantsql.py
+++ module2-sql-for-analysis/RPG_elephantsql.py
@@ -11,10 +11,10 @@ Original file is located at
 
 import psycopg2
 
-dbname = 'user' #same than user
-user = 'user' #same than dbname
-password = 'pass' #Don't commit this to github
-host = 'host' #from SERVER type
+dbname = 'rzivbomq' #same than user
+user = 'rzivbomq' #same than dbname
+password = 'toHi2VGNeZnh_deVMOSAGGsiYYIe-O7K' #Don't commit this to github
+host = 'salt.db.elephantsql.com' #from SERVER type
 
 pg_conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host)
 
@@ -266,7 +266,7 @@ weapons[0]
 
 pg_weapons[0]
 
-for weapon, pg_weapon in zip(weapon, pg_weapon):
+for weapon, pg_weapon in zip(weapons, pg_weapons):
     assert weapon == pg_weapon
 
 pg_curs.close()
@@ -315,14 +315,15 @@ VALUES """ + str(ids[0][1:]) + ';'
 
 print(id_insert)
 
-ids[0][1:]
+ids[10][1:]
 
-for id in ids:
+for identification in ids:
   insert_id = """
     INSERT INTO charactercreator_character_inventory
     (character_id, item_id)
-    VALUES """ + str(ids[1:]) + ';'
+    VALUES """ + str(identification[1:]) + ';'
   pg_curs.execute(insert_id)
+  #print(insert_id)
 
 insert_id
 
@@ -340,8 +341,359 @@ ids[0]
 
 pg_ids[0]
 
-for identification, pg_identification in zip(identification, pg_identification):
+for identification, pg_identification in zip(ids, pg_ids):
     assert identification == pg_identification
+
+pg_curs.close()
+pg_conn.commit()
+
+"""# charactercreator_mage"""
+
+sl_curs.execute('SELECT COUNT(*) FROM charactercreator_mage').fetchall()
+
+mages = sl_curs.execute('SELECT * FROM charactercreator_mage;').fetchall()
+
+sl_curs.execute('PRAGMA table_info(charactercreator_mage);').fetchall()
+
+create_mages_table = """
+  CREATE TABLE charactercreator_mage (
+	character_ptr_id	INT NOT NULL,
+	has_pet	INT NOT NULL,
+	mana	INT NOT NULL,
+	FOREIGN KEY(character_ptr_id) REFERENCES charactercreator_character(character_id),
+	PRIMARY KEY(character_ptr_id)
+);
+  """
+
+pg_conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host)
+pg_conn
+pg_curs = pg_conn.cursor()
+
+pg_curs.execute(create_mages_table)
+
+show_tables = """
+SELECT *
+FROM pg_catalog.pg_tables
+WHERE schemaname != 'pg_catalog'
+AND schemaname != 'information_schema';
+"""
+
+pg_curs.execute(show_tables)
+pg_curs.fetchall()
+
+mage_insert = """
+INSERT INTO charactercreator_mage
+(character_ptr_id, has_pet, mana)
+VALUES """ + str(mages[0]) + ';'
+
+print(mage_insert)
+
+for mage in mages:
+  insert_mage = """
+    INSERT INTO  charactercreator_mage
+    (character_ptr_id, has_pet, mana)
+    VALUES """ + str(mage[0:]) + ';'
+  pg_curs.execute(insert_mage)
+
+insert_mage
+
+pg_curs.execute ('SELECT * FROM charactercreator_mage;')
+pg_curs.fetchall()
+
+pg_curs.close()
+pg_conn.commit()
+
+pg_curs = pg_conn.cursor()
+pg_curs.execute('SELECT * FROM charactercreator_mage;')
+pg_mages = pg_curs.fetchall()
+
+mages[0]
+
+pg_mages[0]
+
+for mage, pg_mage in zip(mages, pg_mages):
+    assert mage == pg_mage
+
+pg_curs.close()
+pg_conn.commit()
+
+"""# charactercreator_thief"""
+
+sl_curs.execute('SELECT COUNT(*) FROM charactercreator_thief').fetchall()
+
+thieves = sl_curs.execute('SELECT * FROM charactercreator_thief').fetchall()
+
+sl_curs.execute('PRAGMA table_info(charactercreator_thief);').fetchall()
+
+pg_conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host)
+pg_conn
+pg_curs = pg_conn.cursor()
+
+create_thieves_table = """
+  CREATE TABLE charactercreator_thief (
+	character_ptr_id	INT NOT NULL,
+	is_sneaking	INT NOT NULL,
+	energy	INT NOT NULL,
+	FOREIGN KEY(character_ptr_id) REFERENCES charactercreator_character(character_id),
+	PRIMARY KEY(character_ptr_id)
+);
+"""
+
+pg_curs.execute(create_thieves_table)
+
+show_tables = """
+SELECT *
+FROM pg_catalog.pg_tables
+WHERE schemaname != 'pg_catalog'
+AND schemaname != 'information_schema';
+"""
+
+pg_curs.execute(show_tables)
+pg_curs.fetchall()
+
+thieves[0]
+
+thief_insert = """
+INSERT INTO charactercreator_thief
+(character_ptr_id, is_sneaking, energy)
+VALUES """ + str(thieves[0]) + ';'
+
+print(thief_insert)
+
+for thief in thieves:
+  insert_thief = """
+    INSERT INTO  charactercreator_thief
+    (character_ptr_id, is_sneaking, energy)
+    VALUES """ + str(thief[0:]) + ';'
+  pg_curs.execute(insert_thief)
+
+insert_thief
+
+pg_curs.execute ('SELECT * FROM charactercreator_thief;')
+pg_curs.fetchall()
+
+pg_curs.close()
+pg_conn.commit()
+
+pg_curs = pg_conn.cursor()
+pg_curs.execute('SELECT * FROM charactercreator_thief;')
+pg_thieves = pg_curs.fetchall()
+
+thieves[0]
+
+pg_thieves[0]
+
+for thief, pg_thief in zip(thieves, pg_thieves):
+    assert thief == pg_thief
+
+pg_curs.close()
+pg_conn.commit()
+
+"""# charactercreator_cleric"""
+
+sl_curs.execute('SELECT COUNT(*) FROM charactercreator_cleric').fetchall()
+
+clerics = sl_curs.execute('SELECT * FROM charactercreator_cleric').fetchall()
+
+sl_curs.execute('PRAGMA table_info(charactercreator_cleric);').fetchall()
+
+create_clerics_table = """
+  CREATE TABLE charactercreator_cleric (
+	character_ptr_id	INT NOT NULL,
+	using_shield	INT NOT NULL,
+	mana	INT NOT NULL,
+	FOREIGN KEY(character_ptr_id) REFERENCES charactercreator_character(character_id),
+	PRIMARY KEY(character_ptr_id)
+);
+"""
+
+pg_conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host)
+pg_conn
+pg_curs = pg_conn.cursor()
+
+pg_curs.execute(create_clerics_table)
+
+show_tables = """
+SELECT *
+FROM pg_catalog.pg_tables
+WHERE schemaname != 'pg_catalog'
+AND schemaname != 'information_schema';
+"""
+
+pg_curs.execute(show_tables)
+pg_curs.fetchall()
+
+cleric_insert = """
+INSERT INTO charactercreator_cleric
+(character_ptr_id, using_shield, mana)
+VALUES """ + str(clerics[0]) + ';'
+
+print(cleric_insert)
+
+for cleric in clerics:
+  insert_cleric = """
+    INSERT INTO  charactercreator_cleric
+    (character_ptr_id, using_shield, mana)
+    VALUES """ + str(cleric[0:]) + ';'
+  pg_curs.execute(insert_cleric)
+
+insert_cleric
+
+pg_curs.execute ('SELECT * FROM charactercreator_cleric;')
+pg_curs.fetchall()
+
+pg_curs.close()
+pg_conn.commit()
+
+pg_curs = pg_conn.cursor()
+pg_curs.execute('SELECT * FROM charactercreator_cleric;')
+pg_clerics = pg_curs.fetchall()
+
+clerics[0]
+
+pg_clerics[0]
+
+for cleric, pg_cleric in zip(clerics, pg_clerics):
+    assert cleric == pg_cleric
+
+pg_curs.close()
+pg_conn.commit()
+
+"""# charactercreator_fighter"""
+
+sl_curs.execute('SELECT COUNT(*) FROM charactercreator_fighter').fetchall()
+
+fighters = sl_curs.execute('SELECT * FROM charactercreator_fighter').fetchall()
+
+sl_curs.execute('PRAGMA table_info(charactercreator_fighter);').fetchall()
+
+create_fighters_table = """
+  CREATE TABLE charactercreator_fighter (
+	character_ptr_id	INT NOT NULL,
+	using_shield	INT NOT NULL,
+	rage	INT NOT NULL,
+	FOREIGN KEY(character_ptr_id) REFERENCES charactercreator_character(character_id),
+	PRIMARY KEY(character_ptr_id)
+);
+"""
+
+pg_conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host)
+pg_conn
+pg_curs = pg_conn.cursor()
+
+pg_curs.execute(create_fighters_table)
+
+show_tables = """
+SELECT *
+FROM pg_catalog.pg_tables
+WHERE schemaname != 'pg_catalog'
+AND schemaname != 'information_schema';
+"""
+
+pg_curs.execute(show_tables)
+pg_curs.fetchall()
+
+fighter_insert = """
+INSERT INTO charactercreator_fighter
+(character_ptr_id, using_shield, rage)
+VALUES """ + str(fighters[0]) + ';'
+
+print(fighter_insert)
+
+for fighter in fighters:
+  insert_fighter = """
+    INSERT INTO  charactercreator_fighter
+    (character_ptr_id, using_shield, rage)
+    VALUES """ + str(fighter[0:]) + ';'
+  pg_curs.execute(insert_fighter)
+
+insert_fighter
+
+pg_curs.execute ('SELECT * FROM charactercreator_fighter;')
+pg_curs.fetchall()
+
+pg_curs.close()
+pg_conn.commit()
+
+pg_curs = pg_conn.cursor()
+pg_curs.execute('SELECT * FROM charactercreator_fighter;')
+pg_fighters = pg_curs.fetchall()
+
+fighters[0]
+
+pg_fighters[0]
+
+for fighter, pg_fighter in zip(fighters, pg_fighters):
+    assert fighter == pg_fighter
+
+pg_curs.close()
+pg_conn.commit()
+
+"""# charactercreator_necromancer"""
+
+sl_curs.execute('SELECT COUNT(*) FROM charactercreator_necromancer').fetchall()
+
+nmcs = sl_curs.execute('SELECT * FROM charactercreator_necromancer').fetchall()
+
+sl_curs.execute('PRAGMA table_info(charactercreator_necromancer);').fetchall()
+
+create_nmcs_table = """
+  CREATE TABLE charactercreator_necromancer (
+	mage_ptr_id	INT NOT NULL,
+	talisman_charged	INT NOT NULL,
+	FOREIGN KEY(mage_ptr_id) REFERENCES charactercreator_mage(character_ptr_id),
+	PRIMARY KEY(mage_ptr_id)
+);
+"""
+
+pg_conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host)
+pg_conn
+pg_curs = pg_conn.cursor()
+
+pg_curs.execute(create_nmcs_table)
+
+show_tables = """
+SELECT *
+FROM pg_catalog.pg_tables
+WHERE schemaname != 'pg_catalog'
+AND schemaname != 'information_schema';
+"""
+
+pg_curs.execute(show_tables)
+pg_curs.fetchall()
+
+nmc_insert = """
+INSERT INTO charactercreator_necromancer
+(mage_ptr_id, talisman_charged)
+VALUES """ + str(nmcs[0]) + ';'
+
+print(nmc_insert)
+
+for nmc in nmcs:
+  insert_nmc = """
+    INSERT INTO  charactercreator_necromancer
+    (mage_ptr_id, talisman_charged)
+    VALUES """ + str(nmc[0:]) + ';'
+  pg_curs.execute(insert_nmc)
+
+insert_nmc
+
+pg_curs.execute ('SELECT * FROM charactercreator_necromancer;')
+pg_curs.fetchall()
+
+pg_curs.close()
+pg_conn.commit()
+
+pg_curs = pg_conn.cursor()
+pg_curs.execute('SELECT * FROM charactercreator_necromancer;')
+pg_nmcs = pg_curs.fetchall()
+
+nmcs[0]
+
+pg_nmcs[0]
+
+for nmc, pg_nmc in zip(nmcs, pg_nmcs):
+    assert nmc == pg_nmc
 
 pg_curs.close()
 pg_conn.commit()
