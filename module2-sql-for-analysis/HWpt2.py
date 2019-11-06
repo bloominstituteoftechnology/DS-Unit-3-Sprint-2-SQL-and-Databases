d50d6021834860a530a96e7fd42f0d07a8dbf55a
HakujouRyu <47827212+HakujouRyu@users.noreply.github.com>
2019-11-06 10:45:48 -0500

    Update HWpt2.py

--- module2-sql-for-analysis/HWpt2.py
+++ module2-sql-for-analysis/HWpt2.py
@@ -8,6 +8,7 @@ df = pd.read_csv('module2-sql-for-analysis/titanic.csv')
 df.head()
 #See what the datatypes are
 df.dtypes
+df.info()
 
 # Create engine for DF insertion 
 engine = create_engine('postgres://ensbdkiv:s2-dWAkXkoJhwcZe1PQfHN9Tx6i7clv9@salt.db.elephantsql.com:5432/ensbdkiv')
