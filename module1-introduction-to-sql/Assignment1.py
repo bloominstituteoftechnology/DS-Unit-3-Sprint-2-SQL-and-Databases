811488a74a23353cc37ce303c4f92004991fac96
lechemrc <roblecheminant@gmail.com>
2019-11-13 20:58:14 -0700

    updated assignment

--- module1-introduction-to-sql/Assignment1.py
+++ module1-introduction-to-sql/Assignment1.py
@@ -8,11 +8,61 @@ cur.execute('SELECT * FROM charactercreator_character')
 print('SELECT * FROM charactercreator_character')
 cur.fetchall()
 
-# # Total of each subclass
-# print('How many of each specific subclass?')
-# cur.execute('')
-# print('')
-# cur.fetchall()
+# Total of each subclass
+print('How many of each specific subclass?')
 
+# clerics
+cur.execute('''
+    SELECT *
+    FROM charactercreator_character
+    LEFT JOIN charactercreator_cleric
+    ON character_id = character_ptr_id
+    WHERE character_ptr_id IS NOT NULL
+    ''')
+result = cur.fetchall()
+print(f'There are {len(result)} clerics')
 
+# fighters
+cur.execute('''
+    SELECT *
+    FROM charactercreator_character
+    LEFT JOIN charactercreator_fighter
+    ON character_id = character_ptr_id
+    WHERE character_ptr_id IS NOT NULL
+    ''')
+result = cur.fetchall()
+print(f'There are {len(result)} fighters')
+
+# mages
+cur.execute('''
+    SELECT *
+    FROM charactercreator_character
+    LEFT JOIN charactercreator_mage
+    ON character_id = character_ptr_id
+    WHERE character_ptr_id IS NOT NULL
+    ''')
+result = cur.fetchall()
+print(f'There are {len(result)} mages')
+
+# necromancers
+cur.execute('''
+    SELECT *
+    FROM charactercreator_character
+    LEFT JOIN charactercreator_necromancer
+    ON character_id = mage_ptr_id
+    WHERE mage_ptr_id IS NOT NULL
+    ''')
+result = cur.fetchall()
+print(f'There are {len(result)} necromancers')
+
+# thiefs
+cur.execute('''
+    SELECT *
+    FROM charactercreator_character
+    LEFT JOIN charactercreator_cleric
+    ON character_id = character_ptr_id
+    WHERE character_ptr_id IS NOT NULL
+    ''')
+result = cur.fetchall()
+print(f'There are {len(result)} thieves')
 
