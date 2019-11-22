1b6bd2b89a0ecfbb9bc58d934344876261a11320
lechemrc <roblecheminant@gmail.com>
2019-11-21 19:26:11 -0700

    updated assignment and sql files

--- module1-introduction-to-sql/assignment1_pt2.py
+++ module1-introduction-to-sql/assignment1_pt2.py
@@ -1,6 +1,5 @@
 import sqlite3
 import pandas as pd
-from sqlalchemy import create_engine
 
 conn = sqlite3.connect('buddymove_holidayiq.sqlite3')
 cur = conn.cursor()
@@ -10,23 +9,32 @@ df = pd.read_csv('https://raw.githubusercontent.com/lechemrc/DS-Unit-3-Sprint-2\
 df = df.rename(columns={'User Id':'User_id'})
 df.head()
 
-df.to_sql('buddymove_holidayiq', con=conn)
+# df.to_sql('buddymove_holidayiq', con=conn)
 
 cur.execute('''
     SELECT * FROM buddymove_holidayiq
 ''')
-cur.fetchall()
+result = cur.fetchall()
+print(result)
 
+# Selecting all values with > 100 for Nature and Shopping
 cur.execute('''
     SELECT COUNT(*)
     FROM buddymove_holidayiq
     WHERE Nature >= 100
     AND Shopping >= 100
 ''')
-cur.fetchall()
+result = cur.fetchall()
+print(result)
 
-cur.execute('''
-    SELECT AVG(Sports), AVG(Religious), AVG(Nature), AVG(Theatre), AVG(), AVG(), AVG()
-    FROM buddymove_holidayiq
-''')
-cur.fetchall()
+# Creating iterable list
+avg_list = ['Sports', 'Religious', 'Nature', 'Theatre', 'Shopping', 'Picnic']
+
+# Iterating through each category to find the average
+for item in avg_list:
+    cur.execute(f"""
+        SELECT AVG({item})
+        FROM buddymove_holidayiq
+    """)
+    result = cur.fetchall()
+    print(f'The average of {item} is {result}')
