e65b0072b41669755b3f4906e08fcc632f33bb8c
Daniel Jaouen <dcj24@cornell.edu>
2019-11-23 13:26:32 -0500

    Add stretch goals

--- SC/northwind.py
+++ SC/northwind.py
@@ -3,6 +3,7 @@ import sqlite3
 conn = sqlite3.connect('northwind_small.sqlite3')
 cur = conn.cursor()
 
+# PART 2 --------------------------
 query1 = """
 SELECT ProductName, UnitPrice
 FROM Product
@@ -26,6 +27,7 @@ FROM
 res2 = cur.execute(query2)
 print(res2.fetchall())
 
+# PART 3 --------------------------
 query3 = """
 SELECT p.ProductName, s.CompanyName
 FROM Product p
@@ -47,3 +49,27 @@ ORDER BY p.CategoryId
 
 res4 = cur.execute(query4)
 print(res4.fetchall())
+
+# PART 2 Stretch --------------------------
+query5 = """
+SELECT AVG(HireDate - BirthDate), City
+FROM Employee
+GROUP BY City
+"""
+
+res5 = cur.execute(query5)
+print(res5.fetchall())
+
+# PART 3 Stretch --------------------------
+query6 = """
+SELECT COUNT(*), e.Title, e.FirstName, e.LastName
+FROM EmployeeTerritory et
+INNER JOIN Employee e
+WHERE e.Id = et.EmployeeId
+GROUP BY EmployeeId
+ORDER BY COUNT(*) DESC
+LIMIT 1
+"""
+
+res6 = cur.execute(query6)
+print(res6.fetchall())
