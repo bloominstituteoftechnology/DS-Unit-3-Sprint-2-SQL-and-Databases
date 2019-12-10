b5e01a70b231e7801edf39cbce2b6a552e6e4ee8
MIkeS <53836885+mseryy01@users.noreply.github.com>
2019-12-10 17:28:54 -0400

    Assignment part 2

---
+++ module1-introduction-to-sql/assignment_part2.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import sqlite3
+
+df = pd.read_csv('buddymove_holidayiq.csv')
+
+conn = sqlite3.connect('buddymove_holidayiq.sqlite3')
+
+df.to_sql('df', conn, if_exists = 'replace')
+
+"""Count number of rows"""
+print(pd.read_sql_query('SELECT COUNT(*) FROM df;', conn))
+
+"""
+How many users who reviewed at least 100 Nature in the category also reviewed
+at least 100 in the Shopping category?
+"""
+print(pd.read_sql_query('SELECT COUNT(*) FROM df WHERE Nature >= 100 AND Shopping >=100;', conn))
+
+"""
