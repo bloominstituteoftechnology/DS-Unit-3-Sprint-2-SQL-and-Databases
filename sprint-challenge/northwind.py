ef3de286644f612fde5b2a56c4f92587cee41bcc
KhaSab5292 <KhaSab5292@users.noreply.github.com>
2019-12-13 13:49:13 -0500

    part 3

--- sprint-challenge/northwind.py
+++ sprint-challenge/northwind.py
@@ -4,13 +4,6 @@ import sqlite3
 conn = sqlite3.connect('northwind_small.sqlite3')
 curs = conn.cursor()
 
-#Viewing table names
-curs.execute(
-    "SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;"
-    )
-answer = curs.fetchall()
-print(f'The table names are: \n{answer}\n')
-
 #What are the ten most expensive items (per unit price) in the database?
 curs.execute("""
     SELECT ProductName, UnitPrice 
@@ -28,3 +21,30 @@ curs.execute("""
 """)
 answer = curs.fetchall()
 print(f'The average age at hiring is {answer}.\n')
+
+#Ten most expensive products and their suppliers
+curs.execute("""
+    SELECT ProductName, UnitPrice, CompanyName
+    FROM Product p
+    LEFT JOIN Supplier s
+    ON p.ID = s.ID
+    ORDER BY UnitPrice DESC
+    LIMIT 10;
+""")
+answer = curs.fetchall()
+print(f'The ten most expensive products and their suppliers are: \n{answer}\n')
+
+#What is the largest category by number of unique products?
+curs.execute("""
+    SELECT CategoryName, COUNT(*) - COUNT(DISTINCT CategoryName) 
+    FROM Product p
+    LEFT JOIN Category c
+    ON p.ID = c.ID
+    ORDER BY CategoryName DESC;
+""")
+answer = curs.fetchall()
+print(f'The largest category based on unique products is {a}\n')
+
+# Closing connection and committing
+curs.close()
+conn.commit()
