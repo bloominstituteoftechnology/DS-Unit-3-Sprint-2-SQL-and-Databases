d586266358c615b1b871ab7dd8882effa58a935c
KhaSab5292 <KhaSab5292@users.noreply.github.com>
2019-12-16 20:47:53 -0500

    rpg.queries.py

--- module1-introduction-to-sql/rpg_queries.py
+++ module1-introduction-to-sql/rpg_queries.py
@@ -16,33 +16,33 @@ print('There are', characters, 'characters in total.')
 
 #How many of each specific subclass?
 curs.execute("""
-            SELECT COUNT(character_ptr_id)
-            FROM charactercreator_cleric;
-            """)
+    SELECT COUNT(character_ptr_id)
+    FROM charactercreator_cleric;
+    """)
 clerics = curs.fetchall()[0][0]
 
 curs.execute("""
-            SELECT COUNT(character_ptr_id)
-            FROM charactercreator_fighter;
-            """)
+    SELECT COUNT(character_ptr_id)
+    FROM charactercreator_fighter;
+    """)
 fighters = curs.fetchall()[0][0]
 
 curs.execute("""
-            SELECT COUNT(character_ptr_id)
-            FROM charactercreator_mage;
-            """)
+    SELECT COUNT(character_ptr_id)
+    FROM charactercreator_mage;
+    """)
 mages = curs.fetchall()[0][0]
 
 curs.execute("""
-            SELECT COUNT(mage_ptr_id)
-            FROM charactercreator_necromancer;
-            """)
+    SELECT COUNT(mage_ptr_id)
+    FROM charactercreator_necromancer;
+    """)
 necromancers = curs.fetchall()[0][0]
 
 curs.execute("""
-            SELECT COUNT(character_ptr_id)
-            FROM charactercreator_thief;
-            """)
+    SELECT COUNT(character_ptr_id)
+    FROM charactercreator_thief;
+    """)
 thieves = curs.fetchall()[0][0]
 
 print(clerics, 'Clerics,', fighters, 'Fighters,', 
@@ -51,28 +51,28 @@ print(clerics, 'Clerics,', fighters, 'Fighters,',
 
 #How many total Items?
 curs.execute("""
-             SELECT COUNT(item_id)
-             FROM armory_item;
-             """)
+    SELECT COUNT(item_id)
+    FROM armory_item;
+    """)
 items = curs.fetchall()[0][0]
 print('There are', items, 'itmes in total.')
 
 
 #How many of the Items are weapons? How many are not?
 curs.execute("""
-             SELECT COUNT(item_ptr_id)
-             FROM armory_weapon;
-             """)
+    SELECT COUNT(item_ptr_id)
+    FROM armory_weapon;
+    """)
 weapons = curs.fetchall()[0][0]
 print(weapons, 'weapons, and', items - weapons, 'non-weapons')
 
 
 #How many Items does each character have? (Return first 20 rows)
 curs.execute("""
-            SELECT COUNT(item_id), character_id
-            FROM charactercreator_character_inventory
-            GROUP BY character_id;
-             """)
+    SELECT COUNT(item_id), character_id
+    FROM charactercreator_character_inventory
+    GROUP BY character_id;
+    """)
 items_per_char = curs.fetchall()
 items_df = pd.DataFrame(items_per_char, columns = ['total_items', 'char_id'])
 print(items_df.head(20))
@@ -80,12 +80,11 @@ print(items_df.head(20))
 
 #How many Weapons does each character have? (Return first 20 rows)
 curs.execute("""
-            SELECT COUNT(item_ptr_id), character_id
-            FROM armory_weapon as aw, 
-            charactercreator_character_inventory as cci 
-            WHERE aw.item_ptr_id = cci.item_id
-            GROUP BY character_id;
-            """)
+    SELECT COUNT(item_ptr_id), character_id
+    FROM armory_weapon as aw, charactercreator_character_inventory as cci 
+    WHERE aw.item_ptr_id = cci.item_id
+    GROUP BY character_id;
+    """)
 weap_per_char = curs.fetchall()
 weapons_df = pd.DataFrame(weap_per_char, columns = ['total_weapons', 'character_id'])
 print(weapons_df.head(20))
