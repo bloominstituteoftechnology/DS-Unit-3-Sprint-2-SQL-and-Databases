66a2f1b0c5a9f9fff21bb8cdc30b267c0b9ee67e
ThisIsJorgeLima <ThisIsJorgeLima@users.noreply.github.com>
2020-01-22 05:55:28 -0500

    Final Commit

--- module1-introduction-to-sql/assignment.py
+++ module1-introduction-to-sql/assignment.py
@@ -26,60 +26,60 @@ print('The total number of items are:', curs.execute(query6).fetchone())
 
 """Total # of weapons"""
 query7 = 'SELECT COUNT(*) FROM armory_weapon'
-print('Total number of weapons:', curs.execute(query7).fetchone())
+print('The Total number of weapons are:', curs.execute(query7).fetchone())
 
 """Total # of items that are not weapons:"""
 print('The total number of Items that are not weapons are:', 174-37)
 
 """How many Items does each character have (first 20 rows)"""
 query9 = """
-SELECT COUNT(CCI.item_id)
-FROM charactercreator_character as CC,
-charactercreator_character_inventory as CCI
-WHERE CCI.character_id = CC.character_id
-GROUP BY CC.character_id;
+SELECT COUNT(cci.item_id)
+FROM charactercreator_character as cc,
+charactercreator_character_inventory as cci
+WHERE cci.character_id = cc.character_id
+GROUP BY cc.character_id;
 """
-print('Number of items per character:', curs.execute(query9).fetchmany(20))
+print('The number of items per character are:', curs.execute(query9).fetchmany(20))
 
 """How many weapons does each character have (first 20 rows)?"""
 query10 = """
 SELECT COUNT(AW.item_ptr_id)
-FROM charactercreator_character_inventory as CCI,
-charactercreator_character as CC,
-armory_item as AI,
-armory_weapon as AW
-WHERE CCI.character_id = CC.character_id
-AND CCI.item_id = AI.item_id
-AND AI.item_id = AW.item_ptr_id
-GROUP BY CC.character_id;
+FROM charactercreator_character_inventory as cci,
+charactercreator_character as cc,
+armory_item as ai,
+armory_weapon as aw
+WHERE cci.character_id = cc.character_id
+AND cci.item_id = ai.item_id
+AND ai.item_id = aw.item_ptr_id
+GROUP BY cc.character_id;
 """
-print('Number of weapons per character:', curs.execute(query10).fetchmany(20))
+print('The number of weapons per character are:', curs.execute(query10).fetchmany(20))
 
 """On average, how many Items does each character have?"""
 query11 = """
 SELECT AVG(avg_items)
 FROM(SELECT COUNT(CCI.item_id) as avg_items
-FROM charactercreator_character as CC,
-charactercreator_character_inventory as CCI
-WHERE CCI.character_id = CC.character_id
-GROUP BY CC.character_id) AS T;
+FROM charactercreator_character as cc,
+charactercreator_character_inventory as cci
+WHERE cci.character_id = CC.character_id
+GROUP BY cc.character_id) AS T;
 """
-print('Average num of items per character:', curs.execute(query11).fetchone())
+print('The avg. num of items per character are:', curs.execute(query11).fetchone())
 
 """On average, how many Weapons does each character have?"""
 query12 = """
 SELECT AVG(avg_weaps)
 FROM (SELECT COUNT(AW.item_ptr_id) as avg_weaps
-FROM charactercreator_character_inventory as CCI,
-charactercreator_character as CC,
-armory_item as AI,
-armory_weapon as AW
-WHERE CCI.character_id = CC.character_id
-AND CCI.item_id = AI.item_id
-AND AI.item_id = AW.item_ptr_id
-GROUP BY CC.character_id) AS Z;
+FROM charactercreator_character_inventory as cci,
+charactercreator_character as cc,
+armory_item as ai,
+armory_weapon as aw
+WHERE cci.character_id = CC.character_id
+AND cci.item_id = ai.item_id
+AND ai.item_id = aw.item_ptr_id
+GROUP BY cc.character_id) AS Z;
 """
-print('Average num of weapons per character:', curs.execute(query12).fetchone())
+print('The avg. num of weapons per character are:', curs.execute(query12).fetchone())
 
 curs.close()
 conn.commit()
