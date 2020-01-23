923165e06778c48b10fcb05bfc24be162fe7223f
KhaSab5292 <KhaSab5292@users.noreply.github.com>
2020-01-23 14:18:47 -0500

    stretch

--- module1-introduction-to-sql/buddymove_holidayiq.py
+++ module1-introduction-to-sql/buddymove_holidayiq.py
@@ -1,13 +1,13 @@
-import sqlite3
 import pandas as pd
+import sqlite3
+
 
 df = pd.read_csv('buddymove_holidayiq.csv')
 print(df.shape)
-print(df.isnull().sum())
-
 conn = sqlite3.connect('buddymove_holidayiq.sqlite3')
 curs = conn.cursor()
-#df.to_sql('review', conn)
+#df.to_sql('reviews', conn)(ONLY ONCE)
+
 
 #Count how many rows you have - it should be 249!
 curs.execute("""
@@ -16,6 +16,7 @@ curs.execute("""
     """)
 answer = curs.fetchall()[0][0]
 print('There are', answer, 'rows')
+
 
 #How many users who reviewed at least 100 Nature in the category 
 #also reviewed at least 100 in the Shopping category?
@@ -26,7 +27,20 @@ curs.execute("""
     AND Shopping >= 100;
     """)
 answer = curs.fetchall()[0][0]
-print(answer, 'Users have reviewed at least 100 for Nature and Shopping')
+print(answer, 'Users have reviewed at least 100 Nature and Shopping')
+
+
+#(Stretch) What are the average number of reviews for each category?
+curs.execute("""
+    SELECT AVG(Sports), AVG(Religious), AVG(Nature),
+    AVG(Theatre), AVG(Shopping), AVG(Picnic)
+    FROM review;
+    """)
+answer = curs.fetchall()
+average = pd.DataFrame(answer, columns = 
+    ['Sports', 'Religious', 'Nature', 'Theatre', 'Shopping', 'Picnic'])
+print(average)
+
 
 #Commit and close
 curs.close()
