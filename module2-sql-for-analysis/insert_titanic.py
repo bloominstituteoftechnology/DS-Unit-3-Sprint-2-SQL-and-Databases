36345210893d1e300c278711471f4dc0d381e6fe
KhaSab5292 <KhaSab5292@users.noreply.github.com>
2020-01-24 18:36:00 -0500

    last part of assignment

--- module2-sql-for-analysis/insert_titanic.py
+++ module2-sql-for-analysis/insert_titanic.py
@@ -4,19 +4,21 @@ import pandas as pd
 
 dbname = 'lsustohi'
 user = 'lsustohi'
-password = 'nCW8Zgf4Lp0oVej5-fs0dxQY9OEhyMhY'
+password = '?'
 host = 'rajje.db.elephantsql.com'
 
 #Create connections and cursors
 pg_conn = psycopg2.connect(dbname=dbname, user=user,
                            password=password, host=host)
 pg_curs = pg_conn.cursor()
-
 sl_conn = sqlite3.connect('titanic.sqlite3')
 sl_curs = sl_conn.cursor()
 
 df = pd.read_csv('titanic.csv')
-#df.to_sql('titanic', sl_conn) only once
+
+# Remove apostrophes from Name to prevent parsing erros
+df['Name'] = df['Name'].str.replace(r"[\"\',]", '')
+#df.to_sql('titanic', sl_conn)   Only Once
 
 
 # Extract titanic data
@@ -30,7 +32,7 @@ CREATE TABLE titanic (
     index SERIAL PRIMARY KEY,
     survived INT,
     pclass INT,
-    name VARCHAR(70),
+    name VARCHAR(90),
     sex VARCHAR(10),
     age INT,
     siblings_spouses_aboard INT,
@@ -38,5 +40,17 @@ CREATE TABLE titanic (
     fare REAL
 );
 """
+#pg_curs.execute(create_titanic_table)
+#pg_conn.commit()  Only Once   
 
+
+#Insert data into table
+for person in titanic_sql:
+    insert_person = """
+    INSERT INTO titanic
+    (survived, pclass, name, sex, age,
+    siblings_spouses_aboard, parents_children_aboard, fare)
+    VALUES """ + str(person[1:]) + ";"
+    pg_curs.execute(insert_person)
+#pg_conn.commit()  Only Once
 
