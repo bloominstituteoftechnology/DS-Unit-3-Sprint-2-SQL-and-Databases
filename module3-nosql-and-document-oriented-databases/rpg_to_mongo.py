fc84f84f54881f6e0f493b1d527bf81920c2e126
Doina Langille <doina.jitoreanu@gmail.com>
2020-02-19 19:04:49 -0500

    Module 3 assignment added

--- module3-nosql-and-document-oriented-databases/rpg_to_mongo.py
+++ module3-nosql-and-document-oriented-databases/rpg_to_mongo.py
@@ -1,3 +1,16 @@
+"""
+The difference between MongoDB and PostgreSQL
+is in the way you read and write the data.
+For example, when importing in PostgreSQL you look 
+at the entire row, while for Mongo you have to assign a value
+to each column.
+The advantage of Mongo is that you don't need a schema.
+However, having a well defined relational schema in postgres
+helps us retreiving more information and relationships between tables.
+I would say it's easier to work with postgres when having structured data,
+and to work with mongo when having unstructured data.
+"""
+
 import os
 import pymongo
 import sqlite3
