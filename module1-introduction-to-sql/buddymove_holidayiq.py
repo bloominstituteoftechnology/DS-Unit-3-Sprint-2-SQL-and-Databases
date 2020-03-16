fb30677c86d3256a561b8f6b9811e388ef6bec4e
austiezr <austinziech@gmail.com>
2020-03-16 13:11:36 -0600

    cleaning

--- module1-introduction-to-sql/buddymove_holidayiq.py
+++ module1-introduction-to-sql/buddymove_holidayiq.py
@@ -23,8 +23,10 @@ query = 'SELECT COUNT(*) FROM "buddymove_holidayiq.sqlite3"'
 print(f'Total Rows: {execute(query)[0][0]}\n')
 
 # Users with at least 100 nature and shopping reviews
-query = 'SELECT COUNT(*) FROM "buddymove_holidayiq.sqlite3" WHERE "Nature" >= 100 AND "Shopping" >= 100'
-print(f'Users With Minimum 100 Reviews in Nature and Shopping: {execute(query)[0][0]}\n')
+query = 'SELECT COUNT(*) FROM "buddymove_holidayiq.sqlite3" ' \
+        'WHERE "Nature" >= 100 AND "Shopping" >= 100'
+print(f'Users With Minimum 100 Reviews in Nature and Shopping: '
+      f'{execute(query)[0][0]}\n')
 
 # Average reviews for each category
 categories = ['Sports', 'Religious', 'Nature', 'Theatre', 'Shopping', 'Picnic']
--- module1-introduction-to-sql/rpg_queries.py
+++ module1-introduction-to-sql/rpg_queries.py
@@ -10,7 +10,7 @@ def execute(sql_query):
 
 # total characters
 query = 'SELECT COUNT(*) FROM charactercreator_character'
-print(f'Total Characters: {execute(query)}')
+print(f'Total Characters: {execute(query)[0][0]}')
 
 # subclass counts
 subclass = ['mage', 'thief', 'cleric', 'fighter', 'necromancer']
@@ -19,31 +19,37 @@ for x in subclass:
         query = f'SELECT COUNT(*) FROM charactercreator_{x}'
         query2 = 'SELECT COUNT(*) FROM charactercreator_necromancer'
         query3 = f'SELECT ({query}) - ({query2})'
-        print(f'Mage Count: {execute(query3)}')
+        print(f'Mage Count: {execute(query3)[0][0]}')
     else:
         query = f'SELECT COUNT(*) FROM charactercreator_{x}'
-        print(f'{x.capitalize()} Count: {execute(query)}')
+        print(f'{x.capitalize()} Count: {execute(query)[0][0]}')
 
 # total items
 query = 'SELECT COUNT(*) FROM armory_item'
-print(f'Total Items: {execute(query)}')
+print(f'Total Items: {execute(query)[0][0]}')
 
 # weapons vs not
 query = 'SELECT COUNT(*) FROM armory_weapon'
 query2 = 'SELECT COUNT(*) FROM armory_item'
 query3 = f'SELECT ({query2}) - ({query})'
-print(f'Weapon Count: {execute(query)}')
-print(f'Non-Weapon Item Count: {execute(query3)}')
+print(f'Weapon Count: {execute(query)[0][0]}')
+print(f'Non-Weapon Item Count: {execute(query3)[0][0]}\n')
 
 # Items per character
-query = 'SELECT character_id FROM charactercreator_character_inventory LIMIT 20'
-counts = {f'Character Id: {i[0]}': f'Item Count: {execute(query).count(i)}' for i in execute(query)}
+query = 'SELECT character_id ' \
+        'FROM charactercreator_character_inventory LIMIT 20'
+counts = {f'Character Id: {i[0]}': f'Item Count: {execute(query).count(i)}'
+          for i in execute(query)}
 print(counts)
 
 # Weapons per character
-query = 'SELECT character_id FROM charactercreator_character_inventory LEFT JOIN armory_weapon ON armory_weapon.item_ptr_id = charactercreator_character_inventory.item_id WHERE item_id = armory_weapon.item_ptr_id LIMIT 20'
-counts = {f'Character Id: {i[0]}': f'Item Count: {execute(query).count(i)}' for i in execute(query)}
-print(counts)
+query = 'SELECT character_id FROM charactercreator_character_inventory ' \
+        'LEFT JOIN armory_weapon ON armory_weapon.item_ptr_id = ' \
+        'charactercreator_character_inventory.item_id WHERE item_id = ' \
+        'armory_weapon.item_ptr_id LIMIT 20'
+counts = {f'Character Id: {i[0]}': f'Item Count: {execute(query).count(i)}'
+          for i in execute(query)}
+print(f'{counts}\n')
 
 # Average items per character
 query = 'SELECT COUNT(*) FROM charactercreator_character_inventory'
@@ -52,9 +58,10 @@ div = tuple(x / y for x, y in zip(execute(query)[0], execute(query2)[0]))
 print(f'Average Items Per Character: {div[0]}')
 
 # Average Weapons Per Character
-query = 'SELECT COUNT(*) FROM charactercreator_character_inventory LEFT JOIN armory_weapon ON armory_weapon.item_ptr_id = charactercreator_character_inventory.item_id WHERE item_id = armory_weapon.item_ptr_id'
+query = 'SELECT COUNT(*) FROM charactercreator_character_inventory ' \
+        'LEFT JOIN armory_weapon ON armory_weapon.item_ptr_id = ' \
+        'charactercreator_character_inventory.item_id WHERE item_id = ' \
+        'armory_weapon.item_ptr_id'
 query2 = 'SELECT COUNT(*) FROM charactercreator_character'
 div = tuple(x / y for x, y in zip(execute(query)[0], execute(query2)[0]))
 print(f'Average Weapons Per Character: {div[0]}')
-
-
