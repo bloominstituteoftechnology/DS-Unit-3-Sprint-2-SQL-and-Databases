ec38ab3419294a21269259e9d30a0c21a04ec9db
jacobpad <jacobpad@gmail.com>
2020-03-16 21:01:52 -0700

    Final Push After Running autopep8

--- module1-introduction-to-sql/buddymove_holidayiq.py
+++ module1-introduction-to-sql/buddymove_holidayiq.py
@@ -8,22 +8,22 @@ curs = con.cursor()
 df = pd.read_csv(url)
 df.to_sql('review', con=con)
 
-#-- Count how many rows you have - it should be 249!
+# -- Count how many rows you have - it should be 249!
 query1 = '''
 SELECT COUNT(*) FROM review;
 '''
 result1 = curs.execute(query1).fetchone()[0]
-print('\n****RESULT1:****\n-- Count how many rows you have - it should be 249!\n', result1, '\n')
+print(
+    '\n****RESULT1:****\n-- Count how many rows you have - it should be 249!\n',
+    result1,
+    '\n')
 
-#-- How many users who reviewed at least 100 Nature in 
+# -- How many users who reviewed at least 100 Nature in
 # the category also reviewed at least 100 in the Shopping category?
 query2 = '''
-SELECT count('User id') 
+SELECT count('User id')
 FROM review
 WHERE Nature >= 100 AND Shopping >= 100;
 '''
 result2 = curs.execute(query2).fetchone()[0]
 print('\n****RESULT2:****\n-- How many users who reviewed at least 100 Nature in the category also reviewed at least 100 in the Shopping category\n', result2, '\n')
-
-
-
