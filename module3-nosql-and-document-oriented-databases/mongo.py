78597add1db02665051de6845f3575f1dee61759
austiezr <austinziech@gmail.com>
2020-03-18 11:57:46 -0600

    finished adding rpg to mongodb

---
+++ module3-nosql-and-document-oriented-databases/mongo.py
@@ -0,0 +1,23 @@
+import pymongo
+import os
+from dotenv import load_dotenv
+import pandas as pd
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+
+client = pymongo.MongoClient(f'mongodb+srv://{DB_USER}:{DB_PASS}@lambda-qip5e.mongodb.net/test?retryWrites=true&w=majority')
+
+db = client.rpg
+
+df = pd.read_json('testdata.json')
+
+for model in df['model'].unique():
+    data = df[df['model'] == model]
+    data_as_list = data['fields'].tolist()
+    collection = db[f'{model}']
+    collection.insert_many(data_as_list)
+
+
