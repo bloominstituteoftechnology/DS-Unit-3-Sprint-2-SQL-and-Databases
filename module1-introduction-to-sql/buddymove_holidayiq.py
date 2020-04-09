2dc4fdbdb5052dc017436319111a76621656e107
jpeone <jesse.gene.peone@gmail.com>
2020-04-08 18:33:51 -0700

    Setup TODOs for MVP

---
+++ module1-introduction-to-sql/buddymove_holidayiq.py
@@ -0,0 +1,23 @@
+
+
+
+#Load data using pandas from buddymove_holidayiq.csv
+#do this platform agnostic, expect 249X7 shape
+#TODO
+
+#Open a connection to new db buddy_move_holidayiq.sqlite3
+#TODO
+
+#us df.to_sql to insert data into a new table titled review
+#TODO
+
+#SQL queries
+#how many rows in the db?
+#TODO
+
+#How many users who reviewed at least 100 Nature in the category also reviewed 
+#at least 100 in the Shopping category?
+#TODO
+
+#Stretch what are the average number of reviews for each category?
+#TODO
---
+++ module1-introduction-to-sql/rpg_queries.py
@@ -0,0 +1,29 @@
+#using SQL queries answer the folowing
+
+
+#How may total Characters are there?
+#TODO
+
+#How many of each subclass?
+#TODO
+
+#How many total Items?
+#TODO
+
+#How many of the items are weapons?
+#TODO
+
+#How many of the items are not weapons?
+#TODO
+
+#How many items does each character have? First 20 rows
+#TODO
+
+#How many Weapons does each character have? First 20 rows
+#TODO
+
+#Average, how many items does each Character have?
+#TODO
+
+#Average, how many weapons does each character have?
+#TODO
