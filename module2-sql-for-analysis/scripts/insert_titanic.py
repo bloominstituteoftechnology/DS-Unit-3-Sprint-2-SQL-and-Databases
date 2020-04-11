7565031524577971239ac33cc0dbe5da492c3031
jpeone <jesse.gene.peone@gmail.com>
2020-04-10 19:18:24 -0700

    organizing things

--- module2-sql-for-analysis/scripts/insert_titanic.py
+++ module2-sql-for-analysis/scripts/insert_titanic.py
@@ -16,17 +16,21 @@ con = psycopg2.connect(dbname = NAME,
                         password = PASSWORD, 
                         host = HOST)
 
-cur = connection.cursor()
+cur = con.cursor()
 
 # sanity checks comment out later
 print('Con:', con)
 print('Cur:', cur)
 
 # set up the os agnostic path
-# TODO
+TITANIC_PATH = os.path.join(os.path.dirname(__file__), 'titanic.csv')
 
 # Load into pandas
+df = pd.read_csv(TITANIC_PATH)
+
+# for MVP, just dump all the titanic CSV into your postgres db
 # TODO
 
-# inspect and evaluate tables to be made
-# TODO
+# Stretch actually split this up into appropriate tables
+# like a passenger table, survivors table, class tables, etc
+# TODO 
