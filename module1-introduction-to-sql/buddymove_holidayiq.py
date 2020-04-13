1012b98958b596ea0e3e5aaca539d9bdcbba0aa4
Kyle Yates <53956594+KyleTy1er@users.noreply.github.com>
2020-04-13 15:36:06 -0700

    added files

--- module1-introduction-to-sql/buddymove_holidayiq.py
+++ module1-introduction-to-sql/buddymove_holidayiq.py
@@ -30,3 +30,32 @@ print ("row count of sql db: ", results)
 # results1 = engine.execute(query1).fetchall()
 
 # print (results1)
+
+
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite:///buddymove_holidayiq.sqlite3', echo=False)
+
+df.to_sql('review', con=engine)
+
+conn = sqlite3.connect(FILEPATH2)
+
+curs = conn.cursor()
+
+query = "SELECT count(*) FROM review"
+
+results = engine.execute(query).fetchall()
+
+print ("row count of sql db: ", results)
+
+print (type(df))
+print (type(df2))
+#  - How many users who reviewed at least 100 `Nature` in the category also
+#   reviewed at least 100 in the `Shopping` category?
+
+# query1 = "SELECT `User Id` `Nature` `Shopping` FROM review WHERE Nature > 99 and Shopping > 99"
+
+
+# results1 = engine.execute(query1).fetchall()
+
+# print (results1)
