06ad673021fb874cb1fbee8d10728908e8a713e6
Kyle Yates <53956594+KyleTy1er@users.noreply.github.com>
2020-04-13 16:33:12 -0700

    assignment finished

--- module1-introduction-to-sql/buddymove_holidayiq.py
+++ module1-introduction-to-sql/buddymove_holidayiq.py
@@ -3,7 +3,6 @@ import pandas as pd
 import os
 
 FILEPATH = os.path.join(os.path.dirname(__file__),"buddymove_holidayiq.csv")
-FILEPATH2 = os.path.join(os.path.dirname(__file__),"buddymove_holidayiq.sqlite3")
 
 df = pd.read_csv(FILEPATH)
 
@@ -11,51 +10,23 @@ print ("shape of pandas df: ", df.shape)
 
 from sqlalchemy import create_engine
 
-engine = create_engine('sqlite://, echo=False)
-
-df.to_sql('review', con=engine)
-
-query = "SELECT count(*) FROM review"
-
-results = engine.execute(query).fetchall()
-
-print ("row count of sql db: ", results)
-
-# #  - How many users who reviewed at least 100 `Nature` in the category also
-# #   reviewed at least 100 in the `Shopping` category?
-
-# query1 = "SELECT SUM(Id) FROM review WHERE Nature > 99 and Shopping > 99"
-
-
-# results1 = engine.execute(query1).fetchall()
-
-# print (results1)
-
-
-from sqlalchemy import create_engine
-
 engine = create_engine('sqlite:///buddymove_holidayiq.sqlite3', echo=False)
 
-df.to_sql('review', con=engine)
+df.to_sql('review', con=engine, if_exists='replace')
 
-conn = sqlite3.connect(FILEPATH2)
+results = engine.execute("SELECT count(*) FROM review").fetchall()
 
-curs = conn.cursor()
+print("row count of sql db: ", results)
 
-query = "SELECT count(*) FROM review"
+query = '''
+SELECT
+COUNT(DISTINCT "index")
+FROM review
+WHERE Nature >= 100 AND Shopping >= 100
+;
+'''
 
-results = engine.execute(query).fetchall()
+results2 = engine.execute(query).fetchall()
 
-print ("row count of sql db: ", results)
+print("users who reviewed at least 100 Nature and Shopping: ", results2[0][0])
 
-print (type(df))
-print (type(df2))
-#  - How many users who reviewed at least 100 `Nature` in the category also
-#   reviewed at least 100 in the `Shopping` category?
-
-# query1 = "SELECT `User Id` `Nature` `Shopping` FROM review WHERE Nature > 99 and Shopping > 99"
-
-
-# results1 = engine.execute(query1).fetchall()
-
-# print (results1)
