c95e2ae47fcce7e433e6d5ca364afa16d986bc4c
masonnystrom <masonnystrom@gmail.com>
2020-04-14 18:10:30 -0400

    update files

---
+++ module2-sql-for-analysis/titanic.py
@@ -0,0 +1,24 @@
+import os
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import pandas as pd
+import psycopg2
+from psycopg2.extras import execute_values
+import glob
+
+load_dotenv() # looks inside the .env file for some env vars
+# passes env var values to python var
+
+DB_HOST = os.getenv("DB_HOST", default="OOPS")
+DB_NAME = os.getenv("DB_NAME", default="OOPS")
+DB_USER = os.getenv("DB_USER", default="OOPS")
+DB_PASSWORD = os.getenv("DB_PASSWORD", default="OOPS")
+DB_PORT =  os.getenv("DB_PORT", default="OOPS") 
+
+for file in glob.glob('Client*.csv'):
+    df = pd.read_csv(titanic.csv)
+
+print(titanic.shape)
+sql_url = f'postgres://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+conn = create_engine(sql_url)
+titanic.to_sql('titanic', if_exists='replace', con=conn, index=False)
