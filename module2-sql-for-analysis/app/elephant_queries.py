a04e4ae2b0a0de90ae8c256132310bf87306f625
dougscohen <dougscohen@gmail.com>
2020-04-14 14:07:35 -0400

    worked on queries

--- module2-sql-for-analysis/app/elephant_queries.py
+++ module2-sql-for-analysis/app/elephant_queries.py
@@ -1,6 +1,8 @@
 import os
 import psycopg2
+import json
 from dotenv import load_dotenv
+from psycopg2.extras import execute_values
 
 # adds contents of the .env file to our enviornment
 # looking in the .env file for env variables
@@ -20,9 +22,37 @@ print(type(conn))
 cur = conn.cursor()
 print(type(cur))
 
+
+#
+# INSERT SOME DATA
+#
+
+my_dict = { "a": 1, "b": ["dog", "cat", 42], "c": 'true' }
+
+# insertion_query = f"INSERT INTO test_table (name, data) VALUES (%s, %s)"
+# cur.execute(insertion_query,
+#  ('A rowwwww', 'null')
+# )
+# cur.execute(insertion_query,
+#  ('Another row, with JSONNNNN', json.dumps(my_dict))
+# )
+
+# h/t: https://stackoverflow.com/questions/8134602/psycopg2-insert-multiple-rows-with-one-query
+insertion_query = f"INSERT INTO test_table (name, data) VALUES %s"
+execute_values(cur, insertion_query, [
+ ('A rowwwww', 'null'),
+ ('Another row, with JSONNNNN', json.dumps(my_dict)),
+ ('Third row', "3")
+])
+
+
 ### An example query
 cur.execute('SELECT * from test_table;')
 
 ### Note - nothing happened yet! We need to actually *fetch* from the cursor
-results = cur.fetchone()
+print('----------')
+results = cur.fetchall()
 print(results)
+
+# ACTUALLY SAVE THE TRANSACTIONS
+conn.commit()
