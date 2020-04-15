700503f3556a4e9b8012634c3fcb9ef0b742b1b3
tmbern <tmbern42@gmail.com>
2020-04-15 10:10:12 -0600

    updated titanic file

--- app/insert_titanic.py
+++ app/insert_titanic.py
@@ -12,28 +12,58 @@ CSV_filepath = os.path.join(os.path.dirname(__file__), "..", "module2-sql-for-an
 df = pd.read_csv(CSV_filepath)
 print(df.head())
 
-# DB_NAME = os.getenv("DB_NAME_DB13_Unit3")
-# DB_USER = os.getenv("DB_USER__DB13_Unit3")
-# DB_PASSWORD = os.getenv("DB_PASSWORD__DB13_Unit3")
-# DB_HOST = os.getenv("DB_HOST__DB13_Unit3")
+# Update column types so we can transfer data to a postgreSQL Database.
+int_columns = ['Survived','Pclass', 'Siblings/Spouses Aboard', 'Parents/Children Aboard']
 
-# connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
+for col in int_columns:
+  df[col] = df[col].astype(float)
 
+# move the data to an iterable tuple list that we can insert to DB
+titanic_list = df.to_records(index=False)
 
-# # create the table for using the columns from the titanic dataset
-# table_name = "test_table2"
+# Get the credentials from env to login to postgreSQL database
+load_dotenv()
+DB_NAME = os.getenv("DB_NAME_DB13_Unit3")
+DB_USER = os.getenv("DB_USER__DB13_Unit3")
+DB_PASSWORD = os.getenv("DB_PASSWORD__DB13_Unit3")
+DB_HOST = os.getenv("DB_HOST__DB13_Unit3")
 
-# query = """
-# CREATE TABLE IF NOT EXISTS table_name(
-#   id SERIAL PRIMARY KEY,
-#   Survived BOOLEAN,
-#   Pclass INTEGER CHECK (Pclass > 0 AND Pclass <= 3 ),
-#   Name VARCHAR(100),
-#   Sex VARCHAR(10) CHECK (Sex in ('male', 'female')),
-#   Age FLOAT(8),
-#   Sibilings_Spouses_Aboard INTEGER,
-#   Parents_Children_Aboard INTEGER,
-#   Fare FLOAT(8)
-# );
-# """
+# create connection to PostgrSQL
+connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
 
+# create the cursor
+cursor = connection.cursor()
+
+# create the query to create a new titanic table in database
+query = """
+CREATE TABLE IF NOT EXISTS titanic_table(
+  id SERIAL PRIMARY KEY,
+  Survived INT CHECK (Survived = 0 OR Survived = 1),
+  Pclass INT CHECK (Pclass > 0 AND Pclass <= 3 ),
+  Name VARCHAR(100),
+  Sex VARCHAR(10) CHECK (Sex in ('male', 'female')),
+  Age FLOAT(8),
+  Sibilings_Spouses_Aboard INT,
+  Parents_Children_Aboard INT,
+  Fare FLOAT(8)
+);
+"""
+
+# execute the query and commit the table to the database
+cursor.execute(query)
+connection.commit()
+
+# create an insertion query to populate the table
+insert_query = """
+INSERT INTO titanic_table 
+(Survived, Pclass, Name, Sex, Age, Sibilings_Spouses_Aboard, Parents_Children_Aboard, Fare)
+VALUES %s;
+"""
+
+# execute the insert query and save the data to the table in the DB
+execute_values(cursor, insert_query, titanic_list)
+connection.commit()
+
+# Close the connection
+cursor.close()
+connection.close()
