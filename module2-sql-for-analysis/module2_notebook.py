184e11339afcae2452000676a8b7e72a6a6aa2b1
Vanagand <michlaporte15@gmail.com>
2020-04-15 17:42:58 -0300

    Assignment 2 update

---
+++ module2-sql-for-analysis/module2_notebook.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv
+import psycopg2
+
+# pipenv install psycopg2-binary
+
+load_dotenv() #> loads contents of the .env file into the script's environment
+
+DB_NAME = "pujosgiv"
+DB_USER = "pujosgiv"
+DB_PASSWORD = "***REMOVED***"
+DB_HOST = "drona.db.elephantsql.com"
+
+connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
+print("CONNECTION:", connection)
+
+create_table_statement = '''
+CREATE TABLE test_table (
+  id        SERIAL PRIMARY KEY,
+  name  varchar(40) NOT NULL,
+  data    JSONB
+);
+'''
+
+# cursor = connection.cursor()
+# print("CURSOR:", cursor)
+
+# cursor.execute('SELECT * from test_table;')
+# result = cursor.fetchall()
+# print("RESULT:", type(result))
+# print(result)
+
+# connection
