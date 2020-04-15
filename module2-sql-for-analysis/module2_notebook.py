1c81621d10ab316027f4cf668b2b8dba2514e1c5
Vanagand <michlaporte15@gmail.com>
2020-04-15 20:05:12 -0300

    Pre BFG line edit

--- module2-sql-for-analysis/module2_notebook.py
+++ module2-sql-for-analysis/module2_notebook.py
@@ -8,11 +8,7 @@ load_dotenv() #> loads contents of the .env file into the script's environment
 
 DB_NAME = "pujosgiv"
 DB_USER = "pujosgiv"
-<<<<<<< HEAD
 DB_PASSWORD = ""
-=======
-DB_PASSWORD = "***REMOVED***"
->>>>>>> cedd2009ed0ca6cc203939c93b5d606f605bff42
 DB_HOST = "drona.db.elephantsql.com"
 
 connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
