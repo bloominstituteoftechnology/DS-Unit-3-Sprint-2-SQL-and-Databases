faaa03f3c9bf0abfba5185bb8435213cec62d4ee
adxpillar <dsa.adeagbo@gmail.com>
2020-04-15 11:08:07 -0400

    new query

--- module1-introduction-to-sql/app/rpg_queries.py
+++ module1-introduction-to-sql/app/rpg_queries.py
@@ -26,7 +26,27 @@ FROM (
     LEFT JOIN charactercreator_character_inventory i ON c.character_id = i.character_id
     LEFT JOIN armory_weapon w ON i.item_id = w.item_ptr_id
     GROUP BY c.character_id
-) subq
+) subq;
+
+SELECT
+  CASE
+  WHEN cl.character_ptr_id is not null THEN "cleric"
+  WHEN f.character_ptr_id is not null THEN "fighter"
+  WHEN n.mage_ptr_id is not null THEN "mage-necro"
+  WHEN m.character_ptr_id is not null THEN "mage"
+  WHEN th.character_ptr_id is not null THEN "thief"
+  ELSE "todo"
+  END as char_type
+  ,count(distinct ch.character_id) as char_count
+from charactercreator_character as ch
+left join charactercreator_cleric as cl on ch.character_id = cl.character_ptr_id
+left join charactercreator_fighter as f on ch.character_id = f.character_ptr_id
+left join charactercreator_mage as m on ch.character_id = m.character_ptr_id
+left join charactercreator_thief as th on ch.character_id = th.character_ptr_id
+-- left join charactercreator_necromancer as n on ch.character_id = n.character_ptr_id
+left join charactercreator_necromancer as n on m.character_ptr_id = n.mage_ptr_id
+group by char_type
+
 """
 
 result = cursor.execute(query).fetchall()
