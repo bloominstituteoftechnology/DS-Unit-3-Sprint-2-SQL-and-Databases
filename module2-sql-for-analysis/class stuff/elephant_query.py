79d48f66395b26e5664a7e812fa491cb3361a2a3
hkang42 <h.kang.q@gmail.com>
2020-04-16 18:15:24 -0400

    redid class 2 material

--- module2-sql-for-analysis/class stuff/elephant_query.py
+++ module2-sql-for-analysis/class stuff/elephant_query.py
@@ -1,14 +1,71 @@
 import psycopg2
+import os
+import json
+"""
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DBNAME = os.getenv("DB_NAME")
+DBUSER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_HOST = os.getenv("DB_HOST")
+"""
+
+DBNAME = 'rsswtvhb'
+DBUSER = "rsswtvhb"
+DB_PASS = "qImj0x6_ofnmgQYeEY9VD2q-93dNW1pt"
+DB_HOST = 'drona.db.elephantsql.com'
 
 ### Connect to ElephantSQL-hosted PostgreSQL
-conn = psycopg2.connect(dbname=DBNAME, user='rsswtvhb',
-                        password=DB_PASS, host=DB_HOST)
+conn = psycopg2.connect(dbname=DBNAME, user=DBUSER,
+                        password=DB_PASS, 
+                        host=DB_HOST)
 ### A "cursor", a structure to iterate over db records to perform queries
 cur = conn.cursor()
-### An example query
+
+
+table_name = "test_table"
+
+my_dict = {"a":1, "b":['dog', 'cat', 42], "c":True}
+
+# create format for our query
+insertion_query = f"INSERT INTO {table_name} (name, data) VALUES (%s, %s)"
+"""
+# use format to insert a row
+cur.execute(insertion_query, ("a row", 'null'))
+
+# use format to insert another row
+cur.execute(insertion_query, ("another row", json.dumps(my_dict) ) )
+"""
+
+
+
+# let's try to do the 2 insertions from above using a loop
+"""
+row_list = [("a row", 'null') , ("another row", json.dumps(my_dict) ) ]
+
+for row in row_list:
+    cur.execute(insertion_query, query)
+"""
+
+# let's try to do the 2 insertions in a single command
+from psycopg2.extras import execute_values
+
+# to use this function, we need to remove one of the placeholders
+new_insertion_query = f"INSERT INTO {table_name} (name, data) VALUES %s"
+
+row_list = [("a row", 'null') , ("another row", json.dumps(my_dict) ) ]
+
+execute_values(cur, new_insertion_query, row_list)
+
+
 cur.execute('SELECT * from test_table;')
-### Note - nothing happened yet! We need to actually *fetch* from the cursor
+
 results = cur.fetchall()
 
 for line in results:
     print(line)
+
+conn.commit()
+
