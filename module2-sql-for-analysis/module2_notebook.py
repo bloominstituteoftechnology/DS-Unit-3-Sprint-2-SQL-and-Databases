36a91239d454414f7dcd71f85948075e65601306
Vanagand <michlaporte15@gmail.com>
2020-04-16 15:18:03 -0300

    Dotenv update

--- module2-sql-for-analysis/module2_notebook.py
+++ module2-sql-for-analysis/module2_notebook.py
@@ -1,14 +1,15 @@
 # module2-sql-for-analysis/module2_notebook.py
+import os
 import psycopg2
-
+from dotenv import load_dotenv
 # pipenv install psycopg2-binary
 
-# load_dotenv() #> loads contents of the .env file into the script's environment
+load_dotenv() #> loads contents of the .env file into the script's environment
 
-DB_NAME = "pujosgiv"
-DB_USER = "pujosgiv"
-DB_PASSWORD = ""
-DB_HOST = "drona.db.elephantsql.com"
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_NAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
 
 connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
 print("CONNECTION:", connection)
