1ba2be22d7fd2aa748e67b629be47e932db0c529
dougscohen <dougscohen@gmail.com>
2020-04-16 18:06:10 -0400

    went back and worked with postgres queries'

--- module2-sql-for-analysis/insert_titanic.py
+++ module2-sql-for-analysis/insert_titanic.py
@@ -16,23 +16,45 @@ print(df.head()) # print the head to ensure right format
 
 print("----------")
 # Open a connection to ElephantSQL postgreSQL database
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME2")
+DB_USER = os.getenv("DB_USER2")
+DB_PASSWORD = os.getenv("DB_PASSWORD2")
+DB_HOST = os.getenv("DB_HOST2")
 connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER,
                         password=DB_PASSWORD, host=DB_HOST)
 print("CONNECTION:", connection)
 
 cursor = connection.cursor()
+print("CURSOR", cursor)
 
 # Create engine to use for df.to_sql
-engine = create_engine('postgres://dyuqpkft:flXZLz...@drona.db.elephantsql.com\
-:5432/dyuqpkft')
+engine = create_engine('postgres://suerwyip:mNWaqz...@drona.db.elephantsql.com\
+:5432/suerwyip')
 
 # Use df.to_sql to insert the data into a new table 'titanic' in the PostgrSQL 
 #. database
 df.to_sql('titanic', engine)
 conn = engine.raw_connection()
+
+# How many passengers survived?
+number_survived_query = '''
+SELECT  count(survived) as count_survived
+FROM    passengers
+WHERE   survived = 1;
+'''
+total_survivors = cursor.execute(number_survived_query).fetchone()
+print("----------")
+print('Total # of Survivors:', total_survivors[0])
+
+# How many passengers died?
+number_died_query = '''
+SELECT  count(survived) as count_died
+FROM    passengers
+WHERE   survived = 0;
+'''
+total_dead = cursor.execute(number_died_query).fetchone()
+print("----------")
+print('Total # of People who Died:', total_dead[0])
+
 
 conn.commit()
