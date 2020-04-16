6cbc3d1dcffd8fb051f8feb2dddfe845e08193bc
Kyle Yates <53956594+KyleTy1er@users.noreply.github.com>
2020-04-16 09:44:08 -0700

    added SS

--- SC_SS.py
+++ SC_SS.py
@@ -9,35 +9,53 @@ Have fun studying!
 ​
 ## Questions of understanding
 1. What is SQL?
+
+        # Structured Query Language - a language for interacting with data bases.
 2. What is a RDBMS?
+
+        # Relation Database Mgmt System - A database mgmt system designed for relational databases - data that is stored in
+        # a structured format using rows and columns with data that is "related".
 3. What is the ETL pipeline?
+        # Set of processing elements that moves data from one system to another.
+
 4. What is a schema?
+        # The organization of the data in a database.
+
 5. What does each letter in ACID stand for? Give an explanation for each and why they matter?
- - A
- - C
- - I
- - D
+ - Atomicity -  if any of the statements constituting a transaction fails to complete, the entire transaction fails and the database is left unchanged.
+ - Consistency - Consistent (the change can only happen if the new state of the system will be valid; 
+                any attempt to commit an invalid change will fail, leaving the system in its previous valid state
+ - Isolation - ensures that concurrent execution of transactions leaves the database in the same state that would have been obtained if the transactions were executed sequentially
+ - Durability - guarantees that once a transaction has been committed, it will remain committed even in the case of a system failure (e.g., power outage or crash).
+
 6. Explain each of the table relationships and give an example for each
- - One-to-One
- - One-to-Many
- - Many-to-Many
+ - One-to-One: One item in a table is equally represented by one item in another table... character_id to character_id
+ - One-to-Many: One item in a table is represented by multiple items in a another table... birthday to many people having that birthday. A customers purchases.
+ - Many-to-Many: customers can purchase various products, and products can be purchased by many customers.
 ​
 ## Syntax
 For the following section, give a brief explanation of each of the SQL commands.
 ​
-1. **SELECT** - 
-2. **WHERE** - 
-3. **LIMIT** - 
-4. **ORDER** -
-5. **JOIN** -
-6. **CREATE TABLE** - 
-7. **INSERT** -
-8. **DISTINCT** -
-9. **GROUP BY** -
-10. **ORDER BY** -
-11. **AVG** - 
-12. **MAX** -
-13. **AS** -
+1. **SELECT** -  SELECT statements are used to fetch data from a database. Every query will begin with SELECT.
+2. **WHERE** -  WHERE is a clause that indicates you want to filter the result set to include only rows where the following condition is true.
+3. **LIMIT** - LIMIT is a clause that lets you specify the maximum number of rows the result set will have.
+4. **ORDER** - ORDER BY is a clause that indicates you want to sort the result set by a particular column either alphabetically or numerically.
+5. **JOIN** - An inner join will combine rows from different tables if the join condition is true. 
+            An outer join will combine rows from different tables even if the join condition is not met. Every row in the left table is returned in the result set, 
+            and if the join condition is not met, then NULL values are used to fill in the columns from the right table.
+            The SQL LEFT JOIN returns all rows from the left table, even if there are no matches in the right table. This means that if the ON 
+            clause matches 0 (zero) records in the right table; the join will still return a row in the result, but with NULL in each column from the right table.
+            The SQL RIGHT JOIN returns all rows from the right table, even if there are no matches in the left table. This means that if the ON 
+            clause matches 0 (zero) records in the left table; the join will still return a row in the result, but with NULL in each column from the left table.
+6. **CREATE TABLE** - CREATE TABLE creates a new table in the database. It allows you to specify the name of the table and the name of each column in the table.
+7. **INSERT** - INSERT statements are used to add a new row to a table.
+8. **DISTINCT** - Unique values of a column can be selected using a DISTINCT query.
+9. **GROUP BY** - The GROUP BY clause is an optional clause of the SELECT statement that combines rows into groups based on 
+            matching values in specified columns. One row is returned for each group.
+10. **ORDER BY** - you specify the column in which you want to sort and the kind of the sort order - ASC and DESC
+11. **AVG** - The SQL AVG function is an aggregate function that calculates the average value of a set. 
+12. **MAX** - SQL provides the MAX function that allows you to find the maximum value in a set of values.
+13. **AS** - The AS command is used to rename a column or table with an alias.
 ​
 ## Starting From Scratch
 Create a file named `study_part1.py` and complete the exercise below. The only library you should need to import is `sqlite3`. Don't forget to be PEP8 compliant!
