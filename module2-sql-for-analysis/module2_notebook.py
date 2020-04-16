a001a1a2009c26080836098931c51d89b032b1e2
Vanagand <michlaporte15@gmail.com>
2020-04-16 14:18:54 -0300

    Module 2 work update

--- module2-sql-for-analysis/module2_notebook.py
+++ module2-sql-for-analysis/module2_notebook.py
@@ -1,3 +1,5 @@
+# module2-sql-for-analysis/module2_notebook.py
+
 import os
 from dotenv import load_dotenv
 import psycopg2
@@ -6,13 +8,21 @@ import psycopg2
 
 load_dotenv() #> loads contents of the .env file into the script's environment
 
-DB_NAME = "pujosgiv"
-DB_USER = "pujosgiv"
-DB_PASSWORD = ""
-DB_HOST = "drona.db.elephantsql.com"
+DB_NAME = os.getenv("pujosgiv")
+DB_USER = os.getenv("pujosgiv")
+DB_PASSWORD = os.getenv("")
+DB_HOST = os.getenv("drona.db.elephantsql.com")
 
 connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
 print("CONNECTION:", connection)
+
+cursor = connection.cursor()
+print("CURSOR:", cursor)
+
+cursor.execute('SELECT * from test_table;')
+result = cursor.fetchall()
+print("RESULT:", type(result))
+print(result)
 
 create_table_statement = '''
 CREATE TABLE test_table (
@@ -21,6 +31,24 @@ CREATE TABLE test_table (
   data    JSONB
 );
 '''
+
+# insert_statement = '''
+# INSERT INTO test_table (name, data) VALUES
+# (
+#   'A row name',
+#   null
+# ),
+# (
+#   'Another row, with JSON',
+#   '{ "a": 1, "b": ["dog", "cat", 42], "c": true }'::JSONB
+# );
+# '''
+
+# cursor.executemany(create_table_statement)
+# cursor.commit()
+
+# cursor.execute(create_table_statement)
+# cursor.fetchone()
 
 # cursor = connection.cursor()
 # print("CURSOR:", cursor)
