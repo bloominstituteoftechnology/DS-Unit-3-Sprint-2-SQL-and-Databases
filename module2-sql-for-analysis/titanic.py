bc3d254cbd15c63582fbb725e4ae91696b3314e5
Fatai King <kinggy89@gmail.com>
2020-05-13 10:21:20 -0400

    hw2

--- module2-sql-for-analysis/titanic.py
+++ module2-sql-for-analysis/titanic.py
@@ -26,34 +26,6 @@ connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER,
                         host=DB_HOST)
 print("CONNECTION: ", connection)
 
-# Create engine to use for df.to_sql
-#engine = create_engine('postgres://suerwyip:mNWaqz...@drona.db.elephantsql.com\
-#:5432/suerwyip')
 
-# Use df.to_sql to insert the data into a new table 'titanic' in the PostgrSQL 
-#. database
-df.to_sql('titanic', engine)
-conn = engine.raw_connection()
-
-# How many passengers survived?
-number_survived_query = '''
-SELECT  count(survived) as count_survived
-FROM    titanic
-WHERE   survived = 1;
-'''
-total_survivors = cursor.execute(number_survived_query).fetchone()
-print("----------")
-print('Total # of Survivors:', total_survivors[0])
-
-# How many passengers died?
-number_died_query = '''
-SELECT  count(survived) as count_died
-FROM    titanic
-WHERE   survived = 0;
-'''
-total_dead = cursor.execute(number_died_query).fetchone()
-print("----------")
-print('Total # of People who Died:', total_dead[0])
-
-
-conn.commit()
+connection.commit()
+connection.close
