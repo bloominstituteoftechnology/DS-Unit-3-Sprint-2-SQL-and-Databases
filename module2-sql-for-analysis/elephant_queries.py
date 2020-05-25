710e1e919c1dd88271c448e0521590066487d61a
John Dailey <johnjdailey@gmail.com>
2020-05-25 05:13:23 -0400

    SQL for Analysis - Assignment Complete

--- module2-sql-for-analysis/elephant_queries.py
+++ module2-sql-for-analysis/elephant_queries.py
@@ -1,16 +1,16 @@
 # app/elephant_queries.py
 
 import os
-from dotenv import load_dotenv
 import psycopg2
+from dotenv import load_dotenv
 from psycopg2.extras import DictCursor
 
 load_dotenv() #> loads contents of the .env file into the script's environment
 
-DB_NAME = os.getenv("DB_NAME", defaults="OOPS")
-DB_USER = os.getenv("DB_USER", defaults="OOPS")
-DB_PASSWORD = os.getenv("DB_PASSWORD", defaults="OOPS")
-DB_HOST = os.getenv("DB_HOST", defaults="OOPS")
+DB_NAME = os.getenv("DB_NAME", default="OOPS")
+DB_USER = os.getenv("DB_USER", default="OOPS")
+DB_PASSWORD = os.getenv("DB_PASSWORD", default="OOPS")
+DB_HOST = os.getenv("DB_HOST", default="OOPS")
 
 connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
 print("CONNECTION:", connection)
