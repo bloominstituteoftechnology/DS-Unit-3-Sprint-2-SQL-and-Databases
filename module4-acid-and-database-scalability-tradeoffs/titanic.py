484de1902b3785b68a0f598ca75e32d67e6ba09a
Johnny Ilmo Koo <johnnykoo84@gmail.com>
2020-05-31 20:55:49 +0900

    Still doing module 4

--- module4-acid-and-database-scalability-tradeoffs/titanic.py
+++ module4-acid-and-database-scalability-tradeoffs/titanic.py
@@ -1,5 +1,5 @@
-import os
 import pandas as pd
+import os
 import psycopg2
 from psycopg2 import extras
 from dotenv import load_dotenv
@@ -21,6 +21,10 @@ psq_dbname = os.getenv('dbname')
 psq_user = os.getenv('user')
 psq_password = os.getenv('password')
 psq_host = os.getenv('host')
+
+mongo_username = os.getenv('mongo_username')
+mongo_password = os.getenv('mongo_password')
+mongo_host = os.getenv('mongo_host')
 
 # Connect to ElephantSQL-hosted PostgreSQL
 psq_conn = psycopg2.connect(
@@ -53,13 +57,48 @@ list_of_tuples = list(df.to_records(index=False))
 ins_query = 'INSERT INTO Titanic (Survived, Pclass, Name, Sex, Age, SiblingsSpouses, ParentsChildren, Fare) VALUES %s;'
 extras.execute_values(cur, ins_query, list_of_tuples)
 
-conn.commit()
+psq_conn.commit()
 
 # check
 cur.execute('SELECT * FROM Titanic')
-print('second fetch', cur.fetchall())
+psql_result = cur.fetchall()
+# print('second fetch', psql_result)
+# print('type psql_result', type(psql_result))
+
+# for mongoDB
+client = pymongo.MongoClient(
+    f"mongodb+srv://{mongo_username}:{mongo_password}@{mongo_host}/test?retryWrites=true&w=majority")
+db = client.test
+
+list_data = []
+
+for row in psql_result:
+    obj = {
+        'survived': row[0],
+        'pclass': row[1],
+        'name': row[2],
+        'sex': row[3],
+        'age': row[4],
+        'siblings_spouse_aboard': row[5],
+        'parents_children_abord': row[6],
+        'Fare': row[7],
+    }
+    list_data.append(obj)
+db['Titanic'].drop()
+db.create_collection('Titanic')
+db['Titanic'].insert_many(list_data)
+
+cursor = db['Titanic'].find({})
+
+# for document in cursor:
+#     print('result', document)
 
 # - How many passengers survived, and how many died?
+cursor = db['Titatnic'].find({
+    'survived': 1
+})
+print('@@', cursor.count())
+
 # - How many passengers were in each class?
 # - How many passengers survived/died within each class?
 # - What was the average age of survivors vs nonsurvivors?
