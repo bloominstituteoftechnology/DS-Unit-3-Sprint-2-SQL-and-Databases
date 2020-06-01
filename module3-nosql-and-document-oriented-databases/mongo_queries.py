15fd255f85c8ddd24e73a38b552453f9c4fe427c
RAV10K1 <ravindra.tennekone@gmail.com>
2020-06-01 12:16:45 -0700

    mongodb

---
+++ module3-nosql-and-document-oriented-databases/mongo_queries.py
@@ -0,0 +1,39 @@
+import pymongo
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_USER = os.getenv("MONGO_USER", default="OOPS")
+DB_PASSWORD = os.getenv("MONGO_PASSWORD", default="OOPS")
+CLUSTER_NAME = os.getenv("MONGO_CLUSTER_NAME", default="OOPS")
+
+
+connection_uri = f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@{CLUSTER_NAME}.mongodb.net/test?retryWrites=true&w=majority"
+print("----------------")
+print("URI:", connection_uri)
+
+client = pymongo.MongoClient(connection_uri)
+print("----------------")
+print("CLIENT:", type(client), client)
+
+db = client.test_database # "test_database" or whatever you want to call it
+print("----------------")
+print("DB:", type(db), db)
+
+collection = db.pokemon_test # "pokemon_test" or whatever you want to call it
+print("----------------")
+print("COLLECTION:", type(collection), collection)
+
+print("----------------")
+print("COLLECTIONS:")
+print(db.list_collection_names())
+
+collection.insert_one({
+    "name": "Pikachu",
+    "level": 30,
+    "exp": 76000000000,
+    "hp": 400,
+})
+print("DOCS:", collection.count_documents({}))
+print(collection.count_documents({"name": "Pikachu"}))
