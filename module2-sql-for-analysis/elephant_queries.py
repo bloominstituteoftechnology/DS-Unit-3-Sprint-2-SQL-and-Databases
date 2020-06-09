97da90a359b4f4c5fd670accd25747cc62e13bd8
britneh <brhacket@gmail.com>
2020-06-09 14:08:30 -0700

    elephant queries

--- module2-sql-for-analysis/elephant_queries.py
+++ module2-sql-for-analysis/elephant_queries.py
@@ -10,6 +10,8 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 
+print(DB_NAME, DB_USER, DB_PASSWORD, DB_HOST)
+
 connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
 print("CONNECTION:", connection)
 
