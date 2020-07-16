6ea57a9ecfca37f4ceaa36da5acb64c5161b9253
dmhliu <dmhliu@gmail.com>
2020-07-16 11:11:32 -0700

    hw

--- module3-nosql-and-document-oriented-databases/mongolamb.py
+++ module3-nosql-and-document-oriented-databases/mongolamb.py
@@ -1,10 +1,12 @@
 import pymongo 
 import datetime
+import json
 client = pymongo.MongoClient("mongodb+srv://foobarfoobar:foobarfoobar@cluster0.vvgzp.gcp.mongodb.net/?retryWrites=true&w=majority")
 db = client.test
 #collection =  db.test_collection
 
-"""represent a document as a dict, or json"""
+"""EXAMPLE FROM DOCS
+represent a document as a dict, or json"""
 
 testpost = {"author": "Dave Liu",
             "text": "trivial example of document",
@@ -13,4 +15,22 @@ testpost = {"author": "Dave Liu",
 posts = db.posts
 post_id = posts.insert_one(testpost).inserted_id
 print(post_id)
-  
+
+"""rpg """
+
+rpg_character = {'id': 1, "data": {'name': "test char", 'attr': [10,3,0,0]}}
+
+rpg_test = db.rpg_test
+
+rpg_test.insert_one(rpg_character)
+
+json_url = "https://github.com/LambdaSchool/Django-RPG/blob/master/testdata.json"
+import bson
+with open('testdata.json') as json_file:
+    rpg_json = json.load(json_file)[0]
+
+rpg_mdb = db.rpg_data
+table_id = rpg_mdb.insert_one(rpg_json)
+curs = rpg_mdb.find({"_id": table_id})
+print(curs.count())
+
