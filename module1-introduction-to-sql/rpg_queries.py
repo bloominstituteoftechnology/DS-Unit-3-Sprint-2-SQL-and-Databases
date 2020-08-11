ff08bfb5a61ecb3e30cfaa7f02888a38c6dec5e0
PeggyK1 <peggy.krom@protonmail.com>
2020-08-11 08:04:55 -0600

    Update rpg_queries

--- module1-introduction-to-sql/rpg_queries.py
+++ module1-introduction-to-sql/rpg_queries.py
@@ -69,7 +69,53 @@ print(174-37)
 
 # How many items does each character have (First 20 Rows)
 # Still needs work
-query5 = 'SELECT * FROM charactercreator_character_inventory GROUP BY character_id LIMIT 20;'
+query5 = """
+'SELECT COUNT(item_id)
+FROM charactercreator_character_inventory AS cci
+GROUP BY cci.character_id
+LIMIT 20'
+"""
 curs.execute(query5)
 results5 = curs.fetchall()
 print(results5)
+
+# How many weapons does each character have (First 20 Rows)
+query6 = """
+'SELECT COUNT(item_ptr_id)
+FROM charactercreator_character_inventory AS cci
+INNER JOIN armory_weapon
+ON item_id = item_ptr_id
+GROUP BY cci.character_id
+LIMIT 20'
+"""
+curs.execute(query6)
+results6 = curs.fetchall()
+print(results6)
+
+# Avg number of items each character has
+
+query7 = """
+'SELECT AVG(item_id) FROM
+(SELECT COUNT(item_id)
+FROM charactercreator_character inventory AS cci
+GROUP BY cci.character_id)'
+"""
+curs.execute(query7)
+results7 = curs.fetchall()
+print(results7)
+
+# print(sum(count) / len(count))
+
+# Avg number of weapons each character has
+
+query8 = """
+'SELECT AVG(item_ptr_id) FROM
+(SELECT COUNT(item_ptr_id)
+FROM charactercreator_character_inventory AS cci
+INNER JOIN armory_weapon
+ON item_id = item_ptr_id
+GROUP BY cci.character_id)
+"""
+curs.execute(query8)
+results8 = curs.fetchall()
+print(results8)
