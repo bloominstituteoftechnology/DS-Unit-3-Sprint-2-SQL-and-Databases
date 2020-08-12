67c455eacf0ffb542a8d71f51ce7381924e81d94
John-G-Thomas <johnthomasg0215@gmail.com>
2020-08-12 12:12:46 -0400

    last commit

--- module2-sql-for-analysis/insert_titanic.py
+++ module2-sql-for-analysis/insert_titanic.py
@@ -3,7 +3,7 @@ import psycopg2
 import sqlite3
 
 """change csv to sqlite"""
-df = pd.read_csv("module2-sql-for-analysis/titanic.csv")
+df = pd.read_csv("titanic.csv")
 df.columns = ['Survived', 'Pclass', 'Name', 'Sex', 'Age', 'Siblings/Spouses Aboard',
               'Parents/Children Aboard', 'Fare']
 """
@@ -13,14 +13,14 @@ connecting to the database
 insert data into new table
 """
 df.to_sql('review', connection, index=False)
-cursor.execute('SELECT * FROM review').fetchall()
-print(df.shape)
-print(df.head)
 """
 cursor
 """
 connection = sqlite3.connect('titanic.sqlite3')
 cursor = connection.cursor()
+cursor.execute('SELECT * FROM review').fetchall()
+print(df.shape)
+print(df.head)
 """
 Count how many rows you have
 """
@@ -58,17 +58,19 @@ def refresh_conn_and_cursor(conn, curs):
 pg_conn, pg_curs = refresh_conn_and_cursor(pg_conn, pg_curs)
 # A bunch of integers, and a varchar
 # We need to make a create statement for PostgreSQL that captures this
-Passengers = """
-CREATE TABLE "review" (
-survived INT NOT NULL,
-  pclass INT,
-  name  VARCHAR(40),
-  sex TEXT,
-  age REAL,
-  siblings_Spouses Aboard INT,
-  parents_Children Aboard INT,
-  fare REAL
-)"""
+create_titanic_table = """
+CREATE TABLE titanic (
+  Survived INTEGER,
+  Pclass INTEGER,
+  Name VARCHAR(30),
+  Sex TEXT,
+  Age REAL,
+  Siblings_Spouses_Aboard INTEGER,
+  Parent_Childeren_Aboard INTEGER,
+  Fare REAL
+);
+"""
 # Execute the create table
-pg_curs.execute(Passengers)
+pg_curs = pg_conn.cursor()
+pg_curs.execute(create_titanic_table)
 pg_conn.commit()
