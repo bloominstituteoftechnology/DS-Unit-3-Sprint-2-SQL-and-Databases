1a57405a7db2d00323bfea036a45ae7fcf9f2f77
John-G-Thomas <johnthomasg0215@gmail.com>
2020-08-12 18:51:29 -0400

    last commit

--- module3-nosql-and-document-oriented-databases/Cluster.py
+++ module3-nosql-and-document-oriented-databases/Cluster.py
@@ -1,12 +1,21 @@
 # If colab not locally Find out the IP address of this Colab Instance
 # !curl ipecho.net/plain
+"""
+"How was working with MongoDB different from working with PostgreSQL? What was easier, and what was harder?":
+-MongoDB and PostgrSQL biggest difference is hpw the data/information is stored.
+-In PostgreSQL there are schemas that are used to connect to individual data tables. MongoDB formats and stores the
+all the data formated in documents.  The two have unique differences and but in my opinon MongoDB seems simpler/easier
+way to store data and PostgreSQL has in my opinion extra steps so its harder.
+"""
 """first make shell and install pymongo and dnspython"""
 import pymongo
+
 password = 'Aa02155120'  # Don't commit/share this! Reset it if it leak
 User = 'John-Thomas'
 dbname = 'test'
 connection = (
-            "mongodb+srv://John-Thomas:" + password + "@cluster.y2ftp.mongodb.net/" + dbname + "?retryWrites=true&w=majority")
+        "mongodb+srv://John-Thomas:" + password + "@cluster.y2ftp.mongodb.net/" + dbname + "?retryWrites=true&w"
+                                                                                           "=majority")
 client = pymongo.MongoClient(connection)
 db = client.test
 dir(db.test)
@@ -44,3 +53,9 @@ rpg_doc = {
 }
 db.test.insert_one(rpg_doc)
 list(db.test.find({'doc_type': 'rpg_character'}))
+# Our goal - copy the charactercreator_character table
+get_characters = 'SELECT * FROM charactercreator_character;'
+characters = sl_curs.execute(get_characters).fetchall()
+characters[:10]
+# TODO - come up with a nice document format
+# And insert all these into Mongo!
