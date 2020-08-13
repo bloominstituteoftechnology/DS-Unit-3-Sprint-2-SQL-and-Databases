84b03cea19f35be65b6ea6f100ad93ca9f87169e
PeggyK1 <peggy.krom@protonmail.com>
2020-08-13 13:22:28 -0600

    update

--- module3-nosql-and-document-oriented-databases/rpg_mongodb.py
+++ module3-nosql-and-document-oriented-databases/rpg_mongodb.py
@@ -1,5 +1,5 @@
-pip install pymongo
-pip install dnspython
+# pip install pymongo
+# pip install dnspython
 
 import pymongo
 import sqlite3
@@ -22,8 +22,29 @@ sl_curs = sl_conn.cursor()
 get_characters = 'SELECT * FROM charactercreator_character'
 characters = sl_curs.execute(get_characters).fetchall()
 
+characters[:10]
 # Assigning characters to list
-characters = [characters]
+# characters = [characters]
+
+# Make a doc
+for character in characters
+    rpg_doc = {
+        'sql_key': characters[0],
+        'name': characters[1],
+        'hp': characters[2],
+        'level': characters[3]
+    }
+    db.test.insert_one(rpg_doc)
 
 # Insert characters
-db.test.insert_many(characters)
+# db.test.insert_many(characters)
+
+
+### QUESTIONS
+### HOW WAS WORKING WITH MONGODB DIFFERENT FROM WORKING POSTGRESQL
+# MongoDB seems to have fewer steps but postgresql seems more simple
+
+# PostgreSQL has a stricter schema, you can have different features
+# in the data in MongoDB which could cause problems with your DB,
+
+# I think PostgreSQL was easier and MongoDB was harder
