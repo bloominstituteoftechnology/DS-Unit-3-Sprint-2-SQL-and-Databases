7bc0468bea0defd404fc2855f1767953a2b3160e
ed.chin.git <edgardochin@gmail.com>
2020-09-24 15:50:33 -0500

    Refactor

--- SQL Server/test.py
+++ SQL Server/test.py
@@ -1,17 +1,26 @@
 from sqlalchemy import create_engine
 import pandas as pd
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 import pymssql  ## http://www.pymssql.org/index.html
 
 def main():
     # ____ Connect to SQL Server __________
-    SVR = os.getenv("DS_DB_NAME")
-    DRIVER = os.getenv("DS_DB_USER")
-    SVR = os.getenv("DS_DB_HOST")
+    HOST = os.getenv("DS_DB_HOST")
+    DBASE = os.getenv("DS_DB_NAME")
+    USER = os.getenv("DS_DB_USER")
     PWORD = os.getenv("DS_DB_PASSWORD")
-    conn = pymssql.connect(SVR, USER, PWORD, DBASE)
+    conn = pymssql.connect(HOST, USER, PWORD, DBASE)
 
     df = pd.read_sql_query("select * from dbo.Customers", conn)
+    df.set_index('id', inplace=True )
+    
     print(df)
+
+    print(df.columns[0])
+
 
     return
 
